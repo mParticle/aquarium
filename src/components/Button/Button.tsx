c38d19ade5eb441d7dda0c5a1c53b53ea75f52a1
jdickman <jdickman@mparticle.com>
2023-12-08 18:27:03 -0800

    add wrapped Dropdown with initial story

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Button as AntButton, ButtonProps as AntButtonProps } from "antd";
+import { Button as AntButton } from "antd";
+import { ButtonProps as AntButtonProps } from "antd";
 
-interface IButtonProps extends AntButtonProps{
+interface IButtonProps extends AntButtonProps {
   copy: string;
 }
 
-export function Button(props: IButtonProps) {
-  return <AntButton {...props}>{props.copy}</AntButton>;
-}
+export const Button = (props: IButtonProps) => <>
+  <AntButton {...props}>
+    {props.copy}
+  </AntButton>
+</>;
