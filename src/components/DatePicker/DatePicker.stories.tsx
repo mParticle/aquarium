0432b98e300150fb06942c62f0e7ee645c201c8e
jdickman <jdickman@mparticle.com>
2023-12-10 21:14:19 -0800

    Add DatePicker Component

---
+++ src/components/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,19 @@
+import { DatePicker } from "src/components/DatePicker/DatePicker";
+import { Meta } from "@storybook/react";
+import { StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof DatePicker> = {
+  title: "Aquarium/Data Entry/Date Picker",
+  component: DatePicker,
+
+  args: {},
+};
+export default meta;
+
+
+type Story = StoryObj<typeof DatePicker>
+
+
+export const Primary: Story = {
+  args: {},
+};
