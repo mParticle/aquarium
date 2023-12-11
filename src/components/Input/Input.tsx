83223e2fc6c2bd77ea47e3a628584ba67f330a50
jdickman <jdickman@mparticle.com>
2023-12-10 21:15:19 -0800

    Update Input Component, remove copy prop

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,11 +3,8 @@ import { Input as AntInput } from "antd";
 import { InputProps as AntInputProps } from "antd";
 
 interface IInputProps extends AntInputProps {
-  copy: string;
 }
 
 export const Input = (props: IInputProps) => <>
-  <AntInput {...props}>
-    {props.copy}
-  </AntInput>
+  <AntInput {...props}/>
 </>;
