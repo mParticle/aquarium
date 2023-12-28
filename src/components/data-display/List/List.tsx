7826389309d2746a05322f4916fe083074550b58
jdickman <jdickman@mparticle.com>
2023-12-28 13:18:13 -0500

    add generated List stories

--- src/components/data-display/List/List.tsx
+++ src/components/data-display/List/List.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { List as AntList } from "antd";
 import { ListProps as AntListProps } from "antd";
 
-export interface IListProps extends AntListProps {
+export interface IListProps<T> extends AntListProps<T> {
 }
 
-export const List = (props: IListProps) => {
-  return <>
-    <AntList {...props}/>
-  </>;
-};
+export const List = <T extends any>(props: IListProps<T>) => <>
+  <AntList {...props}/>
+</>;
+
+List.Item = AntList.Item
