82227dcb30a37e1b3e24d108768b58b45fca955e
jdickman <jdickman@mparticle.com>
2023-12-28 13:26:51 -0500

    add generated Empty stories

--- src/components/data-display/Empty/Empty.tsx
+++ src/components/data-display/Empty/Empty.tsx
@@ -10,3 +10,6 @@ export const Empty = (props: IEmptyProps) => {
     <AntEmpty {...props}/>
   </>;
 };
+
+Empty.PRESENTED_IMAGE_DEFAULT = AntEmpty.PRESENTED_IMAGE_DEFAULT;
+Empty.PRESENTED_IMAGE_SIMPLE = AntEmpty.PRESENTED_IMAGE_SIMPLE;
