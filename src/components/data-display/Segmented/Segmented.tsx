7e7fdf2a127e98f8c91a28c646e49ea352692ce0
jdickman <jdickman@mparticle.com>
2024-01-02 10:33:51 -0500

    update Segmented props

--- src/components/data-display/Segmented/Segmented.tsx
+++ src/components/data-display/Segmented/Segmented.tsx
@@ -3,7 +3,7 @@ import { Segmented as AntSegmented } from "antd";
 import { SegmentedProps as AntSegmentedProps } from "antd";
 
 export interface ISegmentedProps extends AntSegmentedProps {
-  ref: React.RefObject<HTMLDivElement>;
+  ref?: React.RefObject<HTMLDivElement>;
 }
 
 export const Segmented = (props: ISegmentedProps) => {
