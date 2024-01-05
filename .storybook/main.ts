6d62cf6d923652c88dc75bb57715a8f4d6370a4d
Kevin Prudente <kprudente@mparticle.com>
2024-01-05 16:56:56 -0500

    Adding a type for the storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,11 @@
+// Replace your-framework with the framework you are using (e.g., react-webpack5, vue3-vite)
+import type { StorybookConfig } from "@storybook/react-vite";
 import react from '@vitejs/plugin-react'
+import { PluginOption } from "vite";
 
-const config = {
+type StorybookVitePlugins = { plugins: PluginOption[][] }
+
+const config: StorybookConfig & StorybookVitePlugins = {
   framework: "@storybook/react-vite",
 
   stories: [
