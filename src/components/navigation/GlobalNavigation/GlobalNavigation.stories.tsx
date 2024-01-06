0dfbcad4fb2895fb00365d3c958c39687f22087e
jdickman <jdickman@mparticle.com>
2024-01-05 23:54:45 -0500

    custom styles for menu in nav, starting models for nav menu items

--- src/components/navigation/GlobalNavigation/GlobalNavigation.stories.tsx
+++ src/components/navigation/GlobalNavigation/GlobalNavigation.stories.tsx
@@ -1,14 +1,58 @@
-import * as React from 'react'
+import * as React from "react";
 import { Meta } from "@storybook/react";
 import { StoryObj } from "@storybook/react";
 import { GlobalNavigation } from "src/components";
 import { Layout } from "src/components";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { faLifeRing } from "@fortawesome/free-solid-svg-icons";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { IGlobalNavigationTool } from "src/components/navigation/GlobalNavigation/GlobalNavigation";
+import { IGlobalNavigationManagement } from "src/components/navigation/GlobalNavigation/GlobalNavigation";
+
+
+const tools: IGlobalNavigationTool[] = [
+  {
+    label: "Activity",
+    icon: faBars,
+  },
+  {
+    label: "Data Master",
+    icon: faDatabase,
+  },
+  {
+    label: "Audiences",
+    icon: faPeopleGroup,
+  },
+];
+
+
+const management: IGlobalNavigationManagement[] = [
+  {
+    label: "Account",
+    icon: faBell,
+  },
+  {
+    label: "Support",
+    icon: faLifeRing,
+  },
+  {
+    label: "Settings",
+    icon: faGear,
+  },
+];
+
 
 const meta: Meta<typeof GlobalNavigation> = {
   title: "Aquarium/Navigation/GlobalNavigation",
   component: props => <Layout><GlobalNavigation {...props}></GlobalNavigation></Layout>,
 
-  args: {},
+  args: {
+    tools,
+    management,
+  },
 };
 export default meta;
 
--- src/components/navigation/GlobalNavigation/GlobalNavigation.tsx
+++ src/components/navigation/GlobalNavigation/GlobalNavigation.tsx
@@ -1,27 +1,36 @@
 import React from "react";
 import { useState } from "react";
-import { faUpload, faHamburger, faDiceOne, faBell, faLifeRing, faGear } from "@fortawesome/free-solid-svg-icons";
 import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { Layout, Menu } from "src/components";
 import { Flex } from "src/components";
-import { IMenuProps } from "src/components/navigation/Menu/Menu";
 import { Icon } from "src/components/general/Icon/Icon";
+import { ItemType } from "antd/es/menu/hooks/useItems";
+import "./global-navigation.css";
+
+export interface IBaseGlobalNavigationItem {
+  label: string;
+  icon: IconDefinition;
+}
+
+export interface IGlobalNavigationTool extends IBaseGlobalNavigationItem {
+}
+
+export interface IGlobalNavigationManagement extends IBaseGlobalNavigationItem {
+}
+
 
 export interface IGlobalNavigationProps {
-
+  tools: IGlobalNavigationTool[];
+  management: IGlobalNavigationManagement[];
 }
 
 export const GlobalNavigation = (props: IGlobalNavigationProps) => {
   const [collapsed, setCollapsed] = useState(true);
 
-  const topItems: IMenuProps["items"] = [faUpload, faHamburger, faDiceOne].map(generateItem);
-
-  const bottomItems: IMenuProps["items"] = [faBell, faLifeRing, faGear].map(generateItem);
-  
-
   return (
     <Layout.Sider width={200}
-                  style={{ border: "solid 1px black", minHeight: "500px", backgroundColor: "white" }}
+                  collapsedWidth={100}
+                  className="globalNavigation__slider"
                   collapsible
                   collapsed={collapsed}
                   trigger={null}
@@ -30,21 +39,20 @@ export const GlobalNavigation = (props: IGlobalNavigationProps) => {
     >
 
       <Flex vertical justify="space-between" style={{ height: "100%" }}>
-        <Menu items={topItems}/>
-        <Menu items={bottomItems}  />
+        <Menu items={props.tools.map(generateMenuItem)}/>
+        <Menu items={props.management.map(generateMenuItem)}/>
       </Flex>
 
     </Layout.Sider>
   );
 
-
-  function generateItem(icon: IconDefinition, index: number) {
+  function generateMenuItem(item: IBaseGlobalNavigationItem, index: number): ItemType {
     const key = index + 1 + "";
     return {
       key: `sub${key}`,
-      icon: <Icon icon={icon}/>,
-      label: `subnav ${key}`,
-
+      icon: <Icon icon={item.icon}/>,
+      label: item.label,
+      className: "globalNavigation__item",
       children: new Array(4).fill(null).map((_, j) => {
         const subKey = index * 4 + j + 1;
         return {
