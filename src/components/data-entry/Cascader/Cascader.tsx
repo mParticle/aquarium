0b7fe7654e653e780c5b1518dc9af3f95e05714c
jared-dickman <zenador15@gmail.com>
2024-01-12 16:12:54 -0500

    Chore: fixup prop types (#54)

--- src/components/data-entry/Cascader/Cascader.tsx
+++ src/components/data-entry/Cascader/Cascader.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Cascader as AntCascader } from "antd";
 import { CascaderProps as AntCascaderProps } from "antd";
 
-export interface ICascaderProps extends AntCascaderProps {
-}
+export type ICascaderProps = AntCascaderProps 
 
 export const Cascader = (props: ICascaderProps) => {
   return <>
