e847659201e078a59e610e2d3d71d6c5e6a4739e
Gabriel Tiburcio <v-gtiburcio@mparticle.com>
2024-01-16 10:34:01 -0300

    Initial prettier and eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "standard-with-typescript",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    extraFileExtensions: [".md", ".css"],
+  },
+  plugins: ["react", "react-hooks"],
+  rules: {},
+};
---
+++ docs/dev.md
@@ -0,0 +1,5 @@
+### Editor configuration
+
+- **Prettier**: For configuring your editor to play nicely with Prettier, take a look at the [Editors doc page](https://prettier.io/docs/en/editors).
+  - Also, if you're using VSCode you might want to set prettier as the default formatter and also turn on "Format on Save" option.
+- **ESLint**: Check [Integrations doc page](https://eslint.org/docs/latest/use/integrations)
