1919d21b6c531ae293bd378e34fa085a4a257faf
jdickman <jdickman@mparticle.com>
2024-01-17 16:10:53 -0500

    update index

--- vite.config.js
+++ vite.config.js
@@ -2,46 +2,41 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      src: '/src',
+export default defineConfig(
+  {
+    resolve: {
+      alias: {
+        src: '/src',
+      },
     },
-  },
 
-  plugins: [
-    dts({
-      rollupTypes: true,
-      insertTypesEntry: true,
-      outDir: 'dist',
-      exclude: [
-        'src/**/*.stories.ts',
-        'src/**/*.stories.tsx',
-        'src/hooks/*.ts',
-      ],
-    }),
-  ],
-  
-  esbuild: {
-    jsxInject: `import React from "react"; import { expect } from "@storybook/test";`,
-  },
-  
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/components/index.ts'),
-      name: 'aquarium',
-      formats: ['es'],
-      fileName: 'aquarium',
-    },
-    rollupOptions: {
-      external: ['react', 'antd'],
-      output: {
-        globals: {
-          antd: 'antd',
-          react: 'React',
+    plugins: [
+      dts({
+            rollupTypes: true,
+            exclude: [
+              'src/**/*.stories.ts',
+              'src/**/*.stories.tsx',
+              'src/hooks/*.ts',
+            ],
+          }),
+    ],
+
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: resolve(__dirname, 'src/components/index.ts'),
+        name: 'aquarium',
+        formats: ['es'],
+        fileName: 'aquarium',
+      },
+      rollupOptions: {
+        external: ['react', 'antd'],
+        output: {
+          globals: {
+            antd: 'antd',
+            react: 'React',
+          },
         },
       },
     },
-  },
-})
+  })
