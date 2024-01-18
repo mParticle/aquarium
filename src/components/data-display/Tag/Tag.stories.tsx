5960fde038f641067aa23b7a544c9c50c4c5217b
Gabriel Tiburcio <v-gtiburcio@mparticle.com>
2024-01-18 14:17:10 -0300

    tag stories

--- src/components/data-display/Tag/Tag.stories.tsx
+++ src/components/data-display/Tag/Tag.stories.tsx
@@ -1,19 +1,126 @@
 import { Meta } from "@storybook/react";
 import { StoryObj } from "@storybook/react";
 import { Tag } from "src/components/data-display/Tag/Tag";
+import { Divider, Flex, Icon, Space } from "src/components";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { ExampleStory } from "src/utils/ExampleStory";
+import { useState } from "react";
+import { JustifyCenter } from "src/components/layout/Flex/Flex.stories";
+import { userEvent } from "@storybook/test";
 
 const meta: Meta<typeof Tag> = {
   title: "Aquarium/Data Display/Tag",
-  component: Tag,
+  component: (props) => <Tag {...props}>Tag</Tag>,
 
-  args: {},
+  args: {
+    closeIcon: false,
+    color: "",
+    icon: null,
+    bordered: true,
+    onClose: (e) => {
+      alert("Tag Closed");
+    },
+  },
 };
 export default meta;
 
+type Story = StoryObj<typeof Tag>;
 
-type Story = StoryObj<typeof Tag>
+export const Primary: Story = {};
 
+export const WithCustomCloseIcon: Story = {
+  args: {
+    closeIcon: true,
+  },
+};
 
+export const WithColor: Story = {
+  args: {
+    color: "#3498db",
+  },
+};
 
-export const Primary: Story = {
-}; 
+export const WithIcon: Story = {
+  args: {
+    icon: <Icon icon={faUser} />,
+  },
+};
+
+export const WithoutBorder: Story = {
+  args: {
+    bordered: false,
+  },
+};
+
+export const WithOnCloseCallback: Story = {
+  args: {
+    closeIcon: true,
+    onClose: (e) => {
+      alert("Tag Closed");
+    },
+  },
+};
+
+export const CheckableTagPrimary: Story = {
+  render: () => {
+    const [checked, setChecked] = useState(true);
+
+    return (
+      <>
+        <ExampleStory title="Basic checkable usage">
+          <Flex justify="center">
+            <Tag.CheckableTag
+              checked={checked}
+              bordered
+              onChange={(checked) => {
+                alert(`Tag ${checked ? "Checked" : "Unchecked"}`);
+                setChecked((curr) => !curr);
+              }}
+            >
+              Tag
+            </Tag.CheckableTag>
+          </Flex>
+        </ExampleStory>
+      </>
+    );
+  },
+  play: async (context) => {
+    const tag = context.canvasElement.querySelector(
+      ".ant-tag.ant-tag-checkable"
+    );
+    expect(tag).not.toBeNull();
+    expect(tag?.classList.contains("ant-tag-checkable-checked")).toBeTruthy();
+    await userEvent.click(tag);
+    expect(tag?.classList.contains("ant-tag-checkable-checked")).toBeFalsy();
+  },
+};
+
+export const Colors: Story = {
+  render: () => {
+    return (
+      <>
+        <Divider orientation="left">Presets</Divider>
+        <Space size={[0, 8]} wrap>
+          <Tag color="magenta">magenta</Tag>
+          <Tag color="red">red</Tag>
+          <Tag color="volcano">volcano</Tag>
+          <Tag color="orange">orange</Tag>
+          <Tag color="gold">gold</Tag>
+          <Tag color="lime">lime</Tag>
+          <Tag color="green">green</Tag>
+          <Tag color="cyan">cyan</Tag>
+          <Tag color="blue">blue</Tag>
+          <Tag color="geekblue">geekblue</Tag>
+          <Tag color="purple">purple</Tag>
+        </Space>
+        <Divider orientation="left">Custom</Divider>
+        <Space size={[0, 8]} wrap>
+          <Tag color="#f50">#f50</Tag>
+          <Tag color="#2db7f5">#2db7f5</Tag>
+          <Tag color="#87d068">#87d068</Tag>
+          <Tag color="#108ee9">#108ee9</Tag>
+        </Space>
+      </>
+    );
+  },
+};
--- src/components/data-display/Tag/Tag.tsx
+++ src/components/data-display/Tag/Tag.tsx
@@ -1,11 +1,27 @@
 import { Tag as AntTag } from "antd";
 import { TagProps as AntTagProps } from "antd";
+import { CheckableTagProps as AntCheckableTagProps } from "antd/lib/tag";
 
-export interface ITagProps extends AntTagProps {
-}
+export interface ITagProps extends AntTagProps {}
+
+/**
+ * Ant's Tag.CheckableTag props for some reason are not very well typed so we have to do this
+ * to make autocomplete work correctly
+ */
+type CheckableTagProps = AntCheckableTagProps & ITagProps;
 
 export const Tag = (props: ITagProps) => {
-  return <>
-    <AntTag {...props}/>
-  </>;
+  return (
+    <>
+      <AntTag {...props} />
+    </>
+  );
+};
+
+Tag.CheckableTag = (props: CheckableTagProps) => {
+  return (
+    <>
+      <AntTag.CheckableTag {...props} />
+    </>
+  );
 };
