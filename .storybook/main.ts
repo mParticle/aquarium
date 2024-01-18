3f03cbac1499572787b934e2c6be2af7f0706426
jdickman <jdickman@mparticle.com>
2024-01-18 17:21:31 -0500

    build: update test-storybook:ci config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,9 @@
 // Replace your-framework with the framework you are using (e.g., react-webpack5, vue3-vite)
 import type { StorybookConfig } from "@storybook/react-vite";
 import react from '@vitejs/plugin-react'
-import { PluginOption } from "vite";
+import { PluginOption, Plugin } from 'vite'
 
-type StorybookVitePlugins = { plugins: PluginOption[][] }
+type StorybookVitePlugins = { plugins: (PluginOption[] | Plugin)[]; };
 
 const config: StorybookConfig & StorybookVitePlugins = {
   framework: "@storybook/react-vite",
