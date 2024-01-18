de807e333173aa4b959c0497726687e078df8131
Gabriel Tiburcio <v-gtiburcio@mparticle.com>
2024-01-17 21:13:01 -0300

    format storybook config files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,27 +1,27 @@
 // Replace your-framework with the framework you are using (e.g., react-webpack5, vue3-vite)
-import type { StorybookConfig } from "@storybook/react-vite";
-import react from "@vitejs/plugin-react";
-import { PluginOption } from "vite";
+import type { StorybookConfig } from '@storybook/react-vite'
+import react from '@vitejs/plugin-react'
+import { PluginOption } from 'vite'
 
-type StorybookVitePlugins = { plugins: PluginOption[][] };
+type StorybookVitePlugins = { plugins: PluginOption[][] }
 
 const config: StorybookConfig & StorybookVitePlugins = {
-  framework: "@storybook/react-vite",
+  framework: '@storybook/react-vite',
 
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
 
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
 
   docs: {
     autodocs: true,
-    defaultName: "Documentation",
+    defaultName: 'Documentation',
   },
 
   plugins: [react()],
-};
+}
 
-export default config;
+export default config
