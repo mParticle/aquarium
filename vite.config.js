02fab6df3109afe4661ab1cda04dbd6b978b1dee
Gabriel Tiburcio <v-gtiburcio@mparticle.com>
2024-01-23 18:51:49 -0300

    Removes babel stuff and antd from bundle

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 
 export default defineConfig({
   resolve: {
@@ -19,47 +18,7 @@ export default defineConfig({
       fileName: "aquarium",
     },
     rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
-      plugins: [
-        /**
-         * Running Babel on the generated code:
-         *  https://github.com/rollup/plugins/blob/master/packages/babel/README.md#running-babel-on-the-generated-code
-         *
-         * Transforming ES6+ syntax to ES5 is not supported yet, there are two ways to do:
-         *  https://github.com/evanw/esbuild/issues/1010#issuecomment-803865232
-         * We choose to run Babel on the output files after esbuild.
-         *
-         * @vitejs/plugin-legacy does not support library mode:
-         *  https://github.com/vitejs/vite/issues/1639
-         */
-        getBabelOutputPlugin({
-          allowAllFormats: true,
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                useBuiltIns: false, // Default：false
-                // Exclude transforms that make all code slower
-                exclude: ["transform-typeof-symbol"],
-                // https://babeljs.io/docs/en/babel-preset-env#modules
-                modules: false,
-              },
-            ],
-          ],
-          plugins: [
-            /**
-             * Extract the helper function.
-             */
-            [
-              "@babel/plugin-transform-runtime",
-              {
-                corejs: false,
-                // version: require('@babel/runtime').version,
-              },
-            ],
-          ],
-        }),
-      ],
+      external: ["react", "react/jsx-runtime", "antd"],
       output: {
         globals: {
           antd: "antd",
