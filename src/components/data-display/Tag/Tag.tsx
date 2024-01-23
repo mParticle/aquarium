f7f6f6d801ca60e45ad91b40f00d66053dd86523
Gabriel Tibúrcio <tibuurcio@gmail.com>
2024-01-23 19:02:31 -0300

    remove comments

--- src/components/data-display/Tag/Tag.tsx
+++ src/components/data-display/Tag/Tag.tsx
@@ -4,10 +4,6 @@ import { CheckableTagProps as AntCheckableTagProps } from "antd/lib/tag";
 
 export interface ITagProps extends AntTagProps {}
 
-/**
- * Ant's Tag.CheckableTag props for some reason are not very well typed so we have to do this
- * to make autocomplete work correctly
- */
 type CheckableTagProps = AntCheckableTagProps & ITagProps;
 
 export const Tag = (props: ITagProps) => {
