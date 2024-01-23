ec0976789a86755cfdbea5cbd2a7435fd27d55e4
Gabriel Tiburcio <v-gtiburcio@mparticle.com>
2024-01-23 18:01:36 -0300

    Testing another approach to running babel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { babel } from "@rollup/plugin-babel";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 
 export default defineConfig({
   resolve: {
@@ -21,9 +21,43 @@ export default defineConfig({
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
       plugins: [
-        babel({
-          babelHelpers: "runtime",
-          extensions: [".js", ".jsx", ".es6", ".es", ".mjs", "ts"],
+        /**
+         * Running Babel on the generated code:
+         *  https://github.com/rollup/plugins/blob/master/packages/babel/README.md#running-babel-on-the-generated-code
+         *
+         * Transforming ES6+ syntax to ES5 is not supported yet, there are two ways to do:
+         *  https://github.com/evanw/esbuild/issues/1010#issuecomment-803865232
+         * We choose to run Babel on the output files after esbuild.
+         *
+         * @vitejs/plugin-legacy does not support library mode:
+         *  https://github.com/vitejs/vite/issues/1639
+         */
+        getBabelOutputPlugin({
+          allowAllFormats: true,
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                useBuiltIns: false, // Default：false
+                // Exclude transforms that make all code slower
+                exclude: ["transform-typeof-symbol"],
+                // https://babeljs.io/docs/en/babel-preset-env#modules
+                modules: false,
+              },
+            ],
+          ],
+          plugins: [
+            /**
+             * Extract the helper function.
+             */
+            [
+              "@babel/plugin-transform-runtime",
+              {
+                corejs: false,
+                // version: require('@babel/runtime').version,
+              },
+            ],
+          ],
         }),
       ],
       output: {
