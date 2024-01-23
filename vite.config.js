46876df759d8a041b92672255d3bdc100f05940d
Gabriel Tiburcio <v-gtiburcio@mparticle.com>
2024-01-23 16:36:06 -0300

    Test react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   resolve: {
@@ -8,7 +9,7 @@ export default defineConfig({
       src: "/src",
     },
   },
-  plugins: [dts({ include: "src", rollupTypes: true })],
+  plugins: [react(), dts({ include: "src", rollupTypes: true })],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
