db11f97f46c72dcdd1bb4257ec58994db25bdd29
jdickman <jdickman@mparticle.com>
2024-01-24 13:26:16 +0000

    test withoutVitePlugins

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,12 @@
-// Replace your-framework with the framework you are using (e.g., react-webpack5, vue3-vite)
 import type { StorybookConfig } from "@storybook/react-vite";
 import react from '@vitejs/plugin-react'
 import { PluginOption, Plugin } from 'vite'
+import { withoutVitePlugins } from '@storybook/builder-vite'
 
 type StorybookVitePlugins = { plugins: (PluginOption[] | Plugin)[]; };
 
 const config: StorybookConfig & StorybookVitePlugins = {
   framework: "@storybook/react-vite",
-
   stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(ts|tsx)",
@@ -25,6 +24,13 @@ const config: StorybookConfig & StorybookVitePlugins = {
   },
 
   plugins: [react()],
+
+  async viteFinal(config) {
+    return ({
+      ...config,
+      plugins: await withoutVitePlugins(config.plugins, ["vite:dts"]),
+    });
+  },
 };
 
 export default config;
