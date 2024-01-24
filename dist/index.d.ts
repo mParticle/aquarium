c3850741f94011e3baffe2a0e9bc9b26040d6cb5
jdickman <jdickman@mparticle.com>
2024-01-24 13:18:37 +0000

    test pushing dist index.d.ts

---
+++ dist/index.d.ts
@@ -0,0 +1,486 @@
+import { AffixProps } from 'antd';
+import { AlertProps } from 'antd';
+import { AnchorProps } from 'antd';
+import { AppProps } from 'antd';
+import { AutoCompleteProps } from 'antd';
+import { AvatarProps } from 'antd';
+import { BadgeProps } from 'antd';
+import { BaseOptionType } from 'antd/es/select';
+import { BreadcrumbProps } from 'antd';
+import { ButtonProps } from 'antd';
+import { CalendarProps } from 'antd';
+import { CardProps } from 'antd';
+import { CarouselProps } from 'antd';
+import { CascaderPanelProps } from 'antd/es/cascader/Panel';
+import { CascaderProps } from 'antd';
+import { CheckboxGroupProps } from 'antd/es/checkbox';
+import { CheckboxProps } from 'antd';
+import { CheckboxRef } from 'rc-checkbox';
+import { CheckboxValueType } from 'antd/es/checkbox/Group';
+import { Col } from 'antd';
+import { CollapseProps } from 'antd';
+import { ColorPickerProps } from 'antd';
+import { ConfigProviderProps } from 'antd/es/config-provider';
+import { Dayjs } from 'dayjs';
+import { default as default_2 } from 'antd/es/alert/ErrorBoundary';
+import { DefaultOptionType } from 'antd/es/select';
+import { DescriptionsProps } from 'antd';
+import { DividerProps } from 'antd';
+import { DrawerProps } from 'antd';
+import { DropdownButtonProps } from 'antd/es/dropdown';
+import { DropdownProps } from 'antd';
+import { EmptyProps } from 'antd';
+import { FC } from 'react';
+import { FlexProps } from 'antd';
+import { FloatButtonProps } from 'antd';
+import { FormProps } from 'antd';
+import { ForwardRefExoticComponent } from 'react';
+import { Grid } from 'antd';
+import { GroupProps } from 'antd/es/input';
+import { ColProps as IColProps } from 'antd';
+import { ImageProps } from 'antd';
+import { IModalProps as IModalProps_2 } from '../../../../../../../src/components/feedback/Modal/Modal';
+import { InputNumberProps } from 'antd';
+import { InputProps } from 'antd';
+import { InputRef } from 'antd';
+import { RowProps as IRowProps } from 'antd';
+import { JSX as JSX_2 } from 'react/jsx-runtime';
+import { JSXElementConstructor } from 'react';
+import { LayoutProps } from 'antd';
+import { ListItemTypeProps } from 'antd/es/list/Item';
+import { ListProps } from 'antd';
+import { MemoExoticComponent } from 'react';
+import { MentionProps } from 'antd';
+import { MenuProps } from 'antd';
+import { MessageArgsProps } from 'antd';
+import { ModalProps } from 'antd';
+import { NotificationArgsProps } from 'antd';
+import { PaginationProps } from 'antd';
+import { PasswordProps } from 'antd/es/input';
+import { PickerDateProps } from 'antd/es/date-picker/generatePicker';
+import { PopconfirmProps } from 'antd';
+import { PopoverProps } from 'antd';
+import { ProgressProps } from 'antd';
+import { QRCodeProps } from 'antd';
+import { RadioButtonProps } from 'antd/es/radio/radioButton';
+import { RadioGroupProps } from 'antd';
+import { RadioProps } from 'antd';
+import { RateProps } from 'antd';
+import { ReactElement } from 'react';
+import { ReactNode } from 'react';
+import { RefAttributes } from 'react';
+import { ResultProps } from 'antd';
+import { Row } from 'antd';
+import { SearchProps } from 'antd/es/input';
+import { SegmentedProps } from 'antd';
+import { SelectProps } from 'antd';
+import { SiderProps } from 'antd';
+import { SkeletonProps } from 'antd';
+import { SliderRangeProps } from 'antd/es/slider';
+import { SliderSingleProps } from 'antd/es/slider';
+import { SpaceProps } from 'antd';
+import { SpinProps } from 'antd';
+import { StatisticProps } from 'antd';
+import { StepsProps } from 'antd';
+import { SwitchProps } from 'antd';
+import { TableProps } from 'antd';
+import { TabsProps } from 'antd';
+import { TagProps } from 'antd';
+import { TextAreaProps } from 'antd/es/input';
+import { TextAreaRef } from 'antd/es/input/TextArea';
+import { TimelineProps } from 'antd';
+import { TimePickerProps } from 'antd';
+import { TooltipPropsWithTitle } from 'antd/es/tooltip';
+import { TourProps } from 'antd';
+import { TransferProps } from 'antd';
+import { TreeProps } from 'antd';
+import { TreeSelectProps } from 'antd';
+import { UploadProps } from 'antd';
+import { WatermarkProps } from 'antd';
+
+export declare const Affix: (props: IAffixProps) => JSX_2.Element;
+
+export declare const Alert: {
+    (props: IAlertProps): JSX_2.Element;
+    ErrorBoundary: typeof default_2;
+};
+
+export declare const Anchor: (props: IAnchorProps) => JSX_2.Element;
+
+declare type AntDatePickerProps = PickerDateProps<Dayjs>;
+
+export declare const App: (props: IAppProps) => JSX_2.Element;
+
+export declare const AutoComplete: (props: IAutoCompleteProps) => JSX_2.Element;
+
+export declare const Avatar: (props: IAvatarProps) => JSX_2.Element;
+
+export declare const Badge: (props: IBadgeProps) => JSX_2.Element;
+
+export declare const Breadcrumb: (props: IBreadcrumbProps) => JSX_2.Element;
+
+export declare const Button: (props: IButtonProps) => JSX_2.Element;
+
+export declare const Calendar: (props: ICalendarProps) => JSX_2.Element;
+
+export declare const Card: (props: ICardProps) => JSX_2.Element;
+
+export declare const Carousel: (props: ICarouselProps) => JSX_2.Element;
+
+export declare const Cascader: {
+    (props: ICascaderProps): JSX_2.Element;
+    Panel: FC<CascaderPanelProps>;
+    SHOW_PARENT: "SHOW_PARENT";
+    SHOW_CHILD: "SHOW_CHILD";
+};
+
+export declare const Checkbox: {
+    (props: ICheckboxProps): JSX_2.Element;
+    Group: <T extends CheckboxValueType = CheckboxValueType>(props: CheckboxGroupProps<T> & RefAttributes<HTMLDivElement>) => ReactElement<any, string | JSXElementConstructor<any>>;
+};
+
+export { Col }
+
+export declare const Collapse: (props: ICollapseProps) => JSX_2.Element;
+
+export declare const ColorPicker: (props: IColorPickerProps) => JSX_2.Element;
+
+export declare const ConfigProvider: (props: IConfigProviderProps) => JSX_2.Element;
+
+export declare const DatePicker: (props: IDatePickerProps) => JSX_2.Element;
+
+export declare const Descriptions: (props: IDescriptionsProps) => JSX_2.Element;
+
+export declare const Divider: (props: IDividerProps) => JSX_2.Element;
+
+export declare const Drawer: (props: IDrawerProps) => JSX_2.Element;
+
+export declare const Dropdown: {
+    (props: IDropdownProps): JSX_2.Element;
+    Button: FC<DropdownButtonProps>;
+};
+
+export declare const Empty: {
+    (props: IEmptyProps): JSX_2.Element;
+    PRESENTED_IMAGE_DEFAULT: ReactNode;
+    PRESENTED_IMAGE_SIMPLE: ReactNode;
+};
+
+export declare const Flex: (props: IFlexProps) => JSX_2.Element;
+
+export declare const FloatButton: (props: IFloatButtonProps) => JSX_2.Element;
+
+export declare const Form: (props: IFormProps) => JSX_2.Element;
+
+export { Grid }
+
+export declare interface IAffixProps extends AffixProps {
+}
+
+export declare interface IAlertProps extends AlertProps {
+}
+
+export declare interface IAnchorProps extends AnchorProps {
+}
+
+export declare interface IAppProps extends AppProps {
+}
+
+export declare interface IAutoCompleteProps extends AutoCompleteProps {
+}
+
+export declare interface IAvatarProps extends AvatarProps {
+}
+
+export declare interface IBadgeProps extends BadgeProps {
+}
+
+export declare interface IBreadcrumbProps extends BreadcrumbProps {
+}
+
+export declare interface IButtonProps extends ButtonProps {
+}
+
+export declare interface ICalendarProps extends CalendarProps {
+}
+
+export declare interface ICardProps extends CardProps {
+}
+
+export declare interface ICarouselProps extends CarouselProps {
+}
+
+export declare type ICascaderProps<DataNodeType extends BaseOptionType = any> = CascaderProps<DataNodeType>;
+
+export declare interface ICheckboxProps extends CheckboxProps {
+}
+
+export declare interface ICollapseProps extends CollapseProps {
+}
+
+export declare interface IColorPickerProps extends ColorPickerProps {
+}
+
+export { IColProps }
+
+export declare interface IConfigProviderProps extends ConfigProviderProps {
+}
+
+export declare interface IDatePickerProps extends AntDatePickerProps {
+}
+
+export declare interface IDescriptionsProps extends DescriptionsProps {
+}
+
+export declare interface IDividerProps extends DividerProps {
+}
+
+export declare interface IDrawerProps extends DrawerProps {
+}
+
+export declare interface IDropdownProps extends DropdownProps {
+}
+
+export declare interface IEmptyProps extends EmptyProps {
+}
+
+export declare interface IFlexProps extends FlexProps {
+}
+
+export declare interface IFloatButtonProps extends FloatButtonProps {
+}
+
+export declare interface IFormProps extends FormProps {
+    children: React.ReactNode;
+}
+
+export declare interface IImageProps extends ImageProps {
+}
+
+export declare interface IInputNumberProps extends InputNumberProps {
+}
+
+export declare interface IInputProps extends InputProps {
+}
+
+export declare interface ILayoutProps extends LayoutProps {
+}
+
+export declare interface IListProps extends ListProps {
+}
+
+export declare interface ILoadingModalProps<Data> extends Omit<IModalProps_2, "children"> {
+    fetchData(): Promise<Data>;
+    children(initData: Data): React.ReactNode;
+}
+
+declare const Image_2: (props: IImageProps) => JSX_2.Element;
+export { Image_2 as Image }
+
+export declare interface IMentionsProps extends MentionProps {
+}
+
+export declare interface IMenuProps extends MenuProps {
+}
+
+export declare interface IMessageProps extends MessageArgsProps {
+    children: React.ReactNode;
+}
+
+export declare interface IModalProps extends ModalProps {
+}
+
+export declare interface INotificationProps extends NotificationArgsProps {
+    children: React.ReactNode;
+}
+
+export declare const Input: {
+    (props: IInputProps): JSX_2.Element;
+    Group: FC<GroupProps>;
+    Password: ForwardRefExoticComponent<PasswordProps & RefAttributes<InputRef>>;
+    Search: ForwardRefExoticComponent<SearchProps & RefAttributes<InputRef>>;
+    TextArea: ForwardRefExoticComponent<TextAreaProps & RefAttributes<TextAreaRef>>;
+};
+
+export declare const InputNumber: (props: IInputNumberProps) => JSX_2.Element;
+
+export declare interface IPaginationProps extends PaginationProps {
+}
+
+export declare interface IPopconfirmProps extends PopconfirmProps {
+}
+
+export declare interface IPopoverProps extends PopoverProps {
+}
+
+export declare interface IProgressProps extends ProgressProps {
+}
+
+export declare interface IQRCodeProps extends QRCodeProps {
+}
+
+export declare interface IRadioProps extends RadioProps {
+}
+
+export declare interface IRateProps extends RateProps {
+}
+
+export declare interface IResultProps extends ResultProps {
+}
+
+export { IRowProps }
+
+export declare interface ISegmentedProps extends SegmentedProps {
+    ref?: React.RefObject<HTMLDivElement>;
+}
+
+export declare interface ISelectProps<ValueType = any, OptionType extends BaseOptionType | DefaultOptionType = DefaultOptionType> extends SelectProps<ValueType, OptionType> {
+}
+
+export declare interface ISkeletonProps extends Omit<SkeletonProps, "active"> {
+}
+
+export declare type ISliderProps = SliderSingleProps | SliderRangeProps;
+
+export declare interface ISpaceProps extends SpaceProps {
+}
+
+export declare interface ISpinProps extends SpinProps {
+}
+
+export declare interface IStatisticProps extends StatisticProps {
+}
+
+export declare interface IStepsProps extends StepsProps {
+}
+
+export declare interface ISwitchProps extends SwitchProps {
+}
+
+export declare interface ITableProps extends TableProps {
+}
+
+export declare interface ITabsProps extends TabsProps {
+}
+
+export declare interface ITagProps extends TagProps {
+}
+
+export declare interface ITimelineProps extends TimelineProps {
+}
+
+export declare interface ITimePickerProps extends TimePickerProps {
+}
+
+export declare interface ITooltipProps extends TooltipPropsWithTitle {
+}
+
+export declare interface ITourProps extends TourProps {
+}
+
+export declare interface ITransferProps extends TransferProps {
+}
+
+export declare interface ITreeProps extends TreeProps {
+}
+
+export declare interface ITreeSelectProps extends TreeSelectProps {
+}
+
+export declare interface IUploadProps extends UploadProps {
+}
+
+export declare interface IWatermarkProps extends WatermarkProps {
+}
+
+export declare const Layout: {
+    (props: ILayoutProps): JSX_2.Element;
+    Sider: ForwardRefExoticComponent<SiderProps & RefAttributes<HTMLDivElement>>;
+    Footer: ForwardRefExoticComponent<LayoutProps & RefAttributes<HTMLElement>>;
+    Content: ForwardRefExoticComponent<LayoutProps & RefAttributes<HTMLElement>>;
+    Header: ForwardRefExoticComponent<LayoutProps & RefAttributes<HTMLElement>>;
+};
+
+export declare const List: {
+    (props: IListProps): JSX_2.Element;
+    Item: ListItemTypeProps;
+};
+
+export declare function LoadingModal<Data>(props: ILoadingModalProps<Data>): JSX_2.Element;
+
+export declare const Mentions: (props: IMentionsProps) => JSX_2.Element;
+
+export declare const Menu: (props: IMenuProps) => JSX_2.Element;
+
+export declare const Message: (props: IMessageProps) => JSX_2.Element;
+
+export declare const Modal: (props: IModalProps) => JSX_2.Element;
+
+declare const Notification_2: (props: INotificationProps) => JSX_2.Element;
+export { Notification_2 as Notification }
+
+export declare const Pagination: (props: IPaginationProps) => JSX_2.Element;
+
+export declare const Popconfirm: (props: IPopconfirmProps) => JSX_2.Element;
+
+export declare const Popover: (props: IPopoverProps) => JSX_2.Element;
+
+export declare const Progress: (props: IProgressProps) => JSX_2.Element;
+
+export declare const QRCode: (props: IQRCodeProps) => JSX_2.Element;
+
+export declare const Radio: {
+    (props: IRadioProps): JSX_2.Element;
+    Group: MemoExoticComponent<ForwardRefExoticComponent<RadioGroupProps & RefAttributes<HTMLDivElement>>>;
+    Button: ForwardRefExoticComponent<RadioButtonProps & RefAttributes<CheckboxRef>>;
+};
+
+export declare const Rate: (props: IRateProps) => JSX_2.Element;
+
+export declare const Result: (props: IResultProps) => JSX_2.Element;
+
+export { Row }
+
+export declare const Segmented: (props: ISegmentedProps) => JSX_2.Element;
+
+export declare const Select: (props: ISelectProps) => JSX_2.Element;
+
+export declare const Skeleton: (props: ISkeletonProps) => JSX_2.Element;
+
+export declare const Slider: (props: ISliderProps) => JSX_2.Element;
+
+export declare const Space: (props: ISpaceProps) => JSX_2.Element;
+
+export declare const Spin: (props: ISpinProps) => JSX_2.Element;
+
+export declare const Statistic: (props: IStatisticProps) => JSX_2.Element;
+
+export declare const Steps: (props: IStepsProps) => JSX_2.Element;
+
+export declare const Switch: (props: ISwitchProps) => JSX_2.Element;
+
+export declare const Table: (props: ITableProps) => JSX_2.Element;
+
+export declare const Tabs: (props: ITabsProps) => JSX_2.Element;
+
+export declare const Tag: (props: ITagProps) => JSX_2.Element;
+
+export declare const Timeline: (props: ITimelineProps) => JSX_2.Element;
+
+export declare const TimePicker: (props: ITimePickerProps) => JSX_2.Element;
+
+export declare const Tooltip: (props: ITooltipProps) => JSX_2.Element;
+
+export declare const Tour: (props: ITourProps) => JSX_2.Element;
+
+export declare const Transfer: (props: ITransferProps) => JSX_2.Element;
+
+export declare const Tree: (props: ITreeProps) => JSX_2.Element;
+
+export declare const TreeSelect: (props: ITreeSelectProps) => JSX_2.Element;
+
+export declare const Upload: (props: IUploadProps) => JSX_2.Element;
+
+export declare const Watermark: (props: IWatermarkProps) => JSX_2.Element;
+
+export { }
