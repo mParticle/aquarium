fa05812ad1619a5107eafd5378e2d33e4578280c
jdickman <jdickman@mparticle.com>
2024-01-29 15:20:22 +0000

    fix tooltip/popover sometimes not showing

--- src/components/data-display/Tooltip/Tooltip.tsx
+++ src/components/data-display/Tooltip/Tooltip.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { Tooltip as AntTooltip } from "antd";
-import { type TooltipPropsWithTitle as AntTooltipPropsWithTitle } from "antd/es/tooltip";
-import { ConfigProvider } from "src/components";
+import React from 'react'
+import { Tooltip as AntTooltip } from 'antd'
+import { type TooltipPropsWithTitle as AntTooltipPropsWithTitle } from 'antd/es/tooltip'
+import { ConfigProvider } from 'src/components'
 
 export interface ITooltipProps extends AntTooltipPropsWithTitle {}
 
 export const Tooltip = (props: ITooltipProps) => {
   return (
     <ConfigProvider>
-      <AntTooltip {...props} />
+      <AntTooltip {...props} >
+        {/* Fragment fixes tooltip sometimes not showing */}
+        {/*https://github.com/ant-design/ant-design/issues/15909*/}
+        <>{props.children}</>
+      </AntTooltip>
     </ConfigProvider>
   )
 }
--- src/components/navigation/GlobalNavigation/NavigationItem.tsx
+++ src/components/navigation/GlobalNavigation/NavigationItem.tsx
@@ -1,6 +1,7 @@
 import type { IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import { NavigationIcon } from 'src/components/navigation/GlobalNavigation/NavigationIcon'
 import { NavigationList } from 'src/components/navigation/GlobalNavigation/NavigationList'
+import { Tooltip } from 'src/components'
 
 interface INavigationIconProps {
   type: 'link' | 'menu'
@@ -16,12 +17,14 @@ export function NavigationItem(props: INavigationIconProps) {
     <>
       {props.type === 'link' && (
         <>
-          <NavigationIcon
-            icon={props.icon}
-            label={props.label}
-            onClick={props.onClick}
-            className="globalNavigation__item"
-          />
+          <Tooltip title={props.label} placement="right">
+            <NavigationIcon
+              className="globalNavigation__item"
+              icon={props.icon}
+              label={props.label}
+              onClick={props.onClick}
+            />
+          </Tooltip>
         </>
       )}
 
