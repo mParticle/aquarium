f9e59ce07aba36ef76142672fe4d63f06f2c8dbe
jdickman <jdickman@mparticle.com>
2024-02-21 11:29:32 -0500

    use latest styles from design/figma

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,21 +1,21 @@
-import { getInitials } from "./utils";
+import { getInitials } from './utils'
 
 describe('Testing getInitials', () => {
-    it.each([
-        ["Ren & Stimpy", "RS"],
-        ["Ren & Stimpy-Wein", "RS"],
-        ["Ren & Stimpy- Wein", "RW"],
-        ["Ren & Stimpy - Wein", "RW"],
-        ["R. N. Stimpy", "RS"],
-        ["1Ren Stimpy", "RS"],
-        ["Ren", "R"],
-        ["* Ren & Stimpy", "RS"],
-        ["234* Ren & Stimpy", "RS"],
-    ])('it should take \"%s\" and return \"%s\"', (words, expectedInitials) => {
-        // act
-        const actualInitials = getInitials(words);
+  it.each([
+    ['Ren & Stimpy', 'RS'],
+    ['Ren & Stimpy-Wein', 'RS'],
+    ['Ren & Stimpy- Wein', 'RW'],
+    ['Ren & Stimpy - Wein', 'RW'],
+    ['R. N. Stimpy', 'RS'],
+    ['1Ren Stimpy', 'RS'],
+    ['Ren', 'R'],
+    ['* Ren & Stimpy', 'RS'],
+    ['234* Ren & Stimpy', 'RS'],
+  ])('it should take "%s" and return "%s"', (words, expectedInitials) => {
+    // act
+    const actualInitials = getInitials(words)
 
-        // assert
-        expect(actualInitials).toBe(expectedInitials);
-    });
-});
+    // assert
+    expect(actualInitials).toBe(expectedInitials)
+  })
+})
