155b0fe4574e3dd769761b102ff941d327c566ee
Gabriel Tiburcio <v-gtiburcio@mparticle.com>
2024-03-01 18:58:59 -0300

    Allows our Input to be used with Refs the same way as native Antd Input does it

--- src/components/data-entry/Input/Input.tsx
+++ src/components/data-entry/Input/Input.tsx
@@ -2,20 +2,19 @@ import { Input as AntInput } from 'antd'
 import { type InputProps as AntInputProps } from 'antd'
 import { ConfigProvider } from 'src/components'
 import { type InputRef } from 'antd'
+import { forwardRef, type Ref } from 'react'
 
 export interface IInputProps extends AntInputProps {}
 
 export { type InputRef }
 
-export const Input = (props: IInputProps) => {
+const Input = (props: IInputProps, ref: Ref<InputRef>) => {
   return (
     <ConfigProvider>
-      <AntInput {...props} />
+      <AntInput {...props} ref={ref} />
     </ConfigProvider>
   )
 }
 
-Input.Group = AntInput.Group
-Input.Password = AntInput.Password
-Input.Search = AntInput.Search
-Input.TextArea = AntInput.TextArea
+const InputWithRef = forwardRef(Input)
+export { InputWithRef as InternalInput }
