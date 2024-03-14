786f7afda140375098bbd993ae93ddf4249a94b8
jared-dickman <zenador15@gmail.com>
2024-03-14 14:10:23 -0600

    Fix: add check next to active workspace (#139)

--- src/components/navigation/GlobalNavigation/WorkspaceSelector/WorkspaceSelector.tsx
+++ src/components/navigation/GlobalNavigation/WorkspaceSelector/WorkspaceSelector.tsx
@@ -9,6 +9,8 @@ import {
   type IWorkspaceSelectorDisplayItem,
   Popover,
 } from 'src/components'
+import { Flex } from 'src/components'
+import { CheckIcon } from 'src/components'
 import React, { type ChangeEvent, useRef, useState } from 'react'
 import { useCallback } from 'react'
 import { useEffect } from 'react'
@@ -184,7 +186,12 @@ export function WorkspaceSelector(props: IWorkspaceSelectorProps) {
             className:
               'workspaceSelector__workspaceName' +
               (workspace.isActive ? ' workspaceSelector__workspaceName--active' : ''),
-            label: workspace.label,
+            label: (
+              <Flex justify="space-between" align="center">
+                {workspace.label}
+                {workspace.isActive && <CheckIcon className="workspaceSelector__activeIcon" />}
+              </Flex>
+            ),
             id: workspace.id,
             key: `${workspace.id}_${workspace.label}`,
             onClick: workspace.onClick,
