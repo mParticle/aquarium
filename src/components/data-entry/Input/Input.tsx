20a7e4709c6a77c5e197a449c6ad715e8b5c1a80
Gabriel Tibúrcio <v-gtiburcio@mparticle.com>
2024-03-25 11:37:45 -0300

    fix: Input now support refs (#169)

--- src/components/data-entry/Input/Input.tsx
+++ src/components/data-entry/Input/Input.tsx
@@ -2,18 +2,29 @@ import { Input as AntInput } from 'antd'
 import { type InputProps as AntInputProps } from 'antd'
 import { ConfigProvider } from 'src/components'
 import { type InputRef } from 'antd'
+import { forwardRef, type ForwardRefExoticComponent, type Ref, type RefAttributes } from 'react'
 
-export interface IInputProps extends AntInputProps {}
+export { Input, type InputRef, type IInputProps }
 
-export { type InputRef }
+interface IInputProps extends AntInputProps {}
 
-export const Input = (props: IInputProps) => {
+type CompoundedComponent = ForwardRefExoticComponent<IInputProps & RefAttributes<InputRef>> & {
+  Group: typeof AntInput.Group
+  Search: typeof AntInput.Search
+  TextArea: typeof AntInput.TextArea
+  Password: typeof AntInput.Password
+}
+
+const InternalInput = (props: IInputProps, ref: Ref<InputRef>) => {
   return (
     <ConfigProvider>
-      <AntInput {...props} />
+      <AntInput {...props} ref={ref} />
     </ConfigProvider>
   )
 }
+
+const InternalInputWithRef = forwardRef(InternalInput)
+const Input = InternalInputWithRef as CompoundedComponent
 
 Input.Group = AntInput.Group
 Input.Password = AntInput.Password
