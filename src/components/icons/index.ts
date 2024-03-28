f875406822c3a4fdcdd9a46345b4ca530669a8de
Gary Kaganas <gkaganas@mparticle.com>
2024-03-28 12:23:16 -0400

    Adds new icons to list and re-arranges list alphabetically

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,53 +1,75 @@
-import ChartLineIcon from 'src/assets/svg/chart-line.svg?react'
-import ConnectionsIcon from 'src/assets/svg/connections.svg?react'
-import DatabaseIcon from 'src/assets/svg/database.svg?react'
-import GridIcon from 'src/assets/svg/grid.svg?react'
-import CheckIcon from 'src/assets/svg/check.svg?react'
-import ShieldKeyholeIcon from 'src/assets/svg/shield-keyhole.svg?react'
-import UsersIcon from 'src/assets/svg/users.svg?react'
-import WrenchIcon from 'src/assets/svg/wrench.svg?react'
-import AlicornIcon from 'src/assets/svg/alicorn.svg?react'
-import MessageQuestionIcon from 'src/assets/svg/message-question.svg?react'
-import GearIcon from 'src/assets/svg/gear.svg?react'
-import SparklesIcon from 'src/assets/svg/sparkles.svg?react'
-import SiteMapIcon from 'src/assets/svg/sitemap.svg?react'
-import ChartColumnIcon from 'src/assets/svg/chart-column.svg?react'
-import HeartIcon from 'src/assets/svg/heart.svg?react'
-import LightBulbIcon from 'src/assets/svg/lightbulb.svg?react'
-import SplitIcon from 'src/assets/svg/split.svg?react'
-import CircleNodesIcon from 'src/assets/svg/circle-nodes.svg?react'
-import CloudIcon from 'src/assets/svg/cloud.svg?react'
-import FolderClosedIcon from 'src/assets/svg/folder-closed.svg?react'
-import AddIcon from 'src/assets/svg/add.svg?react'
-import MpLogoIcon from 'src/assets/svg/mpLogo.svg?react'
-import LockIcon from 'src/assets/svg/lock.svg?react'
-import SearchIcon from 'src/assets/svg/search.svg?react'
-import AnalyticsIcon from 'src/assets/svg/analytics.svg?react'
+import AddIcon from 'src/assets/svg/add.svg?react';
+import AlicornIcon from 'src/assets/svg/alicorn.svg?react';
+import AnalyticsIcon from 'src/assets/svg/analytics.svg?react';
+import ArrowRightFromLine from "src/assets/svg/arrow-right-from-line.svg?react";
+import BookOpen from "src/assets/svg/book-open.svg?react";
+import ChartColumnIcon from 'src/assets/svg/chart-column.svg?react';
+import ChartLineIcon from 'src/assets/svg/chart-line.svg?react';
+import CheckIcon from 'src/assets/svg/check.svg?react';
+import CircleNodesIcon from 'src/assets/svg/circle-nodes.svg?react';
+import CloudIcon from 'src/assets/svg/cloud.svg?react';
+import ConnectionsIcon from 'src/assets/svg/connections.svg?react';
+import DatabaseIcon from 'src/assets/svg/database.svg?react';
+import Ellipsis from "src/assets/svg/ellipsis.svg?react";
+import Fingerprint from "src/assets/svg/fingerprint.svg?react";
+import FolderClosedIcon from 'src/assets/svg/folder-closed.svg?react';
+import GearIcon from 'src/assets/svg/gear.svg?react';
+import GridIcon from 'src/assets/svg/grid.svg?react';
+import HeartIcon from 'src/assets/svg/heart.svg?react';
+import LightBulbIcon from 'src/assets/svg/lightbulb.svg?react';
+import LockIcon from 'src/assets/svg/lock.svg?react';
+import MagnifyingGlassWaveform from "src/assets/svg/magnifying-glass-waveform.svg?react";
+import MessageQuestionIcon from 'src/assets/svg/message-question.svg?react';
+import MpLogoIcon from 'src/assets/svg/mpLogo.svg?react';
+import SearchIcon from 'src/assets/svg/search.svg?react';
+import ShieldKeyhole from "src/assets/svg/shield-keyhole.svg?react";
+import ShieldKeyholeIcon from 'src/assets/svg/shield-keyhole.svg?react';
+import SiteMapIcon from 'src/assets/svg/sitemap.svg?react';
+import Sliders from "src/assets/svg/sliders.svg?react";
+import SparklesIcon from 'src/assets/svg/sparkles.svg?react';
+import Split from "src/assets/svg/split.svg?react";
+import SplitIcon from 'src/assets/svg/split.svg?react';
+import SquareUser from "src/assets/svg/square-user.svg?react";
+import TriangleExclamation from "src/assets/svg/triangle-exclamation.svg?react";
+import UserGear from "src/assets/svg/user-gear.svg?react";
+import UsersIcon from 'src/assets/svg/users.svg?react';
+import WrenchIcon from 'src/assets/svg/wrench.svg?react';
 
 export {
   AddIcon,
-  LockIcon,
-  MpLogoIcon,
-  SearchIcon,
-  CheckIcon,
-  ChartLineIcon,
-  ConnectionsIcon,
-  DatabaseIcon,
-  GridIcon,
-  ShieldKeyholeIcon,
-  UsersIcon,
-  WrenchIcon,
   AlicornIcon,
-  MessageQuestionIcon,
-  GearIcon,
-  SparklesIcon,
-  SiteMapIcon,
+  AnalyticsIcon,
+  ArrowRightFromLine,
+  BookOpen,
   ChartColumnIcon,
-  HeartIcon,
-  LightBulbIcon,
-  SplitIcon,
+  ChartLineIcon,
+  CheckIcon,
   CircleNodesIcon,
   CloudIcon,
+  ConnectionsIcon,
+  DatabaseIcon,
+  Ellipsis,
+  Fingerprint,
   FolderClosedIcon,
-  AnalyticsIcon,
+  GearIcon,
+  GridIcon,
+  HeartIcon,
+  LightBulbIcon,
+  LockIcon,
+  MagnifyingGlassWaveform,
+  MessageQuestionIcon,
+  MpLogoIcon,
+  SearchIcon,
+  ShieldKeyhole,
+  ShieldKeyholeIcon,
+  SiteMapIcon,
+  Sliders,
+  SparklesIcon,
+  Split,
+  SplitIcon,
+  SquareUser,
+  TriangleExclamation,
+  UserGear,
+  UsersIcon,
+  WrenchIcon,
 }
