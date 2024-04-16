0a59396a1358c1a8defaca81b6d08b0ca598511c
nrmerlis <nmerlis@mparticle.com>
2024-04-16 19:04:23 -0300

    - Solving merge conflicts!

--- src/components/data-entry/QueryItem/QueryItem.tsx
+++ src/components/data-entry/QueryItem/QueryItem.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Action from './Action'
 import { Qualifier } from './Qualifier'
 import { Text } from './Text'
+import { TextInput } from 'src/components/data-entry/QueryItem/TextInput'
 
 export const QueryItem = () => {
   return <>DO NOT USE THIS OR YOU WILL BE FIRED!</>
@@ -9,3 +10,4 @@ export const QueryItem = () => {
 QueryItem.Action = Action
 QueryItem.Qualifier = Qualifier
 QueryItem.Text = Text
+QueryItem.TextInput = TextInput
