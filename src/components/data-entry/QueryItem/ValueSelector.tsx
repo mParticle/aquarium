a82b5a725b0309c8c506d994ea8ad92b661b101c
Zoe Hayes <zoehayes286@gmail.com>
2024-04-19 17:05:39 -0400

    Revert "feat: adding QueryItem.ValueSelector.InputText "

--- src/components/data-entry/QueryItem/ValueSelector.tsx
+++ src/components/data-entry/QueryItem/ValueSelector.tsx
@@ -1,7 +1,7 @@
-import { TextInput } from 'src/components/data-entry/QueryItem/TextInput'
+import React from 'react'
+import { Cascader } from './Cascader'
 
 export const ValueSelector = () => {
   return <>DON'T USE THIS OR YOU WILL BE FIRED!</>
 }
-
-ValueSelector.TextInput = TextInput
+ValueSelector.Cascader = Cascader
