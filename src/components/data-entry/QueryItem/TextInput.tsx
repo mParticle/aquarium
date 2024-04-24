7a1532fa2a8d10152cb947a1079da7111abdab96
Zoe Hayes <zoehayes286@gmail.com>
2024-04-24 11:24:42 -0400

    fix: make onChange prop of QueryItem InputText optional

--- src/components/data-entry/QueryItem/TextInput.tsx
+++ src/components/data-entry/QueryItem/TextInput.tsx
@@ -4,7 +4,7 @@ import { Input } from 'src/components'
 import { Typography } from 'src/components/general/Typography/Typography'
 
 interface ITextInputProps {
-  onChange: (value: string) => void
+  onChange?: (value: string) => void
   value?: string
   disabled?: boolean
   errorMessage?: string
@@ -14,7 +14,8 @@ export const TextInput = (props: ITextInputProps) => {
   const isErrorStatus = props.errorMessage && !props.disabled
 
   const _onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    props.onChange(e.target.value)
+    if(props.onChange)
+      props.onChange(e.target.value)
   }
 
   let inputClasses = `query-item query-item--input-text`
