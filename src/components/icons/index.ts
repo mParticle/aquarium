00e3b86136ce571c88c21c996d3782931363e6df
Gaby Zifferman <gabyzif@gmail.com>
2024-05-20 19:02:22 +0200

    add suites icons and order them alphabetically

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -4,58 +4,96 @@ import DatabaseIcon from 'src/assets/svg/database.svg?react'
 import GridIcon from 'src/assets/svg/grid.svg?react'
 import CheckIcon from 'src/assets/svg/check.svg?react'
 import ShieldKeyholeIcon from 'src/assets/svg/shield-keyhole.svg?react'
-import UsersIcon from 'src/assets/svg/users.svg?react'
-import WrenchIcon from 'src/assets/svg/wrench.svg?react'
+import AddIcon from 'src/assets/svg/add.svg?react'
 import AlicornIcon from 'src/assets/svg/alicorn.svg?react'
-import MessageQuestionIcon from 'src/assets/svg/message-question.svg?react'
-import GearIcon from 'src/assets/svg/gear.svg?react'
-import SparklesIcon from 'src/assets/svg/sparkles.svg?react'
-import SiteMapIcon from 'src/assets/svg/sitemap.svg?react'
+import AnalyticsIcon from 'src/assets/svg/analytics.svg?react'
+import AudiencesIcon from 'src/assets/svg/mp_pm_lt_audiences.svg?react'
+import CatalogIcon from 'src/assets/svg/mp_pm_lt_catalog.svg?react'
 import ChartColumnIcon from 'src/assets/svg/chart-column.svg?react'
-import HeartIcon from 'src/assets/svg/heart.svg?react'
-import LightBulbIcon from 'src/assets/svg/lightbulb.svg?react'
-import SplitIcon from 'src/assets/svg/split.svg?react'
 import CircleNodesIcon from 'src/assets/svg/circle-nodes.svg?react'
 import CloudIcon from 'src/assets/svg/cloud.svg?react'
-import FolderClosedIcon from 'src/assets/svg/folder-closed.svg?react'
-import AddIcon from 'src/assets/svg/add.svg?react'
-import MpLogoIcon from 'src/assets/svg/mpLogo.svg?react'
-import LockIcon from 'src/assets/svg/lock.svg?react'
-import SearchIcon from 'src/assets/svg/search.svg?react'
-import AnalyticsIcon from 'src/assets/svg/analytics.svg?react'
+import DsrIcon from 'src/assets/svg/mp_pm_lt_dsr.svg?react'
 import EmptyIcon from 'src/assets/svg/empty.svg?react'
+import EnrichmentIcon from 'src/assets/svg/mp_pm_lt_enrichment.svg?react'
 import EventAttributeIcon from 'src/assets/svg/event-attribute.svg?react'
-import UserAttributeIcon from 'src/assets/svg/user-attribute.svg?react'
 import EventIcon from 'src/assets/svg/event.svg?react'
+import FolderClosedIcon from 'src/assets/svg/folder-closed.svg?react'
+import ForwardingIcon from 'src/assets/svg/mp_pm_lt_forwarding.svg?react'
+import GearIcon from 'src/assets/svg/gear.svg?react'
+import HeartIcon from 'src/assets/svg/heart.svg?react'
+import HelpIcon from 'src/assets/svg/help.svg?react'
+import IdentityIcon from 'src/assets/svg/mp_pm_lt_identity.svg?react'
+import JourneysIcon from 'src/assets/svg/mp_pm_lt_journeys.svg?react'
+import JumpToIcon from 'src/assets/svg/mp_act_lt_jump-to.svg?react'
+import LightBulbIcon from 'src/assets/svg/lightbulb.svg?react'
+import LiveStreamIcon from 'src/assets/svg/mp_pm_lt_live-stream.svg?react'
+import LockIcon from 'src/assets/svg/lock.svg?react'
+import MessageQuestionIcon from 'src/assets/svg/message-question.svg?react'
+import MpLogoIcon from 'src/assets/svg/mpLogo.svg?react'
+import ObservabilityIcon from 'src/assets/svg/mp_pm_lt_observability.svg?react'
+import PrivacyIcon from 'src/assets/svg/mp_pm_lt_privacy.svg?react'
+import RemoveIcon from 'src/assets/svg/remove.svg?react'
+import SearchIcon from 'src/assets/svg/search.svg?react'
+import SetupIcon from 'src/assets/svg/mp_pm_lt_setup.svg?react'
+import SignoutIcon from 'src/assets/svg/signout.svg?react'
+import SiteMapIcon from 'src/assets/svg/sitemap.svg?react'
+import SparklesIcon from 'src/assets/svg/sparkles.svg?react'
+import SplitIcon from 'src/assets/svg/split.svg?react'
+import SystemAlertsIcon from 'src/assets/svg/mp_pm_lt_system-alerts.svg?react'
+import TransformationsIcon from 'src/assets/svg/mp_pm_lt_transformations.svg?react'
+import TrendsIcon from 'src/assets/svg/mp_pm_lt_trends.svg?react'
+import UserAttributeIcon from 'src/assets/svg/user-attribute.svg?react'
+import UsersIcon from 'src/assets/svg/users.svg?react'
+import UserProfileIcon from 'src/assets/svg/mp_pm_lt_user.svg?react'
+import WrenchIcon from 'src/assets/svg/wrench.svg?react'
 
 export {
   AddIcon,
-  LockIcon,
-  MpLogoIcon,
-  SearchIcon,
-  CheckIcon,
-  ChartLineIcon,
-  ConnectionsIcon,
-  DatabaseIcon,
-  GridIcon,
-  ShieldKeyholeIcon,
-  UsersIcon,
-  WrenchIcon,
   AlicornIcon,
-  MessageQuestionIcon,
-  GearIcon,
-  SparklesIcon,
-  SiteMapIcon,
+  AnalyticsIcon,
+  AudiencesIcon,
+  CatalogIcon,
   ChartColumnIcon,
-  HeartIcon,
-  LightBulbIcon,
-  SplitIcon,
+  ChartLineIcon,
+  CheckIcon,
   CircleNodesIcon,
   CloudIcon,
-  FolderClosedIcon,
-  AnalyticsIcon,
+  ConnectionsIcon,
+  DatabaseIcon,
+  DsrIcon,
   EmptyIcon,
+  EnrichmentIcon,
   EventAttributeIcon,
-  UserAttributeIcon,
   EventIcon,
+  FolderClosedIcon,
+  ForwardingIcon,
+  GearIcon,
+  GridIcon,
+  HeartIcon,
+  HelpIcon,
+  IdentityIcon,
+  JourneysIcon,
+  JumpToIcon,
+  LightBulbIcon,
+  LiveStreamIcon,
+  LockIcon,
+  MessageQuestionIcon,
+  MpLogoIcon,
+  ObservabilityIcon,
+  PrivacyIcon,
+  RemoveIcon,
+  SearchIcon,
+  SetupIcon,
+  ShieldKeyholeIcon,
+  SignoutIcon,
+  SiteMapIcon,
+  SparklesIcon,
+  SplitIcon,
+  SystemAlertsIcon,
+  TransformationsIcon,
+  TrendsIcon,
+  UserAttributeIcon,
+  UsersIcon,
+  UserProfileIcon,
+  WrenchIcon,
 }
--- src/constants/Icons.ts
+++ src/constants/Icons.ts
@@ -1,67 +1,101 @@
-import ChartLineIcon from 'src/assets/svg/chart-line.svg?react'
-import ConnectionsIcon from 'src/assets/svg/connections.svg?react'
-import DatabaseIcon from 'src/assets/svg/database.svg?react'
-import GridIcon from 'src/assets/svg/grid.svg?react'
-import CheckIcon from 'src/assets/svg/check.svg?react'
-import ShieldKeyholeIcon from 'src/assets/svg/shield-keyhole.svg?react'
-import UsersIcon from 'src/assets/svg/users.svg?react'
-import WrenchIcon from 'src/assets/svg/wrench.svg?react'
-import AlicornIcon from 'src/assets/svg/alicorn.svg?react'
-import MessageQuestionIcon from 'src/assets/svg/message-question.svg?react'
-import GearIcon from 'src/assets/svg/gear.svg?react'
-import SparklesIcon from 'src/assets/svg/sparkles.svg?react'
-import SiteMapIcon from 'src/assets/svg/sitemap.svg?react'
-import ChartColumnIcon from 'src/assets/svg/chart-column.svg?react'
-import HeartIcon from 'src/assets/svg/heart.svg?react'
-import LightBulbIcon from 'src/assets/svg/lightbulb.svg?react'
-import SplitIcon from 'src/assets/svg/split.svg?react'
-import CircleNodesIcon from 'src/assets/svg/circle-nodes.svg?react'
-import CloudIcon from 'src/assets/svg/cloud.svg?react'
-import FolderClosedIcon from 'src/assets/svg/folder-closed.svg?react'
-import AddIcon from 'src/assets/svg/add.svg?react'
-import MpLogoIcon from 'src/assets/svg/mpLogo.svg?react'
-import SignoutIcon from 'src/assets/svg/signout.svg?react'
-import LockIcon from 'src/assets/svg/lock.svg?react'
-import SearchIcon from 'src/assets/svg/search.svg?react'
-import AnalyticsIcon from 'src/assets/svg/analytics.svg?react'
-import EventIcon from 'src/assets/svg/event.svg?react'
-import EventAttributeIcon from 'src/assets/svg/event-attribute.svg?react'
-import UserAttributeIcon from 'src/assets/svg/user-attribute.svg?react'
-import EmptyIcon from 'src/assets/svg/empty.svg?react'
-import RemoveIcon from 'src/assets/svg/remove.svg?react'
-import HelpIcon from 'src/assets/svg/help.svg?react'
+import {
+  AddIcon,
+  AlicornIcon,
+  AnalyticsIcon,
+  AudiencesIcon,
+  CatalogIcon,
+  ChartColumnIcon,
+  ChartLineIcon,
+  CheckIcon,
+  CircleNodesIcon,
+  CloudIcon,
+  ConnectionsIcon,
+  DatabaseIcon,
+  DsrIcon,
+  EmptyIcon,
+  EnrichmentIcon,
+  EventAttributeIcon,
+  EventIcon,
+  FolderClosedIcon,
+  ForwardingIcon,
+  GearIcon,
+  GridIcon,
+  HeartIcon,
+  HelpIcon,
+  IdentityIcon,
+  JourneysIcon,
+  JumpToIcon,
+  LightBulbIcon,
+  LiveStreamIcon,
+  LockIcon,
+  MessageQuestionIcon,
+  MpLogoIcon,
+  ObservabilityIcon,
+  PrivacyIcon,
+  RemoveIcon,
+  SearchIcon,
+  SetupIcon,
+  ShieldKeyholeIcon,
+  SignoutIcon,
+  SiteMapIcon,
+  SparklesIcon,
+  SplitIcon,
+  SystemAlertsIcon,
+  TransformationsIcon,
+  TrendsIcon,
+  UserAttributeIcon,
+  UserProfileIcon,
+  UsersIcon,
+  WrenchIcon,
+} from 'src/components/icons'
 
 export const Icons = {
-  alicorn: AlicornIcon,
   add: AddIcon,
-  lock: LockIcon,
-  mpLogo: MpLogoIcon,
-  search: SearchIcon,
-  check: CheckIcon,
-  chartLine: ChartLineIcon,
-  connections: ConnectionsIcon,
-  database: DatabaseIcon,
-  grid: GridIcon,
-  shieldKeyhole: ShieldKeyholeIcon,
-  users: UsersIcon,
-  wrench: WrenchIcon,
-  messageQuestion: MessageQuestionIcon,
-  gear: GearIcon,
-  sparkles: SparklesIcon,
-  siteMap: SiteMapIcon,
+  alicorn: AlicornIcon,
+  analytics: AnalyticsIcon,
+  audiences: AudiencesIcon,
+  catalog: CatalogIcon,
   chartColumn: ChartColumnIcon,
-  heart: HeartIcon,
-  lightBulb: LightBulbIcon,
-  split: SplitIcon,
-  signout: SignoutIcon,
+  chartLine: ChartLineIcon,
+  check: CheckIcon,
   circleNodes: CircleNodesIcon,
   cloud: CloudIcon,
-  folderClosed: FolderClosedIcon,
-  analytics: AnalyticsIcon,
+  connections: ConnectionsIcon,
+  database: DatabaseIcon,
+  dsr: DsrIcon,
   empty: EmptyIcon,
-  eventAttribute: EventAttributeIcon,
+  enrichment: EnrichmentIcon,
   event: EventIcon,
-  userAttribute: UserAttributeIcon,
-  remove: RemoveIcon,
+  eventAttribute: EventAttributeIcon,
+  folderClosed: FolderClosedIcon,
+  forwarding: ForwardingIcon,
+  gear: GearIcon,
+  grid: GridIcon,
+  heart: HeartIcon,
   help: HelpIcon,
+  identity: IdentityIcon,
+  journeys: JourneysIcon,
+  jumpTo: JumpToIcon,
+  lightBulb: LightBulbIcon,
+  liveStream: LiveStreamIcon,
+  lock: LockIcon,
+  messageQuestion: MessageQuestionIcon,
+  mpLogo: MpLogoIcon,
+  observability: ObservabilityIcon,
+  privacy: PrivacyIcon,
+  remove: RemoveIcon,
+  search: SearchIcon,
+  setUp: SetupIcon,
+  shieldKeyhole: ShieldKeyholeIcon,
+  signout: SignoutIcon,
+  siteMap: SiteMapIcon,
+  sparkles: SparklesIcon,
+  split: SplitIcon,
+  systemAlerts: SystemAlertsIcon,
+  transformation: TransformationsIcon,
+  trends: TrendsIcon,
+  userAttribute: UserAttributeIcon,
+  userProfile: UserProfileIcon,
+  users: UsersIcon,
+  wrench: WrenchIcon,
 } as const
