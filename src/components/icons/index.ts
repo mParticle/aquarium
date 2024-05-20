df63364d70d5e87a1749acd72ba6bc4c2e5bf5f0
Gaby Zifferman <gabyzif@gmail.com>
2024-05-20 19:35:04 +0200

    remove duplicated icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -7,7 +7,6 @@ import ShieldKeyholeIcon from 'src/assets/svg/shield-keyhole.svg?react'
 import AddIcon from 'src/assets/svg/add.svg?react'
 import AlicornIcon from 'src/assets/svg/alicorn.svg?react'
 import AnalyticsIcon from 'src/assets/svg/analytics.svg?react'
-import AudiencesIcon from 'src/assets/svg/mp_pm_lt_audiences.svg?react'
 import CatalogIcon from 'src/assets/svg/mp_pm_lt_catalog.svg?react'
 import ChartColumnIcon from 'src/assets/svg/chart-column.svg?react'
 import CircleNodesIcon from 'src/assets/svg/circle-nodes.svg?react'
@@ -23,7 +22,6 @@ import GearIcon from 'src/assets/svg/gear.svg?react'
 import HeartIcon from 'src/assets/svg/heart.svg?react'
 import HelpIcon from 'src/assets/svg/help.svg?react'
 import IdentityIcon from 'src/assets/svg/mp_pm_lt_identity.svg?react'
-import JourneysIcon from 'src/assets/svg/mp_pm_lt_journeys.svg?react'
 import JumpToIcon from 'src/assets/svg/mp_act_lt_jump-to.svg?react'
 import LightBulbIcon from 'src/assets/svg/lightbulb.svg?react'
 import LiveStreamIcon from 'src/assets/svg/mp_pm_lt_live-stream.svg?react'
@@ -31,9 +29,7 @@ import LockIcon from 'src/assets/svg/lock.svg?react'
 import MessageQuestionIcon from 'src/assets/svg/message-question.svg?react'
 import MpLogoIcon from 'src/assets/svg/mpLogo.svg?react'
 import ObservabilityIcon from 'src/assets/svg/mp_pm_lt_observability.svg?react'
-import PrivacyIcon from 'src/assets/svg/mp_pm_lt_privacy.svg?react'
 import RemoveIcon from 'src/assets/svg/remove.svg?react'
-import SearchIcon from 'src/assets/svg/search.svg?react'
 import SetupIcon from 'src/assets/svg/mp_pm_lt_setup.svg?react'
 import SignoutIcon from 'src/assets/svg/signout.svg?react'
 import SiteMapIcon from 'src/assets/svg/sitemap.svg?react'
@@ -41,7 +37,6 @@ import SparklesIcon from 'src/assets/svg/sparkles.svg?react'
 import SplitIcon from 'src/assets/svg/split.svg?react'
 import SystemAlertsIcon from 'src/assets/svg/mp_pm_lt_system-alerts.svg?react'
 import TransformationsIcon from 'src/assets/svg/mp_pm_lt_transformations.svg?react'
-import TrendsIcon from 'src/assets/svg/mp_pm_lt_trends.svg?react'
 import UserAttributeIcon from 'src/assets/svg/user-attribute.svg?react'
 import UsersIcon from 'src/assets/svg/users.svg?react'
 import UserProfileIcon from 'src/assets/svg/mp_pm_lt_user.svg?react'
@@ -51,7 +46,6 @@ export {
   AddIcon,
   AlicornIcon,
   AnalyticsIcon,
-  AudiencesIcon,
   CatalogIcon,
   ChartColumnIcon,
   ChartLineIcon,
@@ -72,7 +66,6 @@ export {
   HeartIcon,
   HelpIcon,
   IdentityIcon,
-  JourneysIcon,
   JumpToIcon,
   LightBulbIcon,
   LiveStreamIcon,
@@ -80,9 +73,7 @@ export {
   MessageQuestionIcon,
   MpLogoIcon,
   ObservabilityIcon,
-  PrivacyIcon,
   RemoveIcon,
-  SearchIcon,
   SetupIcon,
   ShieldKeyholeIcon,
   SignoutIcon,
@@ -91,7 +82,6 @@ export {
   SplitIcon,
   SystemAlertsIcon,
   TransformationsIcon,
-  TrendsIcon,
   UserAttributeIcon,
   UsersIcon,
   UserProfileIcon,
