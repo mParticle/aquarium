946439e09140c4cd3a980436b1dc6fff2a209e2e
Gaby Zifferman <gabyzif@gmail.com>
2024-05-21 19:21:39 +0200

    add / replace extra icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,16 +1,16 @@
-import ChartLineIcon from 'src/assets/svg/chart-line.svg?react'
-import ConnectionsIcon from 'src/assets/svg/connections.svg?react'
-import DatabaseIcon from 'src/assets/svg/database.svg?react'
-import GridIcon from 'src/assets/svg/grid.svg?react'
-import CheckIcon from 'src/assets/svg/check.svg?react'
-import ShieldKeyholeIcon from 'src/assets/svg/shield-keyhole.svg?react'
 import AddIcon from 'src/assets/svg/add.svg?react'
 import AlicornIcon from 'src/assets/svg/alicorn.svg?react'
-import AnalyticsIcon from 'src/assets/svg/analytics.svg?react'
+import AnalyticsIcon from 'src/assets/svg/mp_pm_dt_analytics.svg?react'
+import C360Icon from 'src/assets/svg/mp_pm_dt_c360.svg?react'
 import CatalogIcon from 'src/assets/svg/mp_pm_lt_catalog.svg?react'
 import ChartColumnIcon from 'src/assets/svg/chart-column.svg?react'
+import ChartLineIcon from 'src/assets/svg/chart-line.svg?react'
+import CheckIcon from 'src/assets/svg/check.svg?react'
 import CircleNodesIcon from 'src/assets/svg/circle-nodes.svg?react'
 import CloudIcon from 'src/assets/svg/cloud.svg?react'
+import ConnectionsIcon from 'src/assets/svg/connections.svg?react'
+import DataPlatform from 'src/assets/svg/mp_pm_dt_data-platform.svg?react'
+import DatabaseIcon from 'src/assets/svg/database.svg?react'
 import DsrIcon from 'src/assets/svg/mp_pm_lt_dsr.svg?react'
 import EmptyIcon from 'src/assets/svg/empty.svg?react'
 import EnrichmentIcon from 'src/assets/svg/mp_pm_lt_enrichment.svg?react'
@@ -19,6 +19,7 @@ import EventIcon from 'src/assets/svg/event.svg?react'
 import FolderClosedIcon from 'src/assets/svg/folder-closed.svg?react'
 import ForwardingIcon from 'src/assets/svg/mp_pm_lt_forwarding.svg?react'
 import GearIcon from 'src/assets/svg/gear.svg?react'
+import GridIcon from 'src/assets/svg/grid.svg?react'
 import HeartIcon from 'src/assets/svg/heart.svg?react'
 import HelpIcon from 'src/assets/svg/help.svg?react'
 import IdentityIcon from 'src/assets/svg/mp_pm_lt_identity.svg?react'
@@ -29,23 +30,29 @@ import LockIcon from 'src/assets/svg/lock.svg?react'
 import MessageQuestionIcon from 'src/assets/svg/message-question.svg?react'
 import MpLogoIcon from 'src/assets/svg/mpLogo.svg?react'
 import ObservabilityIcon from 'src/assets/svg/mp_pm_lt_observability.svg?react'
+import OversightIcon from 'src/assets/svg/mp_pm_dt_oversight.svg?react'
+import PredictionsIcon from 'src/assets/svg/mp_pm_dt_predictions.svg?react'
 import RemoveIcon from 'src/assets/svg/remove.svg?react'
 import SearchIcon from 'src/assets/svg/search.svg?react'
+import SegmentationIcon from 'src/assets/svg/mp_pm_dt_segmentation.svg?react'
+import ShieldKeyholeIcon from 'src/assets/svg/shield-keyhole.svg?react'
 import SignoutIcon from 'src/assets/svg/signout.svg?react'
 import SiteMapIcon from 'src/assets/svg/sitemap.svg?react'
 import SparklesIcon from 'src/assets/svg/sparkles.svg?react'
 import SplitIcon from 'src/assets/svg/split.svg?react'
 import SystemAlertsIcon from 'src/assets/svg/mp_pm_lt_system-alerts.svg?react'
 import TransformationsIcon from 'src/assets/svg/mp_pm_lt_transformations.svg?react'
+import UploadIcon from 'src/assets/svg/mp_act_it_upload.svg?react'
 import UserAttributeIcon from 'src/assets/svg/user-attribute.svg?react'
+import UserProfilesIcon from 'src/assets/svg/mp_pm_lt_userprofiles.svg?react'
 import UsersIcon from 'src/assets/svg/users.svg?react'
-import UserProfileIcon from 'src/assets/svg/mp_pm_lt_user.svg?react'
 import WrenchIcon from 'src/assets/svg/wrench.svg?react'
 
 export {
   AddIcon,
   AlicornIcon,
   AnalyticsIcon,
+  C360Icon,
   CatalogIcon,
   ChartColumnIcon,
   ChartLineIcon,
@@ -53,6 +60,7 @@ export {
   CircleNodesIcon,
   CloudIcon,
   ConnectionsIcon,
+  DataPlatform,
   DatabaseIcon,
   DsrIcon,
   EmptyIcon,
@@ -73,17 +81,21 @@ export {
   MessageQuestionIcon,
   MpLogoIcon,
   ObservabilityIcon,
+  OversightIcon,
+  PredictionsIcon,
   RemoveIcon,
-  ShieldKeyholeIcon,
   SearchIcon,
+  SegmentationIcon,
+  ShieldKeyholeIcon,
   SignoutIcon,
   SiteMapIcon,
   SparklesIcon,
   SplitIcon,
   SystemAlertsIcon,
   TransformationsIcon,
+  UploadIcon,
   UserAttributeIcon,
+  UserProfilesIcon,
   UsersIcon,
-  UserProfileIcon,
   WrenchIcon,
 }
