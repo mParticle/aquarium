35fae010cb61dc6da635d5f3257154205baae9b9
Gaby Zifferman <gabyzif@gmail.com>
2024-05-23 21:47:13 +0200

    add utility classes

--- src/components/general/Button/Button.tsx
+++ src/components/general/Button/Button.tsx
@@ -1,14 +1,13 @@
 import { Button as AntButton } from 'antd'
 import { type ButtonProps as AntButtonProps } from 'antd'
 import { ConfigProvider } from 'src/components/other/ConfigProvider/ConfigProvider'
-import './button.css'
 
 export interface IButtonProps extends AntButtonProps {}
 
 export const Button = (props: IButtonProps) => {
   return (
     <ConfigProvider>
-      <AntButton {...props} className={props.icon ? 'aquarium-button--aligned-center' : ''}>
+      <AntButton {...props} className={props.icon ? 'u-display-flex u-align-items-center ' : ''}>
         {props.children}
       </AntButton>
     </ConfigProvider>
