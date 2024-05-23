ca0901da0c936dd53ab34a7db1a6946877803b4d
Gaby Zifferman <gabyzif@gmail.com>
2024-05-23 17:15:04 -0300

    fix: Align button content to the center (#243)
    Co-authored-by: jared-dickman <zenador15@gmail.com>

--- src/components/general/Button/Button.tsx
+++ src/components/general/Button/Button.tsx
@@ -7,7 +7,9 @@ export interface IButtonProps extends AntButtonProps {}
 export const Button = (props: IButtonProps) => {
   return (
     <ConfigProvider>
-      <AntButton {...props}>{props.children}</AntButton>
+      <AntButton {...props} className={props.icon ? 'u-display-flex u-align-items-center' : ''}>
+        {props.children}
+      </AntButton>
     </ConfigProvider>
   )
 }
