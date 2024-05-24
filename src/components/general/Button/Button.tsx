124fce2d2502f1349bcb56f59a59d2fd1f1bbefa
Gaby Zifferman <gabyzif@gmail.com>
2024-05-24 18:47:26 +0200

    remove variants

--- src/components/general/Button/Button.tsx
+++ src/components/general/Button/Button.tsx
@@ -2,16 +2,11 @@ import { Button as AntButton } from 'antd'
 import { type ButtonProps as AntButtonProps } from 'antd'
 import { ConfigProvider } from 'src/components/other/ConfigProvider/ConfigProvider'
 
-export interface IButtonProps extends AntButtonProps {
-  variant?: 'content-align-center'
-}
+export interface IButtonProps extends AntButtonProps {}
 export const Button = (props: IButtonProps) => {
-  const classMap = {
-    'content-align-center': 'u-display-flex u-align-items-center',
-  }
   return (
     <ConfigProvider>
-      <AntButton {...props} className={`${props.className} ${props.variant ? classMap[props.variant] : ''}`}>
+      <AntButton {...props} className={`${props.className} ${props.icon ? 'u-display-flex u-align-items-center' : ''}`}>
         {props.children}
       </AntButton>
     </ConfigProvider>
