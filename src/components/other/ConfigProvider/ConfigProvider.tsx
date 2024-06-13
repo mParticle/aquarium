8d229add0187961f33e41e93a0622848b5255b2b
Adam Jenkins <ajenkins@mparticle.com>
2024-06-13 12:01:00 -0300

    Add withConfigProvider

--- src/components/other/ConfigProvider/ConfigProvider.tsx
+++ src/components/other/ConfigProvider/ConfigProvider.tsx
@@ -8,6 +8,15 @@ export const ConfigProvider = (props: IConfigProviderProps) => {
   return <AntConfigProvider {...props} theme={LightTheme} />
 }
 
+export const withConfigProvider =
+  <P extends object>(Component: React.ComponentType<P>) =>
+  // eslint-disable-next-line react/display-name
+  (props: P) => (
+    <ConfigProvider>
+      <Component {...props} />
+    </ConfigProvider>
+  )
+
 ConfigProvider.ConfigContext = AntConfigProvider.ConfigContext
 ConfigProvider.useConfig = AntConfigProvider.useConfig
 ConfigProvider.config = AntConfigProvider.config
