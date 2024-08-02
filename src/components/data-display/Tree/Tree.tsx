019b9b57b053fb2e8f162072b1a265be8edb7d0f
ajenkins-mparticle <ajenkins@mparticle.com>
2024-08-02 10:44:48 -0230

    chore: Genericize Tree (#353)

--- src/components/data-display/Tree/Tree.tsx
+++ src/components/data-display/Tree/Tree.tsx
@@ -1,12 +1,13 @@
 import { Tree as AntTree, type TreeDataNode } from 'antd'
 import { type TreeProps as AntTreeProps } from 'antd'
+import type { BasicDataNode, DataNode } from 'antd/es/tree'
 import { ConfigProvider } from 'src/components'
 
-export interface ITreeProps extends AntTreeProps {}
+export interface ITreeProps<T extends BasicDataNode = DataNode> extends AntTreeProps<T> {}
 
 export interface ITreeData extends TreeDataNode {}
 
-export const Tree = (props: ITreeProps) => {
+export const Tree = <T extends BasicDataNode = DataNode>(props: ITreeProps<T>) => {
   return (
     <ConfigProvider>
       <AntTree {...props} />
