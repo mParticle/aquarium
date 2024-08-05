0aa4cab6d6e75bac88e83206768bc5e8ecc4156d
Gaby Zifferman <gabyzif@gmail.com>
2024-08-05 20:36:21 +0200

    create a file for icon types and update the icon component with the new types

--- src/components/general/Icon/Icon.tsx
+++ src/components/general/Icon/Icon.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Icons, IconVariant } from 'src/constants/Icons'
+import { Icons } from 'src/constants/Icons'
+import { IconOptions, IconVariant, IconNames } from 'src/types/iconTypes'
 import './icon.css'
 
 type IconSize = 'xxxxl' | 'xxxl' | 'xxl' | 'xl' | 'lg' | 'md' | 'sm' | 'xs'
@@ -17,26 +18,21 @@ export type IconColor =
   | 'brand'
 
 export interface IIconProps {
-  name: keyof typeof Icons
+  name: IconNames
   color?: IconColor
   size?: IconSize
   variant?: IconVariant
 }
 
 export const Icon: React.FC<IIconProps> = ({ name, color = 'default', size = 'lg', variant }) => {
-  const iconVariants = Icons[name]
-
-  if (!iconVariants) {
-    console.error(`Icon with name "${name}" not found.`)
-    return null
-  }
+  const iconVariants = Icons[name] as IconOptions
 
   if (iconVariants?.deprecated) {
     console.warn(`Icon with name "${name}" is deprecated. Please use "predictions" instead.`)
   }
 
   const iconVariant = variant || iconVariants.default
-  const IconComponent = iconVariants[iconVariant] || iconVariants[iconVariants?.default]
+  const IconComponent = iconVariants[iconVariant] || iconVariants[iconVariants.default]
 
   if (!IconComponent) {
     console.error(`Icon with name "${name}" and variant "${iconVariant}" not found.`)
---
+++ src/types/iconTypes.ts
@@ -0,0 +1,84 @@
+import { RequireAtLeastOne } from 'type-fest'
+
+export type IconVariant = 'light' | 'duo-tone'
+
+export type IconOptions = RequireAtLeastOne<
+  {
+    light?: React.ComponentType<React.SVGProps<SVGSVGElement>>
+    'duo-tone'?: React.ComponentType<React.SVGProps<SVGSVGElement>>
+    default: IconVariant
+    deprecated?: boolean
+  },
+  'light' | 'duo-tone'
+>
+
+/**
+ * @deprecated Use "predictions" instead.
+ */
+export type DeprecatedIconNames = 'sparkles'
+
+export const iconNamesArray = [
+  'add',
+  'alicorn',
+  'analytics',
+  'C360',
+  'catalog',
+  'chartColumn',
+  'chartLine',
+  'check',
+  'circleNodes',
+  'cloud',
+  'connections',
+  'database',
+  'dsr',
+  'empty',
+  'enrichment',
+  'event',
+  'eventAttribute',
+  'fitToScreen',
+  'folderClosed',
+  'forwarding',
+  'gear',
+  'grid',
+  'heart',
+  'help',
+  'identity',
+  'jumpTo',
+  'lightBulb',
+  'liveStream',
+  'paywall',
+  'messageQuestion',
+  'mpLogo',
+  'observability',
+  'oversight',
+  'predictions',
+  'remove',
+  'search',
+  'segmentation',
+  'shieldKeyhole',
+  'signout',
+  'siteMap',
+  'sparkles', // Deprecated
+  'split',
+  'systemAlerts',
+  'transformation',
+  'upload',
+  'userAttribute',
+  'userProfiles',
+  'users',
+  'wrench',
+  'zoomIn',
+  'zoomOut',
+  'premium',
+  'helpVideo',
+  'conversion',
+  'next',
+  'previous',
+  'openTab',
+  'directory',
+  'lock',
+  'unlock',
+  'notification',
+] as const
+
+export type IconNames = (typeof iconNamesArray)[number]
