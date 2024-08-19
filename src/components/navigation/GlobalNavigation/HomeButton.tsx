cd70700274028e766623b4d75d568ab376688d84
Gaby Zifferman <gabyzif@gmail.com>
2024-08-19 14:03:04 +0200

    fix: merge main correctly

--- src/components/navigation/GlobalNavigation/HomeButton.tsx
+++ src/components/navigation/GlobalNavigation/HomeButton.tsx
@@ -1,14 +1,8 @@
-import React, { useState } from 'react'
-import { Center, Icon, Popover, Tooltip } from 'src/components'
-import MiniMap from 'src/components/navigation/MiniMap/MiniMap'
-import { IMiniMapOptions, MiniMapLink } from 'src/components/navigation/GlobalNavigation/GlobalNavigationItems'
+import React from 'react'
+import { Center, Icon, Tooltip } from 'src/components'
 
 interface MpHomeButtonProps {
   onClick: () => void
-}
-
-interface MinimapWithPopoverProps extends IMiniMapOptions {
-  onPopoverClick: () => void
 }
 
 interface TooltipWithButtonProps {
@@ -16,7 +10,6 @@ interface TooltipWithButtonProps {
 }
 
 interface HomeButtonProps {
-  minimapOptions?: IMiniMapOptions
   onMpHomeClick: () => void
 }
 
@@ -25,37 +18,6 @@ function MpHomeButton(props: MpHomeButtonProps) {
     <Center className="globalNavigation__mpHome" onClick={props.onClick}>
       <Icon name="mpLogo" size="lg" color="white" />
     </Center>
-  )
-}
-
-function MinimapWithPopover(props: MinimapWithPopoverProps) {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
-  const handleLinkClick = (link: MiniMapLink) => {
-    setIsPopoverOpen(false)
-    props.onLinkClick(link)
-  }
-  const handlePopoverOpenChange = (newPopoverState: boolean) => {
-    setIsPopoverOpen(newPopoverState)
-  }
-
-  return (
-    <Popover
-      content={
-        <MiniMap
-          overviewHref={props.overviewHref}
-          onUnauthorizedClick={props.onUnauthorizedClick}
-          links={props.links}
-          onLinkClick={handleLinkClick}
-          unauthorizedLinks={props.unauthorizedLinks}
-          activeLink={props.activeLink}
-        />
-      }
-      placement="rightBottom"
-      open={isPopoverOpen}
-      onOpenChange={handlePopoverOpenChange}
-      arrow={false}>
-      <MpHomeButton onClick={props.onPopoverClick} />
-    </Popover>
   )
 }
 
@@ -68,19 +30,5 @@ function TooltipWithButton(props: TooltipWithButtonProps) {
 }
 
 export function HomeButton(props: HomeButtonProps) {
-  if (!props.minimapOptions) {
-    return <TooltipWithButton onTooltipClick={props.onMpHomeClick} />
-  }
-
-  return (
-    <MinimapWithPopover
-      onUnauthorizedClick={props.minimapOptions.onUnauthorizedClick}
-      overviewHref={props.minimapOptions.overviewHref}
-      links={props.minimapOptions.links}
-      onLinkClick={props.minimapOptions.onLinkClick}
-      unauthorizedLinks={props.minimapOptions.unauthorizedLinks}
-      onPopoverClick={props.onMpHomeClick}
-      activeLink={props.minimapOptions.activeLink}
-    />
-  )
+  return <TooltipWithButton onTooltipClick={props.onMpHomeClick} />
 }
