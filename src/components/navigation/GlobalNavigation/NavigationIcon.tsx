70b5720e051963c0791c5c16cc6a969728af4d21
Leobel Izquierdo <v-lizquierdo@mparticle.com>
2024-09-16 15:55:38 -0400

    chore: update navigation icon

--- src/components/navigation/GlobalNavigation/NavigationIcon.tsx
+++ src/components/navigation/GlobalNavigation/NavigationIcon.tsx
@@ -7,17 +7,11 @@ interface INavigationIconProps {
   label: ReactNode
   hideLabel?: boolean
   onClick?: (e: React.MouseEvent) => void
-  disabled?: boolean
 }
-
-const noop = () => {}
 
 export function NavigationIcon(props: INavigationIconProps) {
   return (
-    <Center
-      vertical
-      className={'globalNavigation__icon ' + (props.className ?? '')}
-      onClick={!props.disabled ? props.onClick : noop}>
+    <Center vertical className={'globalNavigation__icon ' + (props.className ?? '')} onClick={props.onClick}>
       {props.icon}
       {!props.hideLabel && <span className="globalNavigation__iconLabel">{props.label}</span>}
     </Center>
