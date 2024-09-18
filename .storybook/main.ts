a4288c9b6c8a2993695fb4503774da20b770e385
Gabriel Tiburcio <v-gtiburcio@mparticle.com>
2024-09-18 15:54:49 -0300

    feat: introducing new storybook structure

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 import react from '@vitejs/plugin-react'
 import { PluginOption, Plugin } from 'vite'
 import { withoutVitePlugins } from '@storybook/builder-vite'
+import remarkGfm from 'remark-gfm'
 
 type StorybookVitePlugins = { plugins: (PluginOption[] | Plugin)[] }
 
@@ -11,9 +12,24 @@ const config: StorybookConfig & StorybookVitePlugins = {
     options: {},
   },
 
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)', '../docs/**/*.mdx'],
 
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            // needed for rendering markdown tables in .mdx
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
+  ],
 
   docs: {
     autodocs: true,
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,28 @@ import type { Preview } from '@storybook/react'
 const preview: Preview = {
   parameters: {
     layout: 'centered',
+    options: {
+      storySort: {
+        order: [
+          'About',
+          ['Introduction', 'Contributing', 'Changelog', 'FAQ'],
+          'Components',
+          'Foundations',
+          ['Colors', 'Typography', 'Icons', 'Errors', 'Loading'],
+          'Candidate Components',
+          'Design Templates',
+          'Component Process',
+          [
+            'Introduction',
+            'Components',
+            ['Using components', 'Change process'],
+            'Candidate Components',
+            ['Introducing new ones', 'Using existing ones', 'Promoting to a component'],
+            'Design Templates',
+          ],
+        ],
+      },
+    },
 
     controls: {
       matchers: {
