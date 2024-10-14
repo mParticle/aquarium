35a3976600288c3e5da311b522f770419409071d
mparticle-automation <developers@mparticle.com>
2024-10-14 16:12:15 +0000

    Deploy preview for PR 447 🛫

---
+++ pr-preview/pr-447/assets/Grid.stories-DZzqDyWa.js
@@ -0,0 +1,30 @@
+var y=Object.defineProperty,u=Object.defineProperties;var g=Object.getOwnPropertyDescriptors;var d=Object.getOwnPropertySymbols;var x=Object.prototype.hasOwnProperty,j=Object.prototype.propertyIsEnumerable;var c=(r,e,l)=>e in r?y(r,e,{enumerable:!0,configurable:!0,writable:!0,value:l}):r[e]=l,n=(r,e)=>{for(var l in e||(e={}))x.call(e,l)&&c(r,l,e[l]);if(d)for(var l of d(e))j.call(e,l)&&c(r,l,e[l]);return r},i=(r,e)=>u(r,g(e));import{j as s}from"./jsx-runtime-BcCOBRXo.js";import{R as w,C as t}from"./row-DegztUsn.js";import"./index-CNk6hRaE.js";import"./index-z2stgOKB.js";import"./useToken-5RYkiW09.js";import"./extends-CF3RwP-h.js";import"./Serializer-OW4gUFyn.js";import"./index-D-4E_f9W.js";import"./isNativeReflectConstruct-CYew8PZR.js";const k={title:"Components/Layout/Grid",args:{}},o={background:"#0092ff",padding:"8px 0"},a={render:()=>s.jsx(s.Fragment,{children:s.jsxs(w,{gutter:[16,24],children:[s.jsx(t,{className:"gutter-row",span:6,children:s.jsx("div",{style:o,children:"col-6"})}),s.jsx(t,{className:"gutter-row",span:6,children:s.jsx("div",{style:o,children:"col-6"})}),s.jsx(t,{className:"gutter-row",span:6,children:s.jsx("div",{style:o,children:"col-6"})}),s.jsx(t,{className:"gutter-row",span:6,children:s.jsx("div",{style:o,children:"col-6"})}),s.jsx(t,{className:"gutter-row",span:6,children:s.jsx("div",{style:o,children:"col-6"})}),s.jsx(t,{className:"gutter-row",span:6,children:s.jsx("div",{style:o,children:"col-6"})}),s.jsx(t,{className:"gutter-row",span:6,children:s.jsx("div",{style:o,children:"col-6"})}),s.jsx(t,{className:"gutter-row",span:6,children:s.jsx("div",{style:o,children:"col-6"})})]})})};var m,p,v;a.parameters=i(n({},a.parameters),{docs:i(n({},(m=a.parameters)==null?void 0:m.docs),{source:n({originalSource:`{
+  render: () => <>
+      <Row gutter={[16, 24]}>
+        <Col className="gutter-row" span={6}>
+          <div style={divStyle}>col-6</div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={divStyle}>col-6</div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={divStyle}>col-6</div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={divStyle}>col-6</div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={divStyle}>col-6</div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={divStyle}>col-6</div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={divStyle}>col-6</div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={divStyle}>col-6</div>
+        </Col>
+      </Row>
+    </>
+}`},(v=(p=a.parameters)==null?void 0:p.docs)==null?void 0:v.source)})});const F=["Primary"];export{a as Primary,F as __namedExportsOrder,k as default};
