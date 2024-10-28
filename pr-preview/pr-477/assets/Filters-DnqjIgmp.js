a82d78820c17be9d2369b3c09b3ecad9c8c2b1ee
mparticle-automation <developers@mparticle.com>
2024-10-28 15:47:37 +0000

    Deploy preview for PR 477 🛫

---
+++ pr-preview/pr-477/assets/Filters-DnqjIgmp.js
@@ -0,0 +1,22 @@
+var c=Object.defineProperty,h=Object.defineProperties;var m=Object.getOwnPropertyDescriptors;var o=Object.getOwnPropertySymbols;var f=Object.prototype.hasOwnProperty,u=Object.prototype.propertyIsEnumerable;var s=(r,e,i)=>e in r?c(r,e,{enumerable:!0,configurable:!0,writable:!0,value:i}):r[e]=i,n=(r,e)=>{for(var i in e||(e={}))f.call(e,i)&&s(r,i,e[i]);if(o)for(var i of o(e))u.call(e,i)&&s(r,i,e[i]);return r},l=(r,e)=>h(r,m(e));import{j as t}from"./jsx-runtime-BcCOBRXo.js";import{useMDXComponents as d}from"./index-BI4Okn8T.js";import{ae as x,af as a}from"./index-CN9Gdq0H.js";import{F as j,W as g,a as y}from"./Filters.stories-C0ecYvvF.js";import"./index-CNk6hRaE.js";import"./iframe-CSsM8iY_.js";import"../sb-preview/runtime.js";import"./index-D-4E_f9W.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DBzvlRjn.js";import"./index-P2MZap72.js";import"./index-DrFu-skq.js";import"./index-Cn2ehl89.js";import"./GlobalNavigation-Dqx_v86T.js";import"./LightTheme-BtoaoHMG.js";import"./row-C8Fpq1AK.js";import"./useToken-BceXVVM_.js";import"./Serializer-OW4gUFyn.js";import"./dayjs.min-KGLNs1IR.js";import"./customParseFormat-oYt9ov06.js";import"./DatePicker.stories-DIth2IKO.js";import"./SelectWithRangePicker-CQJGlric.js";function p(r){const e=n(n({a:"a",em:"em",h1:"h1",h4:"h4",li:"li",p:"p",strong:"strong",ul:"ul"},d()),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(x,{of:j}),`
+`,t.jsx(e.h1,{id:"filters",children:"Filters"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.p,{children:t.jsxs(e.em,{children:["Note: This section covers filters specifically for tables. For query-related filters, please refer to the ",t.jsx(e.a,{href:"#coming",children:"Analytics Filters - Coming Soon"}),"."]})}),`
+`,t.jsx(e.h4,{id:"simple-filters",children:t.jsx(e.strong,{children:"Simple Filters"})}),`
+`,t.jsxs(e.p,{children:["Simple filters are ideal when there are only a few filter options. These are straightforward and quick to use, often appearing as dropdowns above the table. For implementation, refer to the ",t.jsx(e.a,{href:"https://mparticle.github.io/aquarium/?path=/docs/components-data-entry-select--documentation",rel:"nofollow",children:"Select Component"}),"."]}),`
+`,t.jsx(e.p,{children:"Examples:"}),`
+`,t.jsx(a,{of:g}),`
+`,t.jsx(e.h4,{id:"filters-with-apply-button",children:t.jsx(e.strong,{children:"Filters with Apply Button"})}),`
+`,t.jsx(e.p,{children:"Complex filters provide more advanced filtering options, allowing users to apply multiple criteria at once. These filters often include dropdowns, checkboxes, and text fields. Complex filters are particularly useful when multiple filters need to be applied simultaneously or when load times might be a concern."}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"daterange"}),`
+`,t.jsx(e.li,{children:"modal: sorting, filters: one of each input type: checkboxes, input, tree select and placeholder for tags"}),`
+`]}),`
+`,t.jsx(a,{of:y}),`
+`,t.jsx(e.h4,{id:"date-range-filters",children:t.jsx(e.strong,{children:"Date Range Filters"})}),`
+`,t.jsx(e.p,{children:"Date range filters allow users to specify a range of dates to narrow down the data displayed in the table. Typically, these filters include a start date and an end date, with options for common ranges like “Last 7 days” or “Last 30 days.” User presets are also available for frequently used date ranges."}),`
+`,t.jsx(e.p,{children:"Examples:"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"https://mparticle.github.io/aquarium/?path=/docs/components-data-entry-date-picker--documentation",rel:"nofollow",children:"Basic Date Picker"})}),`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"https://mparticle.github.io/aquarium/?path=/docs/candidate-components-directory-date-range-filter--documentation",rel:"nofollow",children:"Date Range Filter with Presets"})}),`
+`]})]})}function I(r={}){const{wrapper:e}=n(n({},d()),r.components);return e?t.jsx(e,l(n({},r),{children:t.jsx(p,n({},r))})):p(r)}export{I as default};
---
+++ pr-preview/pr-477/assets/FloatButton.stories-DDv_i9Qr.js
@@ -0,0 +1,11 @@
+var S=Object.defineProperty,f=Object.defineProperties;var F=Object.getOwnPropertyDescriptors;var i=Object.getOwnPropertySymbols;var b=Object.prototype.hasOwnProperty,x=Object.prototype.propertyIsEnumerable;var n=(e,r,t)=>r in e?S(e,r,{enumerable:!0,configurable:!0,writable:!0,value:t}):e[r]=t,o=(e,r)=>{for(var t in r||(r={}))b.call(r,t)&&n(e,t,r[t]);if(i)for(var t of i(r))x.call(r,t)&&n(e,t,r[t]);return e},a=(e,r)=>f(e,F(r));import{a2 as C}from"./GlobalNavigation-Dqx_v86T.js";import"./jsx-runtime-BcCOBRXo.js";import"./index-CNk6hRaE.js";import"./LightTheme-BtoaoHMG.js";import"./row-C8Fpq1AK.js";import"./useToken-BceXVVM_.js";import"./extends-CF3RwP-h.js";import"./Serializer-OW4gUFyn.js";import"./index-D-4E_f9W.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DBzvlRjn.js";import"./dayjs.min-KGLNs1IR.js";import"./customParseFormat-oYt9ov06.js";const H={title:"Components/General/FloatButton",component:C,args:{description:"Float Button",tooltip:"Tooltip Text",type:"primary",shape:"circle",href:"",target:"",badge:void 0,onClick:e=>{alert("Float Button Clicked!")}},argTypes:{shape:{control:"select",options:["circle","square"]},type:{control:"select",options:["default","primary"]}}},s={},p={args:{badge:{count:5}}},c={args:{shape:"square"}};var m,l,u;s.parameters=a(o({},s.parameters),{docs:a(o({},(m=s.parameters)==null?void 0:m.docs),{source:o({originalSource:"{}"},(u=(l=s.parameters)==null?void 0:l.docs)==null?void 0:u.source)})});var d,g,h;p.parameters=a(o({},p.parameters),{docs:a(o({},(d=p.parameters)==null?void 0:d.docs),{source:o({originalSource:`{
+  args: {
+    badge: {
+      count: 5
+    }
+  }
+}`},(h=(g=p.parameters)==null?void 0:g.docs)==null?void 0:h.source)})});var y,B,q;c.parameters=a(o({},c.parameters),{docs:a(o({},(y=c.parameters)==null?void 0:y.docs),{source:o({originalSource:`{
+  args: {
+    shape: 'square'
+  }
+}`},(q=(B=c.parameters)==null?void 0:B.docs)==null?void 0:q.source)})});const I=["Primary","WithBadge","Square"];export{s as Primary,c as Square,p as WithBadge,I as __namedExportsOrder,H as default};
