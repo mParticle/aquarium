5d341fc64c93ab8c5aabffce35621e2d8b5ea573
Gaby Zifferman <gabyzif@gmail.com>
2024-11-21 11:40:57 +0100

    docs: update tabs stories

--- src/components/data-display/Tabs/Tabs.stories.tsx
+++ src/components/data-display/Tabs/Tabs.stories.tsx
@@ -4,8 +4,27 @@ import { Tabs } from 'src/components/data-display/Tabs/Tabs'
 const meta: Meta<typeof Tabs> = {
   title: 'Components/Data Display/Tabs',
   component: Tabs,
-
-  args: {},
+  args: {
+    defaultActiveKey: '1',
+    items: [
+      {
+        label: 'Tab 1',
+        key: '1',
+        children: 'Tab 1',
+      },
+      {
+        label: 'Tab 2',
+        key: '2',
+        children: 'Tab 2',
+        disabled: true,
+      },
+      {
+        label: 'Tab 3',
+        key: '3',
+        children: 'Tab 3',
+      },
+    ],
+  },
 }
 export default meta
 
