469e5df6ce4539904f52f5a1106bf33fc9294baa
mparticle-automation <developers@mparticle.com>
2024-11-26 21:19:42 +0000

    Deploy preview for PR 500 🛫

---
+++ pr-preview/pr-500/assets/Documentation-BKuRDzSe.js
@@ -0,0 +1,22 @@
+var l=Object.defineProperty,c=Object.defineProperties;var p=Object.getOwnPropertyDescriptors;var a=Object.getOwnPropertySymbols;var m=Object.prototype.hasOwnProperty,u=Object.prototype.propertyIsEnumerable;var r=(s,t,n)=>t in s?l(s,t,{enumerable:!0,configurable:!0,writable:!0,value:n}):s[t]=n,i=(s,t)=>{for(var n in t||(t={}))m.call(t,n)&&r(s,n,t[n]);if(a)for(var n of a(t))u.call(t,n)&&r(s,n,t[n]);return s},o=(s,t)=>c(s,p(t));import{j as e}from"./jsx-runtime-BcCOBRXo.js";import{useMDXComponents as h}from"./index-BI4Okn8T.js";import{ae as x,ag as g}from"./index-hDjkCmrR.js";import{B as j,E as b}from"./Badge.stories-CXPaJ8-p.js";import"./index-CNk6hRaE.js";import"./iframe-UYGngV0y.js";import"../sb-preview/runtime.js";import"./index-D-4E_f9W.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DBzvlRjn.js";import"./index-P2MZap72.js";import"./index-DrFu-skq.js";import"./GlobalNavigation-DZNJryoT.js";import"./LightTheme-BtoaoHMG.js";import"./row-C-vZtyW2.js";import"./useToken-B2DZIv7q.js";import"./Serializer-OW4gUFyn.js";import"./dayjs.min-KGLNs1IR.js";import"./customParseFormat-oYt9ov06.js";import"./ExampleStory-UKiWh2yY.js";function d(s){const t=i(i({a:"a",h1:"h1",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul"},h()),s.components);return e.jsxs(e.Fragment,{children:[e.jsx(x,{of:j}),`
+`,`
+`,e.jsx(t.h1,{id:"badge",children:"Badge"}),`
+`,e.jsx(t.h4,{id:"overview",children:"Overview"}),`
+`,e.jsxs(t.p,{children:["The ",e.jsx(t.strong,{children:"Badge"})," component provides visual indicators for an item’s status, available in two styles: a ",e.jsx(t.strong,{children:"dot"}),' and a **status badge" with text.']}),`
+`,e.jsx(t.h3,{id:"dot-badge",children:e.jsx(t.a,{href:"https://mparticle.github.io/aquarium/?path=/story/components-data-display-badge--dot-badge",rel:"nofollow",children:"Dot Badge"})}),`
+`,e.jsx(t.h4,{id:"when-to-use",children:"When to use"}),`
+`,e.jsxs(t.p,{children:["Use ",e.jsx(t.strong,{children:"Badge"})," when a status needs to be displayed next to an element in a compact form."]}),`
+`,e.jsx(t.h4,{id:"current-usages",children:"Current Usages"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:e.jsx(t.a,{href:"https://mparticle.github.io/aquarium/?path=/story/components-navigation-globalnavigation--primary",rel:"nofollow",children:"Navigation"})})," – Badge is used within the navigation sidebar for notification use cases."]}),`
+`]}),`
+`,e.jsx(t.h3,{id:"status-badge",children:e.jsx(t.a,{href:"https://mparticle.github.io/aquarium/?path=/story/components-data-display-badge--example-status",rel:"nofollow",children:"Status Badge"})}),`
+`,e.jsx(g,{of:b}),`
+`,e.jsx(t.h4,{id:"when-to-use-1",children:"When to Use"}),`
+`,e.jsxs(t.p,{children:['For displaying status with accompanying text (e.g., "Active," "Failed," or "Draft"), use the ',e.jsx(t.strong,{children:"Status Badge"}),". This provides both a visual indicator and descriptive text to communicate the item's state."]}),`
+`,e.jsx(t.h4,{id:"current-usages-1",children:"Current Usages"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:e.jsx(t.a,{href:"https://mparticle.github.io/aquarium/?path=/story/ux-patterns-table-table--basic-table",rel:"nofollow",children:"Table Cell"})})," –The Badge is used within tables to indicate the status of items."]}),`
+`]}),`
+`,e.jsx(t.h4,{id:"related-links",children:"Related Links"}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Type"}),e.jsx(t.th,{children:"Resource"})]})}),e.jsx(t.tbody,{children:e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"Eames"}),e.jsx(t.td,{children:e.jsx(t.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=399-0&node-type=canvas&t=B6HJWqqDsUOypZQj-0",rel:"nofollow",children:"Badge Component"})})]})})]})]})}function z(s={}){const{wrapper:t}=i(i({},h()),s.components);return t?e.jsx(t,o(i({},s),{children:e.jsx(d,i({},s))})):d(s)}export{z as default};
---
+++ pr-preview/pr-500/assets/Documentation-Dpq7gvjE.js
@@ -0,0 +1,16 @@
+var a=Object.defineProperty,p=Object.defineProperties;var m=Object.getOwnPropertyDescriptors;var s=Object.getOwnPropertySymbols;var c=Object.prototype.hasOwnProperty,u=Object.prototype.propertyIsEnumerable;var i=(n,e,o)=>e in n?a(n,e,{enumerable:!0,configurable:!0,writable:!0,value:o}):n[e]=o,r=(n,e)=>{for(var o in e||(e={}))c.call(e,o)&&i(n,o,e[o]);if(s)for(var o of s(e))u.call(e,o)&&i(n,o,e[o]);return n},d=(n,e)=>p(n,m(e));import{j as t}from"./jsx-runtime-BcCOBRXo.js";import{useMDXComponents as l}from"./index-BI4Okn8T.js";import{ae as x,ag as j}from"./index-hDjkCmrR.js";import{T as f,E as g}from"./Tour.stories-BXFdjnCY.js";import"./index-CNk6hRaE.js";import"./iframe-UYGngV0y.js";import"../sb-preview/runtime.js";import"./index-D-4E_f9W.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DBzvlRjn.js";import"./index-P2MZap72.js";import"./index-DrFu-skq.js";import"./GlobalNavigation-DZNJryoT.js";import"./LightTheme-BtoaoHMG.js";import"./row-C-vZtyW2.js";import"./useToken-B2DZIv7q.js";import"./Serializer-OW4gUFyn.js";import"./dayjs.min-KGLNs1IR.js";import"./customParseFormat-oYt9ov06.js";function h(n){const e=r(r({a:"a",h1:"h1",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul"},l()),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(x,{of:f}),`
+`,`
+`,t.jsx(e.h1,{id:"tour",children:"Tour"}),`
+`,t.jsx(e.h3,{id:"overview",children:"Overview"}),`
+`,t.jsxs(e.p,{children:["The ",t.jsx(e.strong,{children:"Tour"})," component is a popup  designed to guide users through different areas or features of a product. It provides step-by-step instructions or highlights to help users understand key elements and functionality."]}),`
+`,t.jsx(e.h3,{id:"when-to-use",children:"When to Use"}),`
+`,t.jsx(e.p,{children:"Use the Tour component when:"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"You want to onboard new users by introducing them to important features."}),`
+`,t.jsx(e.li,{children:"Highlighting new or updated features for existing users."}),`
+`,t.jsx(e.li,{children:"Offering guidance for complex workflows or multi-step processes."}),`
+`]}),`
+`,t.jsx(e.h4,{id:"related-links",children:"Related Links"}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Resource"})]})}),t.jsx(e.tbody,{children:t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"Eames"}),t.jsx(e.td,{children:t.jsx(e.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=1721-40337&node-type=canvas&t=gOAJGJ8C9ev7N7lt-0",rel:"nofollow",children:"Tour Component"})})]})})]}),`
+`,`
+`,t.jsx(j,{of:g})]})}function z(n={}){const{wrapper:e}=r(r({},l()),n.components);return e?t.jsx(e,d(r({},n),{children:t.jsx(h,r({},n))})):h(n)}export{z as default};
