4838a86929bd743f606688ceefeaf13ebf5b1f9d
dan <172324610+danielsiemens@users.noreply.github.com>
2024-11-28 13:22:57 -0800

    feat: utilize generics in UserPreferencesService  (#504)
    Co-authored-by: v-gmarques <v-gmarques@mparticle.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -121,19 +121,17 @@ export { SuiteLogo } from './navigation/GlobalNavigation/SuiteLogo'
 export { Typography } from './general/Typography/Typography'
 
 // UPS
-export { UserPreferencesService } from '../services/user-preferences/user-preferences'
+export { UserPreferencesService } from '../services/user-preferences'
 export { CompositeUserPreferencesService } from '../services/user-preferences/composite-user-preferences-service'
-export { type CompositeUserPreferences } from '../services/user-preferences/models/user-preferences/composite-user-preferences'
 export {
   UserPreferenceScopeType,
   type UserPreferenceDefinition,
   type UserPreferenceDefinitions,
+  type UserPreferencesPerScope,
 } from '../services/user-preferences/models/definitions'
 export {
-  type UserPreferences,
   USER_PREFERENCE_SCOPE_SEPARATOR,
   UserPreferenceGlobalScope,
-  type UserPreference,
   type UserPreferenceScope,
 } from '../services/user-preferences/models/storage-models'
 export {
