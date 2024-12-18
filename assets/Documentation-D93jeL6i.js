a441b2aa968484c3c94bc1220e0ce82e79d29adc
tibuurcio <tibuurcio@gmail.com>
2024-12-18 18:19:41 +0000

    Deploying to gh-pages from @ mParticle/aquarium@c0e3048e12f74cda908714f8518f113c8340250d 🚀

---
+++ assets/Documentation-D93jeL6i.js
@@ -0,0 +1,21 @@
+var l=Object.defineProperty,c=Object.defineProperties;var p=Object.getOwnPropertyDescriptors;var a=Object.getOwnPropertySymbols;var m=Object.prototype.hasOwnProperty,u=Object.prototype.propertyIsEnumerable;var r=(n,t,s)=>t in n?l(n,t,{enumerable:!0,configurable:!0,writable:!0,value:s}):n[t]=s,i=(n,t)=>{for(var s in t||(t={}))m.call(t,s)&&r(n,s,t[s]);if(a)for(var s of a(t))u.call(t,s)&&r(n,s,t[s]);return n},o=(n,t)=>c(n,p(t));import{j as e}from"./jsx-runtime-BcCOBRXo.js";import{useMDXComponents as h}from"./index-BI4Okn8T.js";import{ae as x}from"./index-BmOfQxCj.js";import{B as g}from"./Badge.stories-D8pqDIDo.js";import"./index-CNk6hRaE.js";import"./iframe-Cog5ulpf.js";import"../sb-preview/runtime.js";import"./index-D-4E_f9W.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DBzvlRjn.js";import"./index-P2MZap72.js";import"./index-DrFu-skq.js";import"./GlobalNavigation-Ut2ljeIp.js";import"./LightTheme-BtoaoHMG.js";import"./row-QDIYUeph.js";import"./useToken-2Ajj7nyt.js";import"./ExampleStory-DhKnNwri.js";function d(n){const t=i(i({a:"a",h1:"h1",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul"},h()),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(x,{of:g}),`
+`,`
+`,e.jsx(t.h1,{id:"badge",children:"Badge"}),`
+`,e.jsx(t.h4,{id:"overview",children:"Overview"}),`
+`,e.jsxs(t.p,{children:["The ",e.jsx(t.strong,{children:"Badge"})," component provides visual indicators for an item’s status, available in two styles: a ",e.jsx(t.strong,{children:"dot"}),' and a **status badge" with text.']}),`
+`,e.jsx(t.h3,{id:"dot-badge",children:e.jsx(t.a,{href:"https://mparticle.github.io/aquarium/?path=/story/components-data-display-badge--dot-badge",rel:"nofollow",children:"Dot Badge"})}),`
+`,e.jsx(t.h4,{id:"when-to-use",children:"When to use"}),`
+`,e.jsxs(t.p,{children:["Use ",e.jsx(t.strong,{children:"Badge"})," when a status needs to be displayed next to an element in a compact form."]}),`
+`,e.jsx(t.h4,{id:"current-usages",children:"Current Usages"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:e.jsx(t.a,{href:"https://mparticle.github.io/aquarium/?path=/story/components-navigation-globalnavigation--primary",rel:"nofollow",children:"Navigation"})})," – Badge is used within the navigation sidebar for notification use cases."]}),`
+`]}),`
+`,e.jsx(t.h3,{id:"status-badge",children:e.jsx(t.a,{href:"https://mparticle.github.io/aquarium/?path=/story/components-data-display-badge--example-status",rel:"nofollow",children:"Status Badge"})}),`
+`,e.jsx(t.h4,{id:"when-to-use-1",children:"When to Use"}),`
+`,e.jsxs(t.p,{children:['For displaying status with accompanying text (e.g., "Active," "Failed," or "Draft"), use the ',e.jsx(t.strong,{children:"Status Badge"}),". This provides both a visual indicator and descriptive text to communicate the item's state."]}),`
+`,e.jsx(t.h4,{id:"current-usages-1",children:"Current Usages"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:e.jsx(t.a,{href:"https://mparticle.github.io/aquarium/?path=/story/ux-patterns-table-table--basic-table",rel:"nofollow",children:"Table Cell"})})," –The Badge is used within tables to indicate the status of items."]}),`
+`]}),`
+`,e.jsx(t.h4,{id:"related-links",children:"Related Links"}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Type"}),e.jsx(t.th,{children:"Resource"})]})}),e.jsx(t.tbody,{children:e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"Eames"}),e.jsx(t.td,{children:e.jsx(t.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=399-0&node-type=canvas&t=B6HJWqqDsUOypZQj-0",rel:"nofollow",children:"Badge Component"})})]})})]})]})}function k(n={}){const{wrapper:t}=i(i({},h()),n.components);return t?e.jsx(t,o(i({},n),{children:e.jsx(d,i({},n))})):d(n)}export{k as default};
--- assets/clone-BGS_akXT.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-y2VUG4hh.js";var e=4;function a(o){return r(o,e)}export{a as c};
