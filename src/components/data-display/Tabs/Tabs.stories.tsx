48b392024ca820db093ee27849f087efd3f93981
Anastasiia Stavitskaya <stavitskaya.anastasiia@gmail.com>
2025-01-14 10:24:13 -0800

    docs: data-display small docs updates and clean up stories (#519)

--- src/components/data-display/Tabs/Tabs.stories.tsx
+++ src/components/data-display/Tabs/Tabs.stories.tsx
@@ -1,5 +1,24 @@
 import { type Meta, type StoryObj } from '@storybook/react'
-import { Tabs } from 'src/components/data-display/Tabs/Tabs'
+import { type ITabsProps, Tabs } from 'src/components/data-display/Tabs/Tabs'
+
+const items: ITabsProps['items'] = [
+  {
+    key: '1',
+    label: 'Tab 1',
+    children: 'Content of Tab Pane 1',
+  },
+  {
+    key: '2',
+    label: 'Tab 2',
+    children: 'Content of Tab Pane 2',
+    disabled: true,
+  },
+  {
+    key: '3',
+    label: 'Tab 3',
+    children: 'Content of Tab Pane 3',
+  },
+]
 
 const meta: Meta<typeof Tabs> = {
   title: 'Components/Data Display/Tabs',
@@ -11,4 +30,9 @@ export default meta
 
 type Story = StoryObj<typeof Tabs>
 
-export const Primary: Story = {}
+export const Primary: Story = {
+  args: {
+    items,
+    defaultActiveKey: '1',
+  },
+}
