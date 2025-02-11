99572fdd2524e6b3ca6ec106e2abcd0bfc72aa0d
v-gmarques <v-gmarques@mparticle.com>
2025-02-11 17:17:17 -0300

    fix: AuthManager

--- src/shared/AuthorizationManager.ts
+++ src/shared/AuthorizationManager.ts
@@ -1,14 +1,12 @@
 import { Operation } from "./Operation";
 
 export class AuthorizationManager {
-    private static authorizedOperations: Operation[] = [];
-    
     public static isAuthorized(operation: Operation | Operation[]): boolean {
         if (Array.isArray(operation)) {
-            return operation.every(op => this.authorizedOperations.includes(op));
+            return operation.every(op => window.mParticleConfig.operations.includes(op));
         }
         else {
-            return this.authorizedOperations.includes(operation);
+            return window.mParticleConfig.operations.includes(operation);
         }
     }
 }
