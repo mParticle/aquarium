60f4e5240aaa05ebedc6dd906e297fac1da22223
mparticle-automation <developers@mparticle.com>
2025-02-11 23:17:35 +0000

    Deploy preview for PR 541 🛫

---
+++ pr-preview/pr-541/assets/Documentation-Dv6hPiRk.js
@@ -0,0 +1,18 @@
+var d=Object.defineProperty,m=Object.defineProperties;var p=Object.getOwnPropertyDescriptors;var s=Object.getOwnPropertySymbols;var h=Object.prototype.hasOwnProperty,g=Object.prototype.propertyIsEnumerable;var o=(n,e,r)=>e in n?d(n,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):n[e]=r,i=(n,e)=>{for(var r in e||(e={}))h.call(e,r)&&o(n,r,e[r]);if(s)for(var r of s(e))g.call(e,r)&&o(n,r,e[r]);return n},a=(n,e)=>m(n,p(e));import{j as t}from"./jsx-runtime-BcCOBRXo.js";import{useMDXComponents as c}from"./index-BI4Okn8T.js";import{ae as x,af as j}from"./index-C87C_rmX.js";import{S as b,D as f}from"./SelectWithRangePicker.stories-YaZxzixc.js";import"./index-CNk6hRaE.js";import"./iframe-4MyrA5f1.js";import"../sb-preview/runtime.js";import"./index-D-4E_f9W.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DBzvlRjn.js";import"./index-P2MZap72.js";import"./index-DrFu-skq.js";import"./SelectWithRangePicker-Bn1cbTTz.js";import"./ChartColors-BddzpAPt.js";import"./LightTheme-BtoaoHMG.js";import"./row-D9D95D_h.js";import"./useToken-D9toSmcO.js";import"./index-BU0ZtLqn.js";function l(n){const e=i(i({a:"a",h1:"h1",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul"},c()),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(x,{of:b}),`
+`,t.jsx(e.h1,{id:"date-range-filter",children:"Date Range Filter"}),`
+`,t.jsx(e.p,{children:t.jsx(e.strong,{children:"Overview"})}),`
+`,t.jsx(e.p,{children:`The Date Range Filter is used when a user needs to narrow down the dataset they are working with to a specific date
+and/or time window, allowing users to focus on data relevant to the selected timeframe.`}),`
+`,t.jsx(e.p,{children:t.jsx(e.strong,{children:"Key Components"})}),`
+`,t.jsx(e.p,{children:"The Date Range Filter consists of two main components:"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"?path=/docs/components-data-entry-select--documentation",children:"Select Component"})}),`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"https://ant.design/components/date-picker#date-picker-demo-range-picker",rel:"nofollow",children:"Range Picker"})}),`
+`]}),`
+`,t.jsx(e.p,{children:t.jsx(e.strong,{children:"mParticle Usage"})}),`
+`,t.jsx(e.p,{children:"The Date Range Filter can be used in dashboards, reports, or any interfaces where users interact with time-based data."}),`
+`,t.jsx(j,{of:f}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"current-usages",children:"Current usages"}),`
+`,t.jsx(e.p,{children:t.jsx(e.strong,{children:"9/25/2024 - Observability Project in CDP"})}),`
+`,t.jsxs("ul",{children:[t.jsx("li",{children:t.jsxs("div",{style:{display:"flex",alignItems:"center"},children:[t.jsx("img",{src:"https://badgen.net/badge/icon/Figma?icon=libraries&label&color=purple",alt:"Repo",style:{marginRight:8}}),t.jsx("a",{href:"https://www.figma.com/design/hDSxMHoLlMlLeYbgvT3A6d/Observability-Exploration?node-id=2733-147740&node-type=canvas&t=WfJOqsHGC3JStCh3-0",target:"_top",rel:"nofollow",className:"sbdocs sbdocs-a",children:"Specs"})]})}),t.jsx("li",{children:t.jsxs("div",{style:{display:"flex",alignItems:"center"},children:[t.jsx("img",{src:"https://badgen.net/badge/icon/GitHub?icon=github&label",alt:"Repo",style:{marginRight:8}}),t.jsx("a",{href:"https://git.corp.mparticle.com/mParticle/mPServer/blob/main/src/UI/MP.UI.Web.Nancy/Assets/private/aurelia/src/viewmodels/observability/trace/components/SelectWithRangePicker/SelectWithRangePicker.tsx",target:"_top",rel:"nofollow",className:"sbdocs sbdocs-a",children:"mPServer Example"})]})})]})]})}function U(n={}){const{wrapper:e}=i(i({},c()),n.components);return e?t.jsx(e,a(i({},n),{children:t.jsx(l,i({},n))})):l(n)}export{U as default};
