314f3062c08bba6ac2a06ed6588a100651b95bc7
Anastasiia Stavitskaya <stavitskaya.anastasiia@gmail.com>
2025-02-21 14:12:34 -0800

    refactor: nav list to support submenu

--- src/components/navigation/GlobalNavigation/NavigationList.tsx
+++ src/components/navigation/GlobalNavigation/NavigationList.tsx
@@ -1,4 +1,4 @@
-import { Flex, Icon, Menu, type MenuItemGroupType } from 'src/components'
+import { Flex, Icon, Menu } from 'src/components'
 import { NavigationIcon } from 'src/components/navigation/GlobalNavigation/NavigationIcon'
 import { NavigationItem } from 'src/components/navigation/GlobalNavigation/NavigationItem'
 import { Center } from 'src/components'
@@ -7,34 +7,36 @@ import { type IGlobalNavigationLink } from 'src/components/navigation/GlobalNavi
 import { Fragment } from 'react'
 import { buildLinkFromHrefOptions } from 'src/utils/utils'
 import { NavigationButtonItem } from 'src/components/navigation/GlobalNavigation/NavigationButtonItem'
-import { ItemType } from "antd/lib/menu/interface";
+import { type ItemType } from 'antd/lib/menu/interface'
 
 export interface INavigationListProps {
   items: IGlobalNavigationItem[]
 }
 
 export function NavigationList(props: INavigationListProps) {
-  const visibleItems = props.items.filter(item => item.visible !== false);
+  const visibleItems = props.items.filter(item => item.visible !== false)
 
   return (
     <Center vertical>
-      {visibleItems.map((item) =>
+      {visibleItems.map(item => (
         <Fragment key={item.id}>
           {item.type === 'menu' ? (
-            <Menu expandIcon={null} className="globalNavigation__menu" items={[generateMenuItem(item)]} />
+            <Menu
+              mode="vertical"
+              expandIcon={null}
+              className="globalNavigation__menu"
+              items={[generateMenuItem(item)]}
+            />
           ) : (
             <NavigationItem {...item} isActive={isNavigationItemActive(item)} type="link" />
           )}
         </Fragment>
-      )}
+      ))}
     </Center>
   )
 }
 
 function generateMenuChild(item: IGlobalNavigationItem): ItemType {
-  // TODO: this is not correct, check with gm if we can get disabled on menu
-  const isMenuDisabled = item.type === 'menu' && item?.children.every(child => child.disabled);
-
   const baseItem = {
     key: item.id,
     label: item.label,
@@ -46,32 +48,33 @@ function generateMenuChild(item: IGlobalNavigationItem): ItemType {
         ...baseItem,
         disabled: item.disabled,
         className: `unifiedNavigation__childItem${item.disabled ? ' unifiedNavigation__childItem--disabled' : ''}`,
-        label: <Flex align='center' gap={4}>
-          {item.disabled && <Icon name='unlock' size='xs' />}
-          {buildLinkFromHrefOptions(item.label, item.hrefOptions)}
-        </Flex>
-      };
+        label: (
+          <Flex align="center" gap={4}>
+            {item.disabled && <Icon name="unlock" size="xs" />}
+            {buildLinkFromHrefOptions(item.label, item.hrefOptions)}
+          </Flex>
+        ),
+      }
     case 'button':
       return {
         ...baseItem,
         className: 'globalNavigation__buttonItem',
-        label: <NavigationButtonItem withoutContainer={false} label={item.label} {...item.buttonOptions} />
-      };
+        label: <NavigationButtonItem withoutContainer={false} label={item.label} {...item.buttonOptions} />,
+      }
     case 'menu':
       return {
         ...baseItem,
-        type: 'group',
-        className: `unifiedNavigation__menu${isMenuDisabled ? ' unifiedNavigation__menu--disabled' : ''}`,
+        type: 'submenu',
         label: (
-          <Flex align='center' gap={4}>
-            {isMenuDisabled && <Icon name='unlock' size='xs' />}
+          <span>
             {item.label}
-          </Flex>
+            <i className="ant-menu-submenu-arrow"></i>
+          </span>
         ),
-        children: item.children.filter(item => item.visible !== false).map(generateMenuChild)
-      };
+        children: item.children.filter(item => item.visible !== false).map(generateMenuChild),
+      }
     default:
-      return baseItem;
+      return baseItem
   }
 }
 
@@ -85,28 +88,24 @@ function generateMenuItem(item: IGlobalNavigationItem): ItemType {
     />
   )
 
-  const isActive = isNavigationItemActive(item);
-  let itemClassName = 'globalNavigation__item unifiedNavigation__item';
-  if (isActive) {
-    itemClassName += ' unifiedNavigation__item--active';
-  }
+  const isActive = isNavigationItemActive(item)
 
   return {
     icon: navigationIcon,
     popupClassName: 'globalNavigation__popup globalNavigation__popup--unifiedNav',
-    className: itemClassName,
+    className: 'globalNavigation__item' + (isActive ? ' globalNavigation__item--active' : ''),
     key: item.id,
-    children: item.type === 'menu' ? item.children.filter(item => item.visible !== false).map(generateMenuChild) : undefined,
+    children:
+      item.type === 'menu' ? item.children.filter(item => item.visible !== false).map(generateMenuChild) : undefined,
   }
 }
 
 function isNavigationItemActive(item: IGlobalNavigationItem): boolean {
-  if(item.type === "menu" && item.children) {
+  if (item.type === 'menu' && item.children) {
     return item.children.some(child => isNavigationItemActive(child))
-  }
-  else if(item.type === "link" && item.hrefOptions) {
-    return window.location.href.includes(item.hrefOptions.href);
+  } else if (item.type === 'link' && item.hrefOptions) {
+    return window.location.href.includes(item.hrefOptions.href)
   }
 
-  return false;
+  return false
 }
