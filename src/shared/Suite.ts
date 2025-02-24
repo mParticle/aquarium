b3b5afedeb404dec54133a8729341caaca0cf268
Gabriel Marques <gabriel.koloral@gmail.com>
2025-02-24 09:00:18 -0300

    feat: Introduce temp shared folder (#544)

---
+++ src/shared/Suite.ts
@@ -0,0 +1,8 @@
+export enum Suite {
+    Root = "Root",
+    DataPlatform = "DataPlatform",
+    Customer360 = "Customer360",
+    Segmentation = "Segmentation",
+    Oversight = "Oversight",
+    Analytics = "Analytics",
+}
