6c2915a13c712090508a456b0c47d64f192370fb
mparticle-automation <developers@mparticle.com>
2025-02-26 14:23:33 +0000

    Deploy preview for PR 560 🛫

---
+++ pr-preview/pr-560/assets/Documentation-DKNFwO2D.js
@@ -0,0 +1,13 @@
+var m=Object.defineProperty,a=Object.defineProperties;var p=Object.getOwnPropertyDescriptors;var i=Object.getOwnPropertySymbols;var c=Object.prototype.hasOwnProperty,x=Object.prototype.propertyIsEnumerable;var o=(t,e,r)=>e in t?m(t,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):t[e]=r,s=(t,e)=>{for(var r in e||(e={}))c.call(e,r)&&o(t,r,e[r]);if(i)for(var r of i(e))x.call(e,r)&&o(t,r,e[r]);return t},d=(t,e)=>a(t,p(e));import{j as n}from"./jsx-runtime-BcCOBRXo.js";import{useMDXComponents as l}from"./index-BI4Okn8T.js";import{ae as u,ag as j}from"./index-CXPt2c1n.js";import{I as f,m as w}from"./Input.stories-gfdGTH1P.js";import"./index-CNk6hRaE.js";import"./iframe-DdjUtYKa.js";import"../sb-preview/runtime.js";import"./index-D-4E_f9W.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DBzvlRjn.js";import"./index-P2MZap72.js";import"./index-DrFu-skq.js";import"./NavigationItemsService-ClnanUdo.js";import"./LightTheme-BtoaoHMG.js";import"./row-D9D95D_h.js";import"./useToken-D9toSmcO.js";function h(t){const e=s(s({a:"a",h1:"h1",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul"},l()),t.components);return n.jsxs(n.Fragment,{children:[n.jsx(u,{of:f}),`
+`,`
+`,n.jsx(e.h1,{id:"input",children:"Input"}),`
+`,n.jsx(e.h4,{id:"overview",children:"Overview"}),`
+`,n.jsxs(e.p,{children:["The ",n.jsx(e.strong,{children:"Input"})," component allows users to enter content using a mouse or keyboard. It serves as the most basic form field wrapper and is commonly used where user input is required."]}),`
+`,n.jsx(e.h4,{id:"when-to-use",children:n.jsx(e.strong,{children:"When To Use"})}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"A user input in a form field is needed."}),`
+`,n.jsx(e.li,{children:"A search input is required."}),`
+`]}),`
+`,n.jsx(e.h4,{id:"related-links",children:"Related Links"}),`
+`,n.jsxs(e.table,{children:[n.jsx(e.thead,{children:n.jsxs(e.tr,{children:[n.jsx(e.th,{children:"Type"}),n.jsx(e.th,{children:"Resource"})]})}),n.jsxs(e.tbody,{children:[n.jsxs(e.tr,{children:[n.jsx(e.td,{children:"Eames"}),n.jsx(e.td,{children:n.jsx(e.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=388-11439&p=f&t=zURbLpG60aM6aJiH-0",rel:"nofollow",children:"Input Component"})})]}),n.jsxs(e.tr,{children:[n.jsx(e.td,{children:"AntD"}),n.jsx(e.td,{children:n.jsx(e.a,{href:"https://ant.design/components/input",rel:"nofollow",children:"Input Component"})})]})]})]}),`
+`,n.jsx(j,{meta:w})]})}function S(t={}){const{wrapper:e}=s(s({},l()),t.components);return e?n.jsx(e,d(s({},t),{children:n.jsx(h,s({},t))})):h(t)}export{S as default};
