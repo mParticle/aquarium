e044f76674a38b5283a75bef2bbcd78ae785a1ff
Anastasiia Stavitskaya <stavitskaya.anastasiia@gmail.com>
2025-03-03 15:38:13 -0800

    chore: update drawer story

--- src/components/feedback/Drawer/Drawer.stories.tsx
+++ src/components/feedback/Drawer/Drawer.stories.tsx
@@ -1,5 +1,7 @@
 import { type Meta, type StoryObj } from '@storybook/react'
+import { useState } from 'react'
 import { Drawer } from 'src/components/feedback/Drawer/Drawer'
+import { Button } from 'src/components/general/Button/Button'
 
 const meta: Meta<typeof Drawer> = {
   title: 'Components/Feedback/Drawer',
@@ -11,4 +13,18 @@ export default meta
 
 type Story = StoryObj<typeof Drawer>
 
-export const Primary: Story = {}
+export const Primary: Story = {
+  render: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [open, setOpen] = useState(false)
+
+    return (
+      <>
+        <Button onClick={() => setOpen(true)}>Open Drawer</Button>
+        <Drawer title="Drawer Title" open={open} onClose={() => setOpen(false)}>
+          <p>Drawer content</p>
+        </Drawer>
+      </>
+    )
+  },
+}
