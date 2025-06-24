1eefa131a36c9429cb67da4aa3549b9862e7b4f2
mparticle-automation <developers@mparticle.com>
2025-06-24 18:25:02 +0000

    Deploy preview for PR 617 🛫

---
+++ pr-preview/pr-617/assets/Filters-CHKR7fqK.js
@@ -0,0 +1,19 @@
+var p=Object.defineProperty,c=Object.defineProperties;var h=Object.getOwnPropertyDescriptors;var o=Object.getOwnPropertySymbols;var m=Object.prototype.hasOwnProperty,f=Object.prototype.propertyIsEnumerable;var s=(r,e,i)=>e in r?p(r,e,{enumerable:!0,configurable:!0,writable:!0,value:i}):r[e]=i,n=(r,e)=>{for(var i in e||(e={}))m.call(e,i)&&s(r,i,e[i]);if(o)for(var i of o(e))f.call(e,i)&&s(r,i,e[i]);return r},a=(r,e)=>c(r,h(e));import{j as t}from"./jsx-runtime-BcCOBRXo.js";import{useMDXComponents as d}from"./index-BI4Okn8T.js";import{ae as u,af as x}from"./index-CjkTv3ju.js";import{T as j,W as g}from"./Table.stories-DoYenspr.js";import"./index-CNk6hRaE.js";import"./iframe-D5vtDYa8.js";import"../sb-preview/runtime.js";import"./index-D-4E_f9W.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DBzvlRjn.js";import"./index-P2MZap72.js";import"./index-DrFu-skq.js";import"./NavigationItemsService-oTrdneHk.js";import"./LightTheme-BtoaoHMG.js";import"./row-GPgCuxPF.js";import"./useToken-BSaSgSTZ.js";import"./DatePicker.stories-DS4LbqdD.js";import"./SelectWithRangePicker-LY8Vf75j.js";function l(r){const e=n(n({a:"a",em:"em",h1:"h1",h4:"h4",li:"li",p:"p",strong:"strong",ul:"ul"},d()),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(u,{of:j}),`
+`,t.jsx(e.h1,{id:"filters",children:"Filters"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.p,{children:t.jsxs(e.em,{children:["Note: This section covers filters specifically for tables. For query-related filters, please refer to the ",t.jsx(e.a,{href:"#coming",children:"Analytics Filters - Coming Soon"}),"."]})}),`
+`,t.jsx(e.h4,{id:"filter-search",children:"Filter Search"}),`
+`,t.jsx(e.p,{children:"Located above the table on the right, the search filter allows users to quickly find specific data within the table by entering keywords."}),`
+`,t.jsx(e.h4,{id:"simple-filters",children:t.jsx(e.strong,{children:"Simple Filters"})}),`
+`,t.jsxs(e.p,{children:["Simple filters are ideal when there are only a few filter options. These are straightforward and quick to use and appearing as dropdowns above the table. For examples, refer to the ",t.jsx(e.a,{href:"https://mparticle.github.io/aquarium/?path=/docs/components-data-entry-select--documentation",rel:"nofollow",children:"Select Component"}),"."]}),`
+`,t.jsx(e.h4,{id:"filters-with-apply-button",children:t.jsx(e.strong,{children:"Filters with Apply Button"})}),`
+`,t.jsx(e.p,{children:"Complex filters provide more advanced filtering options, allowing users to apply multiple criteria at once. These filters often include dropdowns, checkboxes, and text fields. Complex filters are particularly useful when multiple filters need to be applied simultaneously or when load times might be a concern."}),`
+`,t.jsx(e.p,{children:"Example:"}),`
+`,t.jsx(x,{of:g}),`
+`,t.jsx(e.h4,{id:"date-range-filters",children:t.jsx(e.strong,{children:"Date Range Filters"})}),`
+`,t.jsx(e.p,{children:"Date range filters allow users to specify a range of dates to narrow down the data displayed in the table. Typically, these filters include a start date and an end date, with options for common ranges like “Last 7 days” or “Last 30 days.” User presets are also available for frequently used date ranges."}),`
+`,t.jsx(e.p,{children:"Examples:"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"https://mparticle.github.io/aquarium/?path=/docs/components-data-entry-date-picker--documentation",rel:"nofollow",children:"Basic Date Picker"})}),`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"https://mparticle.github.io/aquarium/?path=/docs/candidate-components-directory-date-range-filter--documentation",rel:"nofollow",children:"Date Range Filter with Presets"})}),`
+`]})]})}function N(r={}){const{wrapper:e}=n(n({},d()),r.components);return e?t.jsx(e,a(n({},r),{children:t.jsx(l,n({},r))})):l(r)}export{N as default};
