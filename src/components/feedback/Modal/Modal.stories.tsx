8c81313850b5b69478cd2b8cdc327a121497ea80
skosemP <skose@mparticle.com>
2025-08-12 15:00:28 -0400

    feat: add delete confirm modal (#627)

--- src/components/feedback/Modal/Modal.stories.tsx
+++ src/components/feedback/Modal/Modal.stories.tsx
@@ -1,48 +1,12 @@
-import { useState } from 'react'
-import { Modal, type IModalProps } from 'src/components/feedback/Modal/Modal'
-import { type Meta, type StoryObj } from '@storybook/react'
-import { Button } from 'src/components/general/Button/Button'
+import { Modal } from 'src/components/feedback/Modal/Modal'
+import { type Meta } from '@storybook/react'
 
 const meta: Meta<typeof Modal> = {
   title: 'Components/Feedback/Modal',
   component: Modal,
-
   args: {},
 }
 export default meta
 
-type Story = StoryObj<typeof Modal>
-
-const PrimaryTemplate = (args: IModalProps) => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const showModal = () => {
-    setIsModalOpen(true)
-  }
-
-  const handleOk = () => {
-    setIsModalOpen(false)
-  }
-
-  const handleCancel = () => {
-    setIsModalOpen(false)
-  }
-
-  return (
-    <>
-      <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
-
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
-    </>
-  )
-}
-
-export const Primary: Story = {
-  render: PrimaryTemplate,
-}
+// This file serves as the base component export for Modal
+// Individual examples are in the subfolders: Primary and Delete Confirm
