9ee63d56e8247e0af8c7ffd3d0961c4a1146e3ef
nastyastavitskaya <stavitskaya.anastasiia@gmail.com>
2025-08-12 21:50:30 +0000

    Deploying to gh-pages from @ mParticle/aquarium@18a15b004c87f90e28dc01153d97dc0c80ebfe2c 🚀

---
+++ assets/Image.stories-DOpFYghh.js
@@ -0,0 +1,8 @@
+var g=Object.defineProperty,l=Object.defineProperties;var d=Object.getOwnPropertyDescriptors;var p=Object.getOwnPropertySymbols;var u=Object.prototype.hasOwnProperty,f=Object.prototype.propertyIsEnumerable;var c=(a,e,r)=>e in a?g(a,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):a[e]=r,t=(a,e)=>{for(var r in e||(e={}))u.call(e,r)&&c(a,r,e[r]);if(p)for(var r of p(e))f.call(e,r)&&c(a,r,e[r]);return a},o=(a,e)=>l(a,d(e));import{n as h}from"./NavigationItemsService-80zgnE6A.js";const v={title:"Components/Data Display/Image",component:h,args:{}},s={args:{src:"https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",preview:!1,width:100,height:100}};var m,n,i;s.parameters=o(t({},s.parameters),{docs:o(t({},(m=s.parameters)==null?void 0:m.docs),{source:t({originalSource:`{
+  args: {
+    src: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+    preview: false,
+    width: 100,
+    height: 100
+  }
+}`},(i=(n=s.parameters)==null?void 0:n.docs)==null?void 0:i.source)})});const y=["Primary"],_=Object.freeze(Object.defineProperty({__proto__:null,Primary:s,__namedExportsOrder:y,default:v},Symbol.toStringTag,{value:"Module"}));export{_ as I,v as m};
