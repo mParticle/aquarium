fea6aa8962c6a75547130f7f20e9c4b6bea16136
skosemP <skose@mparticle.com>
2025-08-20 19:11:08 +0000

    Deploying to gh-pages from @ mParticle/aquarium@0dfa8356005fe1ca404c567f8b4fddbb159099b7 🚀

--- assets/Avatar.stories-d3Ki8c3z.js
+++
@@ -1,9 +0,0 @@
-var h=Object.defineProperty,f=Object.defineProperties;var I=Object.getOwnPropertyDescriptors;var i=Object.getOwnPropertySymbols;var _=Object.prototype.hasOwnProperty,j=Object.prototype.propertyIsEnumerable;var c=(o,r,e)=>r in o?h(o,r,{enumerable:!0,configurable:!0,writable:!0,value:e}):o[r]=e,s=(o,r)=>{for(var e in r||(r={}))_.call(r,e)&&c(o,e,r[e]);if(i)for(var e of i(r))j.call(r,e)&&c(o,e,r[e]);return o},a=(o,r)=>f(o,I(r));import{j as x}from"./jsx-runtime-BcCOBRXo.js";import{A,I as P}from"./NavigationItemsService-80zgnE6A.js";import"./_baseClone-DBzvlRjn.js";const S={title:"Components/Data Display/Avatar",component:A},t={args:{children:"mP"}},n={args:{icon:x.jsx(P,{name:"userProfiles",size:"lg",color:"white"})}};var m,l,p;t.parameters=a(s({},t.parameters),{docs:a(s({},(m=t.parameters)==null?void 0:m.docs),{source:s({originalSource:`{
-  args: {
-    children: 'mP'
-  }
-}`},(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source)})});var d,u,g;n.parameters=a(s({},n.parameters),{docs:a(s({},(d=n.parameters)==null?void 0:d.docs),{source:s({originalSource:`{
-  args: {
-    icon: <Icon name="userProfiles" size="lg" color="white" />
-  }
-}`},(g=(u=n.parameters)==null?void 0:u.docs)==null?void 0:g.source)})});const v=["WithInitials","WithIcon"],O=Object.freeze(Object.defineProperty({__proto__:null,WithIcon:n,WithInitials:t,__namedExportsOrder:v,default:S},Symbol.toStringTag,{value:"Module"}));export{O as A,S as m};
