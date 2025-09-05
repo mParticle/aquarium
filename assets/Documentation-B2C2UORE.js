898f217f9ed303b1fdffab798897d0d54f53fc41
nastyastavitskaya <stavitskaya.anastasiia@gmail.com>
2025-09-05 03:16:35 +0000

    Deploying to gh-pages from @ mParticle/aquarium@1b91c0850a6110fed61d23ea681f50bca4b82a1d 🚀

---
+++ assets/Documentation-B2C2UORE.js
@@ -0,0 +1,16 @@
+var a=Object.defineProperty,m=Object.defineProperties;var p=Object.getOwnPropertyDescriptors;var s=Object.getOwnPropertySymbols;var c=Object.prototype.hasOwnProperty,u=Object.prototype.propertyIsEnumerable;var i=(n,e,r)=>e in n?a(n,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):n[e]=r,o=(n,e)=>{for(var r in e||(e={}))c.call(e,r)&&i(n,r,e[r]);if(s)for(var r of s(e))u.call(e,r)&&i(n,r,e[r]);return n},d=(n,e)=>m(n,p(e));import{j as t}from"./jsx-runtime-CVnACwZl.js";import{useMDXComponents as l}from"./index-CkQzTg6q.js";import{ae as x,ag as j}from"./index-DwWZIu9p.js";import{T as f,m as g}from"./Tour.stories-Bo2F_CFM.js";import"./index-3OP4wdng.js";import"./iframe-RF-EF0AH.js";import"../sb-preview/runtime.js";import"./index-B-gr4m3q.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-CXAe4HsA.js";import"./index-DtC6qIGv.js";import"./index-DrFu-skq.js";import"./NavigationItemsService-B7Kb2KO7.js";import"./ErrorStateIcon-BEU0OB1a.js";import"./presets-BrZHayqo.js";import"./Context-R5ee3nrF.js";import"./LightTheme-BtoaoHMG.js";import"./row-Bsm_WxsR.js";import"./useToken-C_i1q6Gz.js";function h(n){const e=o(o({a:"a",h1:"h1",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul"},l()),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(x,{of:f}),`
+`,`
+`,t.jsx(e.h1,{id:"tour",children:"Tour"}),`
+`,t.jsx(e.h3,{id:"overview",children:"Overview"}),`
+`,t.jsxs(e.p,{children:["The ",t.jsx(e.strong,{children:"Tour"})," component is a popup  designed to guide users through different areas or features of a product. It provides step-by-step instructions or highlights to help users understand key elements and functionality."]}),`
+`,t.jsx(e.h3,{id:"when-to-use",children:"When to Use"}),`
+`,t.jsx(e.p,{children:"Use the Tour component when:"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"You want to onboard new users by introducing them to important features."}),`
+`,t.jsx(e.li,{children:"Highlighting new or updated features for existing users."}),`
+`,t.jsx(e.li,{children:"Offering guidance for complex workflows or multi-step processes."}),`
+`]}),`
+`,t.jsx(e.h4,{id:"related-links",children:"Related Links"}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Resource"})]})}),t.jsxs(e.tbody,{children:[t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"Eames"}),t.jsx(e.td,{children:t.jsx(e.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=1721-40337&node-type=canvas&t=gOAJGJ8C9ev7N7lt-0",rel:"nofollow",children:"Tour Component"})})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"AntD"}),t.jsx(e.td,{children:t.jsx(e.a,{href:"https://ant.design/components/tour",rel:"nofollow",children:"Tour Component"})})]})]})]}),`
+`,`
+`,t.jsx(j,{meta:g})]})}function z(n={}){const{wrapper:e}=o(o({},l()),n.components);return e?t.jsx(e,d(o({},n),{children:t.jsx(h,o({},n))})):h(n)}export{z as default};
