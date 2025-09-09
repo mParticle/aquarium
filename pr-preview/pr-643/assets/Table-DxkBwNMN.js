a6446e2c7f38531fb465b7ac3827d291a44f7f5d
mparticle-automation <developers@mparticle.com>
2025-09-09 18:56:43 +0000

    Deploy preview for PR 643 🛫

---
+++ pr-preview/pr-643/assets/Table-DxkBwNMN.js
@@ -0,0 +1,24 @@
+var c=Object.defineProperty,p=Object.defineProperties;var h=Object.getOwnPropertyDescriptors;var r=Object.getOwnPropertySymbols;var m=Object.prototype.hasOwnProperty,x=Object.prototype.propertyIsEnumerable;var l=(i,e,n)=>e in i?c(i,e,{enumerable:!0,configurable:!0,writable:!0,value:n}):i[e]=n,a=(i,e)=>{for(var n in e||(e={}))m.call(e,n)&&l(i,n,e[n]);if(r)for(var n of r(e))x.call(e,n)&&l(i,n,e[n]);return i},o=(i,e)=>p(i,h(e));import{j as t}from"./jsx-runtime-CVnACwZl.js";import{useMDXComponents as d}from"./index-CkQzTg6q.js";import{ae as u,af as f}from"./index-WZMJdFtL.js";import{T as j,P as b}from"./Table.stories-D0_qAXvH.js";import"./index-3OP4wdng.js";import"./iframe-D68k3Jm2.js";import"../sb-preview/runtime.js";import"./index-B-gr4m3q.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-CXAe4HsA.js";import"./index-DtC6qIGv.js";import"./index-DrFu-skq.js";import"./NavigationItemsService-B9puW6p-.js";import"./ErrorStateIcon-BEU0OB1a.js";import"./presets-BrZHayqo.js";import"./Context-R5ee3nrF.js";import"./LightTheme-BtoaoHMG.js";import"./row-Bsm_WxsR.js";import"./useToken-C_i1q6Gz.js";import"./DatePicker.stories-8BiuqLcY.js";import"./SelectWithRangePicker-yXVyp-xk.js";function s(i){const e=a(a({a:"a",h1:"h1",h3:"h3",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr"},d()),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(u,{of:j}),`
+`,t.jsx(e.h1,{id:"table",children:"Table"}),`
+`,t.jsx(e.p,{children:`Bug in More Actions
+Move Header, Pagination and Cell Types to the top`}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h3,{id:"overview",children:"Overview"}),`
+`,t.jsxs(e.p,{children:["Tables present and organize data in a structured, readable format, ranging from basic displays to complex interfaces that support queries and data manipulation. You can configure tables using the following elements as needed for your specific use case. mParticle Tables supports multiple cell formats for clear data display. See detailed cell guidelines ",t.jsx(e.a,{href:"#link",children:"here."})]}),`
+`,t.jsx(e.h3,{id:"header",children:"Header"}),`
+`,t.jsx(e.p,{children:"A typical table header contains column titles that describe the data in each column. It includes functionality for sorting columns and may feature help icons with tooltips to provide additional information on each column’s purpose."}),`
+`,t.jsx(e.h3,{id:"cell-types",children:"Cell Types"}),`
+`,t.jsxs(e.p,{children:["mParticle Tables support multiple cell formats for clear data display. See detailed cell guidelines ",t.jsx(e.a,{href:"https://mparticle.github.io/aquarium/?path=/docs/ux-patterns-table-table--cell-text",rel:"nofollow",children:"here."})]}),`
+`,t.jsx(e.h3,{id:"expandable-row",children:"Expandable Row"}),`
+`,t.jsx(e.p,{children:"A table row with expand and collapse functionality, allowing users to toggle additional details or nested content directly within the table."}),`
+`,t.jsx(e.p,{children:"-> Insert Example"}),`
+`,t.jsx(e.h3,{id:"pagination",children:"Pagination"}),`
+`,t.jsx(e.p,{children:"Use pagination in tables to improve performance and reduce load times by fetching only the data needed for the current page."}),`
+`,t.jsx(e.p,{children:"-> Insert Example"}),`
+`,t.jsx(e.h3,{id:"filters",children:"Filters"}),`
+`,t.jsxs(e.p,{children:["Filters help users narrow down large datasets within tables. Learn more about the variety of filter types available in the mParticle table ",t.jsx(e.a,{href:"https://mparticle.github.io/aquarium/?path=/docs/ux-patterns-table-filters--documentation",rel:"nofollow",children:"here."})]}),`
+`,t.jsx(e.h3,{id:"table-example",children:"Table Example"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(f,{of:b}),`
+`,t.jsx(e.h3,{id:"related-links",children:"Related Links"}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Resource"})]})}),t.jsx(e.tbody,{children:t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"Eames"}),t.jsx(e.td,{children:t.jsx(e.a,{href:"https://www.figma.com/design/yCPszaTtkCFxRHaSkkJFR9/Eames-Templates-%5BInternal%5D?node-id=2114-9632&node-type=frame&t=Aa9XxcKyme3ud4cb-0",rel:"nofollow",children:"Figma Templates"})})]})})]})]})}function K(i={}){const{wrapper:e}=a(a({},d()),i.components);return e?t.jsx(e,o(a({},i),{children:t.jsx(s,a({},i))})):s(i)}export{K as default};
