12adbcd07d291a95a4cba93d0849780b2efd6ad8
mparticle-automation <developers@mparticle.com>
2025-09-09 20:10:39 +0000

    Deploy preview for PR 643 🛫

---
+++ pr-preview/pr-643/assets/NavigationItem.stories-CwL5CMKW.js
@@ -0,0 +1,29 @@
+var k=Object.defineProperty,x=Object.defineProperties;var j=Object.getOwnPropertyDescriptors;var l=Object.getOwnPropertySymbols;var R=Object.prototype.hasOwnProperty,f=Object.prototype.propertyIsEnumerable;var p=(a,e,r)=>e in a?k(a,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):a[e]=r,o=(a,e)=>{for(var r in e||(e={}))R.call(e,r)&&p(a,r,e[r]);if(l)for(var r of l(e))f.call(e,r)&&p(a,r,e[r]);return a},t=(a,e)=>x(a,j(e));import{j as n}from"./jsx-runtime-CVnACwZl.js";import{ad as C,I as v}from"./NavigationItemsService-CmBrYWp8.js";import"./ErrorStateIcon-BRZ77WZC.js";import"./_baseClone-CXAe4HsA.js";import"./index-3OP4wdng.js";import"./LightTheme-BtoaoHMG.js";import"./row-Bsm_WxsR.js";import"./Context-R5ee3nrF.js";import"./useToken-C_i1q6Gz.js";import"./presets-BrZHayqo.js";import"./extends-CF3RwP-h.js";import"./index-B-gr4m3q.js";import"./isNativeReflectConstruct-CWIsGhkf.js";const G={title:"Components/Not Prod Ready/Navigation/NavigationItem",component:a=>n.jsx(C,o({},a)),args:{label:"Label"}},s={},i={args:{icon:n.jsx(v,{name:"mpLogo"}),label:"Icon Label",type:"link",onClick:()=>{alert("Redirection!")}}},m={args:{label:"List Label",type:"menu",icon:n.jsx(v,{name:"mpLogo"}),items:[{label:"Nav Item - Menu",type:"menu",children:[{label:"a",type:"link"},{label:"b",type:"link"},{label:"c"}]}]}};var c,b,d;s.parameters=t(o({},s.parameters),{docs:t(o({},(c=s.parameters)==null?void 0:c.docs),{source:o({originalSource:"{}"},(d=(b=s.parameters)==null?void 0:b.docs)==null?void 0:d.source)})});var u,g,y;i.parameters=t(o({},i.parameters),{docs:t(o({},(u=i.parameters)==null?void 0:u.docs),{source:o({originalSource:`{
+  args: {
+    icon: <Icon name="mpLogo" />,
+    label: 'Icon Label',
+    type: 'link',
+    onClick: () => {
+      alert('Redirection!');
+    }
+  }
+}`},(y=(g=i.parameters)==null?void 0:g.docs)==null?void 0:y.source)})});var L,I,N;m.parameters=t(o({},m.parameters),{docs:t(o({},(L=m.parameters)==null?void 0:L.docs),{source:o({originalSource:`{
+  args: {
+    label: 'List Label',
+    type: 'menu',
+    icon: <Icon name="mpLogo" />,
+    items: [{
+      label: 'Nav Item - Menu',
+      type: 'menu',
+      children: [{
+        label: 'a',
+        type: 'link'
+      }, {
+        label: 'b',
+        type: 'link'
+      }, {
+        label: 'c'
+      }]
+    }]
+  }
+}`},(N=(I=m.parameters)==null?void 0:I.docs)==null?void 0:N.source)})});const H=["Primary","NavIcon","NavList"];export{i as NavIcon,m as NavList,s as Primary,H as __namedExportsOrder,G as default};
---
+++ pr-preview/pr-643/assets/Title.stories-C6SMItxZ.js
@@ -0,0 +1,31 @@
+var T=Object.defineProperty,h=Object.defineProperties;var d=Object.getOwnPropertyDescriptors;var m=Object.getOwnPropertySymbols;var g=Object.prototype.hasOwnProperty,b=Object.prototype.propertyIsEnumerable;var p=(t,e,r)=>e in t?T(t,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):t[e]=r,a=(t,e)=>{for(var r in e||(e={}))g.call(e,r)&&p(t,r,e[r]);if(m)for(var r of m(e))b.call(e,r)&&p(t,r,e[r]);return t},o=(t,e)=>h(t,d(e));import{j as i}from"./jsx-runtime-CVnACwZl.js";import{e as n}from"./NavigationItemsService-CmBrYWp8.js";import{E as c}from"./ExampleStory-BllIhssv.js";import"./index-3OP4wdng.js";import"./ErrorStateIcon-BRZ77WZC.js";import"./extends-CF3RwP-h.js";import"./presets-BrZHayqo.js";import"./Context-R5ee3nrF.js";import"./LightTheme-BtoaoHMG.js";import"./row-Bsm_WxsR.js";import"./useToken-C_i1q6Gz.js";import"./index-B-gr4m3q.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-CXAe4HsA.js";const O={title:"Components/General/Typography/Title",component:n.Title},l={render:()=>i.jsxs(c,{title:"Different Headings.",children:[i.jsx(n.Title,{editable:!0,level:1,style:{margin:0},children:"h1. Aquarium Component Library"}),i.jsx(n.Title,{editable:!0,level:2,style:{margin:0},children:"h2. Aquarium Component Library"}),i.jsx(n.Title,{editable:!0,level:3,style:{margin:0},children:"h3. Aquarium Component Library"}),i.jsx(n.Title,{editable:!0,level:4,style:{margin:0},children:"h4. Aquarium Component Library"}),i.jsx(n.Title,{editable:!0,level:5,style:{margin:0},children:"h5. Aquarium Component Library"})]})};var y,s,u;l.parameters=o(a({},l.parameters),{docs:o(a({},(y=l.parameters)==null?void 0:y.docs),{source:a({originalSource:`{
+  render: () => {
+    return <ExampleStory title="Different Headings.">
+        <Typography.Title editable level={1} style={{
+        margin: 0
+      }}>
+          h1. Aquarium Component Library
+        </Typography.Title>
+        <Typography.Title editable level={2} style={{
+        margin: 0
+      }}>
+          h2. Aquarium Component Library
+        </Typography.Title>
+        <Typography.Title editable level={3} style={{
+        margin: 0
+      }}>
+          h3. Aquarium Component Library
+        </Typography.Title>
+        <Typography.Title editable level={4} style={{
+        margin: 0
+      }}>
+          h4. Aquarium Component Library
+        </Typography.Title>
+        <Typography.Title editable level={5} style={{
+        margin: 0
+      }}>
+          h5. Aquarium Component Library
+        </Typography.Title>
+      </ExampleStory>;
+  }
+}`},(u=(s=l.parameters)==null?void 0:s.docs)==null?void 0:u.source)})});const R=["Primary"];export{l as Primary,R as __namedExportsOrder,O as default};
