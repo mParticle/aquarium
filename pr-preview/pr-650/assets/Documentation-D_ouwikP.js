73af8d65911942151e96f9feb0ac3671872899b2
mparticle-automation <developers@mparticle.com>
2025-09-15 15:00:04 +0000

    Deploy preview for PR 650 🛫

---
+++ pr-preview/pr-650/assets/Documentation-D_ouwikP.js
@@ -0,0 +1,14 @@
+var h=Object.defineProperty,p=Object.defineProperties;var m=Object.getOwnPropertyDescriptors;var s=Object.getOwnPropertySymbols;var c=Object.prototype.hasOwnProperty,x=Object.prototype.propertyIsEnumerable;var i=(o,e,t)=>e in o?h(o,e,{enumerable:!0,configurable:!0,writable:!0,value:t}):o[e]=t,r=(o,e)=>{for(var t in e||(e={}))c.call(e,t)&&i(o,t,e[t]);if(s)for(var t of s(e))x.call(e,t)&&i(o,t,e[t]);return o},d=(o,e)=>p(o,m(e));import{j as n}from"./jsx-runtime-CVnACwZl.js";import{useMDXComponents as a}from"./index-CkQzTg6q.js";import{ae as j,ag as u}from"./index-yET1rcpp.js";import{U as f,m as g}from"./Upload.stories-Ug42yfbQ.js";import"./index-3OP4wdng.js";import"./iframe-BhTGXhKX.js";import"../sb-preview/runtime.js";import"./index-B-gr4m3q.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-CXAe4HsA.js";import"./index-DtC6qIGv.js";import"./index-DrFu-skq.js";import"./NavigationItemsService-CwiLuB8p.js";import"./ErrorStateIcon-DUz035cB.js";import"./presets-BrZHayqo.js";import"./Context-R5ee3nrF.js";import"./LightTheme-BtoaoHMG.js";import"./row-Bsm_WxsR.js";import"./useToken-C_i1q6Gz.js";function l(o){const e=r(r({a:"a",h1:"h1",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul"},a()),o.components);return n.jsxs(n.Fragment,{children:[n.jsx(j,{of:f}),`
+`,`
+`,n.jsx(e.h1,{id:"upload",children:"Upload"}),`
+`,n.jsx(e.h4,{id:"overview",children:"Overview"}),`
+`,n.jsx(e.p,{children:"Used to select and upload files or drag and drop files."}),`
+`,n.jsx(e.h4,{id:"when-to-use",children:n.jsx(e.strong,{children:"When To Use"})}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"When you need to upload one or more files."}),`
+`,n.jsx(e.li,{children:"When you want to allow file uploads via drag-and-drop."}),`
+`,n.jsx(e.li,{children:"When you need to display the upload process (e.g., progress indicators)."}),`
+`]}),`
+`,n.jsx(e.h4,{id:"related-links",children:"Related Links"}),`
+`,n.jsxs(e.table,{children:[n.jsx(e.thead,{children:n.jsxs(e.tr,{children:[n.jsx(e.th,{children:"Type"}),n.jsx(e.th,{children:"Resource"})]})}),n.jsxs(e.tbody,{children:[n.jsxs(e.tr,{children:[n.jsx(e.td,{children:"Eames"}),n.jsx(e.td,{children:n.jsx(e.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=396-13801&p=f&t=qTxTI3nmyrN7o7DQ-0",rel:"nofollow",children:"Upload Component"})})]}),n.jsxs(e.tr,{children:[n.jsx(e.td,{children:"AntD"}),n.jsx(e.td,{children:n.jsx(e.a,{href:"https://ant.design/components/upload",rel:"nofollow",children:"Upload Component"})})]})]})]}),`
+`,n.jsx(u,{meta:g})]})}function z(o={}){const{wrapper:e}=r(r({},a()),o.components);return e?n.jsx(e,d(r({},o),{children:n.jsx(l,r({},o))})):l(o)}export{z as default};
