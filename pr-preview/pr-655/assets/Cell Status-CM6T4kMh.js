186231bfc35ab0e0e8d5ff21404db3318ad30869
mparticle-automation <developers@mparticle.com>
2025-10-03 20:21:57 +0000

    Deploy preview for PR 655 🛫

---
+++ pr-preview/pr-655/assets/Cell Status-CM6T4kMh.js
@@ -0,0 +1,9 @@
+var d=Object.defineProperty,h=Object.defineProperties;var m=Object.getOwnPropertyDescriptors;var a=Object.getOwnPropertySymbols;var u=Object.prototype.hasOwnProperty,x=Object.prototype.propertyIsEnumerable;var o=(n,e,s)=>e in n?d(n,e,{enumerable:!0,configurable:!0,writable:!0,value:s}):n[e]=s,i=(n,e)=>{for(var s in e||(e={}))u.call(e,s)&&o(n,s,e[s]);if(a)for(var s of a(e))x.call(e,s)&&o(n,s,e[s]);return n},r=(n,e)=>h(n,m(e));import{j as t}from"./jsx-runtime-CVnACwZl.js";import{useMDXComponents as c}from"./index-CkQzTg6q.js";import"./index-3OP4wdng.js";function l(n){const e=i(i({h1:"h1",h3:"h3",h4:"h4",p:"p"},c()),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(e.h1,{id:"table-cells",children:"Table Cells"}),`
+`,t.jsx(e.h3,{id:"status",children:"Status"}),`
+`,t.jsx(e.p,{children:"Always use badge components for system status. Reserve tags for actual tags, labels, and environment descriptions (e.g., prod, dev). This ensures clear visual distinction between different information types."}),`
+`,t.jsx(e.h4,{id:"do",children:"Do"}),`
+`,t.jsx("img",{src:"images/basic-table.png"}),`
+`,t.jsx(e.h4,{id:"dont",children:"Don't"}),`
+`,t.jsx("img",{src:"images/basic-table.png"}),`
+`,t.jsx(e.h3,{id:"cell-aligment",children:"Cell Aligment"}),`
+`,t.jsx(e.p,{children:"Statuses are left-aligned to create a cohesive and organized table layout."})]})}function b(n={}){const{wrapper:e}=i(i({},c()),n.components);return e?t.jsx(e,r(i({},n),{children:t.jsx(l,i({},n))})):l(n)}export{b as default};
---
+++ pr-preview/pr-655/assets/Documentation-CMBiGBAK.js
@@ -0,0 +1,21 @@
+var h=Object.defineProperty,c=Object.defineProperties;var m=Object.getOwnPropertyDescriptors;var r=Object.getOwnPropertySymbols;var p=Object.prototype.hasOwnProperty,x=Object.prototype.propertyIsEnumerable;var o=(e,t,i)=>t in e?h(e,t,{enumerable:!0,configurable:!0,writable:!0,value:i}):e[t]=i,s=(e,t)=>{for(var i in t||(t={}))p.call(t,i)&&o(e,i,t[i]);if(r)for(var i of r(t))x.call(t,i)&&o(e,i,t[i]);return e},l=(e,t)=>c(e,m(t));import{j as n}from"./jsx-runtime-CVnACwZl.js";import{useMDXComponents as d}from"./index-CkQzTg6q.js";import{ae as j,ag as u}from"./index-B0rh8BjL.js";import{L as g,m as f}from"./List.stories-jvWvO6L0.js";import"./index-3OP4wdng.js";import"./iframe-B39sh5fZ.js";import"../sb-preview/runtime.js";import"./index-B-gr4m3q.js";import"./index-J1TP6alb.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-CXAe4HsA.js";import"./index-DtC6qIGv.js";import"./index-DrFu-skq.js";import"./NavigationItemsService-CnBgOCr2.js";import"./ErrorStateIcon-w4D1x5VF.js";import"./style-DZvg_om7.js";import"./presets-BrZHayqo.js";import"./Context-R5ee3nrF.js";import"./WarningFilled-CZ1UPkPI.js";import"./SuccessStateIcon-D6uerxEu.js";import"./WarningStateIcon-DR3bdROO.js";import"./LightTheme-BtoaoHMG.js";import"./row-Bsm_WxsR.js";import"./useToken-C_i1q6Gz.js";import"./ExampleStory-3zol_NUt.js";function a(e){const t=s(s({a:"a",h1:"h1",h2:"h2",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul"},d()),e.components);return n.jsxs(n.Fragment,{children:[n.jsx(j,{of:g}),`
+`,n.jsx(t.h1,{id:"list",children:"List"}),`
+`,n.jsx(t.h4,{id:"overview",children:"Overview"}),`
+`,n.jsxs(t.p,{children:["The ",n.jsx(t.strong,{children:"List"})," component is a simple, versatile container for displaying multiple items in a single view. List can hold various types of content, providing a clean and organized way to present information."]}),`
+`,n.jsx(t.h3,{id:"when-to-use",children:"When to Use"}),`
+`,n.jsxs(t.ul,{children:[`
+`,n.jsx(t.li,{children:"To display a collection of data in a structured format."}),`
+`,n.jsx(t.li,{children:"When presenting lists of settings"}),`
+`]}),`
+`,n.jsx(t.h2,{id:"key-features",children:"Key Features"}),`
+`,n.jsxs(t.ul,{children:[`
+`,n.jsxs(t.li,{children:[n.jsx(t.strong,{children:"Customizable Headers and Footers:"})," Add headers and footers to provide context or summary for the list."]}),`
+`,n.jsxs(t.li,{children:[n.jsx(t.strong,{children:"Flexible List Item Structure:"})," Layouts can include text, images, and actions, offering versatility in content presentation."]}),`
+`,n.jsxs(t.li,{children:[n.jsx(t.strong,{children:"Pagination:"}),"  Built-in support for pagination to efficiently manage large data sets."]}),`
+`,n.jsxs(t.li,{children:[n.jsx(t.strong,{children:"Load More:"}),'  An optional "Load More" button for dynamically loading additional items.']}),`
+`,n.jsxs(t.li,{children:[n.jsx(t.strong,{children:"Grid Support:"}),"  Display list items in grid layouts for a more visual and organized presentation."]}),`
+`,n.jsxs(t.li,{children:[n.jsx(t.strong,{children:"Interactive Elements:"}),"  Integrate buttons, links, or select into each list item"]}),`
+`]}),`
+`,n.jsx(t.h4,{id:"related-links",children:"Related Links"}),`
+`,n.jsxs(t.table,{children:[n.jsx(t.thead,{children:n.jsxs(t.tr,{children:[n.jsx(t.th,{children:"Type"}),n.jsx(t.th,{children:"Resource"})]})}),n.jsxs(t.tbody,{children:[n.jsxs(t.tr,{children:[n.jsx(t.td,{children:"Eames"}),n.jsx(t.td,{children:n.jsx(t.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=407-405&node-type=canvas&t=B6HJWqqDsUOypZQj-0",rel:"nofollow",children:"List Component"})})]}),n.jsxs(t.tr,{children:[n.jsx(t.td,{children:"AntD"}),n.jsx(t.td,{children:n.jsx(t.a,{href:"https://ant.design/components/list",rel:"nofollow",children:"List Component"})})]})]})]}),`
+`,n.jsx(u,{meta:f})]})}function G(e={}){const{wrapper:t}=s(s({},d()),e.components);return t?n.jsx(t,l(s({},e),{children:n.jsx(a,s({},e))})):a(e)}export{G as default};
