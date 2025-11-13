0f84e378be81928d8db510877f89b1fede3820d9
mparticle-automation <developers@mparticle.com>
2025-11-13 23:42:03 +0000

    Deploy preview for PR 625 🛫

---
+++ pr-preview/pr-625/assets/Message.stories-DuMh4Oej.js
@@ -0,0 +1,22 @@
+var b=Object.defineProperty,_=Object.defineProperties;var h=Object.getOwnPropertyDescriptors;var l=Object.getOwnPropertySymbols;var j=Object.prototype.hasOwnProperty,P=Object.prototype.propertyIsEnumerable;var d=(o,s,r)=>s in o?b(o,s,{enumerable:!0,configurable:!0,writable:!0,value:r}):o[s]=r,e=(o,s)=>{for(var r in s||(s={}))j.call(s,r)&&d(o,r,s[r]);if(l)for(var r of l(s))P.call(s,r)&&d(o,r,s[r]);return o},a=(o,s)=>_(o,h(s));import{j as z}from"./jsx-runtime-C9TrHvcC.js";import{H as O,e as E}from"./NavigationItemsService-D3Vjxi4v.js";const w={title:"Components/Feedback/Message",component:O,args:{children:z.jsx(E,{children:"Show Message"}),className:void 0,content:"Message content",duration:3,icon:void 0,key:void 0,style:void 0,onClick:void 0,onClose:void 0}},t={args:{type:"success"}},n={args:{duration:1}},c={args:{style:{fontSize:"24px",color:"#959ee6"}}},i={args:{onClick:()=>{alert("Message clicked")}}};var m,p,u;t.parameters=a(e({},t.parameters),{docs:a(e({},(m=t.parameters)==null?void 0:m.docs),{source:e({originalSource:`{
+  args: {
+    type: 'success'
+  }
+}`},(u=(p=t.parameters)==null?void 0:p.docs)==null?void 0:u.source)})});var g,S,y;n.parameters=a(e({},n.parameters),{docs:a(e({},(g=n.parameters)==null?void 0:g.docs),{source:e({originalSource:`{
+  args: {
+    duration: 1
+  }
+}`},(y=(S=n.parameters)==null?void 0:S.docs)==null?void 0:y.source)})});var k,C,M;c.parameters=a(e({},c.parameters),{docs:a(e({},(k=c.parameters)==null?void 0:k.docs),{source:e({originalSource:`{
+  args: {
+    style: {
+      fontSize: '24px',
+      color: '#959ee6'
+    }
+  }
+}`},(M=(C=c.parameters)==null?void 0:C.docs)==null?void 0:M.source)})});var f,v,x;i.parameters=a(e({},i.parameters),{docs:a(e({},(f=i.parameters)==null?void 0:f.docs),{source:e({originalSource:`{
+  args: {
+    onClick: () => {
+      alert('Message clicked');
+    }
+  }
+}`},(x=(v=i.parameters)==null?void 0:v.docs)==null?void 0:x.source)})});const B=["Primary","Short","CustomStyle","Clickable"],R=Object.freeze(Object.defineProperty({__proto__:null,Clickable:i,CustomStyle:c,Primary:t,Short:n,__namedExportsOrder:B,default:w},Symbol.toStringTag,{value:"Module"}));export{R as M,t as P};
---
+++ pr-preview/pr-625/assets/Text.stories-D_aBm9Ub.js
@@ -0,0 +1,11 @@
+var b=Object.defineProperty,g=Object.defineProperties;var T=Object.getOwnPropertyDescriptors;var i=Object.getOwnPropertySymbols;var f=Object.prototype.hasOwnProperty,D=Object.prototype.propertyIsEnumerable;var p=(r,e,t)=>e in r?b(r,e,{enumerable:!0,configurable:!0,writable:!0,value:t}):r[e]=t,o=(r,e)=>{for(var t in e||(e={}))f.call(e,t)&&p(r,t,e[t]);if(i)for(var t of i(e))D.call(e,t)&&p(r,t,e[t]);return r},a=(r,e)=>g(r,T(e));import{Q as y}from"./NavigationItemsService-D3Vjxi4v.js";import"./ErrorStateIcon-Clx5lk_q.js";import"./SuccessStateIcon-CrBP74im.js";import"./WarningStateIcon-irkX5OVh.js";import"./_baseClone-DwBCp-Ss.js";import"./jsx-runtime-C9TrHvcC.js";import"./index-3OP4wdng.js";import"./LightTheme-BtoaoHMG.js";import"./row-DlEJUZa0.js";import"./Context-R5ee3nrF.js";import"./useToken-BIYN7ogo.js";import"./presets-CQzyf-6F.js";import"./extends-CF3RwP-h.js";import"./index-DA79H6I3.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./AntdIcon-DRQs8XP6.js";import"./style-BvWo89XW.js";import"./WarningFilled-D6ijQqgK.js";const H={title:"Components/Data Entry/QueryItem/Text",component:y.Text,parameters:{docs:{description:{component:'This is the "Text" component of the QueryItem component group.'}}},args:{}},s={args:{text:"Default Text",disabled:!1}},m={args:{text:"Disabled Text",disabled:!0}};var n,d,c;s.parameters=a(o({},s.parameters),{docs:a(o({},(n=s.parameters)==null?void 0:n.docs),{source:o({originalSource:`{
+  args: {
+    text: 'Default Text',
+    disabled: false
+  }
+}`},(c=(d=s.parameters)==null?void 0:d.docs)==null?void 0:c.source)})});var l,u,x;m.parameters=a(o({},m.parameters),{docs:a(o({},(l=m.parameters)==null?void 0:l.docs),{source:o({originalSource:`{
+  args: {
+    text: 'Disabled Text',
+    disabled: true
+  }
+}`},(x=(u=m.parameters)==null?void 0:u.docs)==null?void 0:x.source)})});const J=["Primary","Disabled"];export{m as Disabled,s as Primary,J as __namedExportsOrder,H as default};
