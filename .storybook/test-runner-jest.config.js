68217a7b7128902617d5c3171583ef2fef4ab1da
astavitskaya <astavitskaya@mparticle.com>
2025-11-13 15:17:10 -0800

    chore: cursor do its thing

--- .storybook/test-runner-jest.config.js
+++ .storybook/test-runner-jest.config.js
@@ -4,8 +4,6 @@ const { getJestConfig } = require('@storybook/test-runner');
  * @type {import('@jest/types').Config.InitialOptions}
  */
 module.exports = {
-  // The default configuration comes from @storybook/test-runner
   ...getJestConfig(),
-  /** Add your own overrides below, learn more from Storybook documentation */
 };
 
