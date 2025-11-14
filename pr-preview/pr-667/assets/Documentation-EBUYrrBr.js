5ca18f1414b9854699f3619d9b5448b13d8d2103
mparticle-automation <developers@mparticle.com>
2025-11-14 02:37:49 +0000

    Deploy preview for PR 667 🛫

---
+++ pr-preview/pr-667/assets/Documentation-EBUYrrBr.js
@@ -0,0 +1,23 @@
+var c=Object.defineProperty,p=Object.defineProperties;var x=Object.getOwnPropertyDescriptors;var i=Object.getOwnPropertySymbols;var j=Object.prototype.hasOwnProperty,f=Object.prototype.propertyIsEnumerable;var a=(n,e,s)=>e in n?c(n,e,{enumerable:!0,configurable:!0,writable:!0,value:s}):n[e]=s,o=(n,e)=>{for(var s in e||(e={}))j.call(e,s)&&a(n,s,e[s]);if(i)for(var s of i(e))f.call(e,s)&&a(n,s,e[s]);return n},l=(n,e)=>p(n,x(e));import{j as t}from"./jsx-runtime-C9TrHvcC.js";import{useMDXComponents as m}from"./index-CkQzTg6q.js";import{ae as u,af as d,ag as r}from"./index-DXLFhhLb.js";import{A as g,S as y,a as w,I as b,W as A,E as k,b as v,c as C,d as L,e as W,f as E,N as S}from"./Alert.stories-BF1q_zLf.js";import"./index-3OP4wdng.js";import"./iframe-YKr9JEvX.js";import"../sb-preview/runtime.js";import"./index-DA79H6I3.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DwBCp-Ss.js";import"./index-dQMQm6Vu.js";import"./index-DrFu-skq.js";import"./NavigationItemsService-BeUgPi48.js";import"./ErrorStateIcon-Clx5lk_q.js";import"./style-BvWo89XW.js";import"./WarningFilled-D6ijQqgK.js";import"./AntdIcon-DRQs8XP6.js";import"./presets-CQzyf-6F.js";import"./Context-R5ee3nrF.js";import"./SuccessStateIcon-CrBP74im.js";import"./WarningStateIcon-irkX5OVh.js";import"./LightTheme-BtoaoHMG.js";import"./row-DlEJUZa0.js";import"./useToken-BIYN7ogo.js";function h(n){const e=o(o({a:"a",h1:"h1",h3:"h3",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr"},m()),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(u,{of:g}),`
+`,t.jsx(e.h1,{id:"alert",children:"Alert"}),`
+`,t.jsx(e.h3,{id:"overview",children:"Overview"}),`
+`,t.jsx(e.p,{children:"Alerts are persistent UI components used to communicate important information to users. They remain visible on the page until manually dismissed or programmatically cleared. Alerts may be used to inform users of relevant system or process updates that don't require immediate action, or to prompt them to acknowledge or resolve an issue before proceeding."}),`
+`,t.jsx(d,{of:y}),`
+`,t.jsx(e.h3,{id:"alert-types",children:"Alert Types"}),`
+`,t.jsx(e.p,{children:"Four alert types are supported, each with distinct visual styling to convey the message's intent and importance: success, info, warning, and error."}),`
+`,t.jsx(r,{of:w}),`
+`,t.jsx(r,{of:b}),`
+`,t.jsx(r,{of:A}),`
+`,t.jsx(r,{of:k}),`
+`,t.jsx(e.h3,{id:"closable-alerts",children:"Closable Alerts"}),`
+`,t.jsx(e.p,{children:"Alerts can be closable or not, depending on whether users need to dismiss them."}),`
+`,t.jsx(r,{of:v}),`
+`,t.jsx(e.h3,{id:"alerts-with-links",children:"Alerts with Links"}),`
+`,t.jsx(e.p,{children:"Alerts can include links within the message text. Links should be styled with text color and underlined to maintain consistency with the alert's visual design."}),`
+`,t.jsx(r,{of:C}),`
+`,t.jsx(r,{of:L}),`
+`,t.jsx(r,{of:W}),`
+`,t.jsx(r,{of:E}),`
+`,t.jsx(d,{of:S}),`
+`,t.jsx(e.h3,{id:"related-links",children:"Related Links"}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Resource"})]})}),t.jsxs(e.tbody,{children:[t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"Eames"}),t.jsx(e.td,{children:t.jsx(e.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=413-15306&p=f&t=1Pi9thxrLJt7TsHN-0",rel:"nofollow",children:"Alert Component"})})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"AntD"}),t.jsx(e.td,{children:t.jsx(e.a,{href:"https://ant.design/components/alert",rel:"nofollow",children:"Alert Component"})})]})]})]})]})}function se(n={}){const{wrapper:e}=o(o({},m()),n.components);return e?t.jsx(e,l(o({},n),{children:t.jsx(h,o({},n))})):h(n)}export{se as default};
