1359b9a0e6ee8b55d3c982fa30561744e18a9e90
mparticle-automation <developers@mparticle.com>
2025-11-20 04:43:57 +0000

    Deploy preview for PR 668 🛫

---
+++ pr-preview/pr-668/assets/PopoverNotProdReady.stories-CGljT8fY.js
@@ -0,0 +1,13 @@
+var j=Object.defineProperty,P=Object.defineProperties;var f=Object.getOwnPropertyDescriptors;var m=Object.getOwnPropertySymbols;var y=Object.prototype.hasOwnProperty,F=Object.prototype.propertyIsEnumerable;var p=(e,r,o)=>r in e?j(e,r,{enumerable:!0,configurable:!0,writable:!0,value:o}):e[r]=o,t=(e,r)=>{for(var o in r||(r={}))y.call(r,o)&&p(e,o,r[o]);if(m)for(var o of m(r))F.call(r,o)&&p(e,o,r[o]);return e},n=(e,r)=>P(e,f(r));import{j as s}from"./jsx-runtime-C9TrHvcC.js";import{P as v,e as H}from"./NavigationItemsService-DZAgWDXG.js";import"./index-3OP4wdng.js";import"./ErrorStateIcon-CT-EOGJH.js";import"./style-NCxQVXRU.js";import"./WarningFilled-D6ijQqgK.js";import"./extends-CF3RwP-h.js";import"./AntdIcon-DRQs8XP6.js";import"./presets-CQzyf-6F.js";import"./Context-R5ee3nrF.js";import"./SuccessStateIcon-ucrlAZbi.js";import"./WarningStateIcon-DCUpTCq_.js";import"./row-DlEJUZa0.js";import"./useToken-BIYN7ogo.js";import"./index-DA79H6I3.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DwBCp-Ss.js";const I={title:"Components/Not Prod Ready/Data Display/Popover",component:v,args:{}},x=e=>{const r=s.jsx("div",{children:s.jsx("p",{children:"Content"})});return s.jsx(s.Fragment,{children:s.jsx(v,{content:r,title:"Title",trigger:e.trigger,children:s.jsx(H,{children:e.children})})})},i={args:{trigger:"hover",children:"Hover Me"},render:x},a={args:{trigger:"focus",children:"Focus Me"},render:x};var c,d,l;i.parameters=n(t({},i.parameters),{docs:n(t({},(c=i.parameters)==null?void 0:c.docs),{source:t({originalSource:`{
+  args: {
+    trigger: 'hover',
+    children: 'Hover Me'
+  },
+  render: PrimaryTemplate
+}`},(l=(d=i.parameters)==null?void 0:d.docs)==null?void 0:l.source)})});var g,u,h;a.parameters=n(t({},a.parameters),{docs:n(t({},(g=a.parameters)==null?void 0:g.docs),{source:t({originalSource:`{
+  args: {
+    trigger: 'focus',
+    children: 'Focus Me'
+  },
+  render: PrimaryTemplate
+}`},(h=(u=a.parameters)==null?void 0:u.docs)==null?void 0:h.source)})});const J=["Hover","Focus"];export{a as Focus,i as Hover,J as __namedExportsOrder,I as default};
