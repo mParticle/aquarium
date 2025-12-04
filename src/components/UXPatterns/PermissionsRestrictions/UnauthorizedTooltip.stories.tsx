5f9e07e8ce2a54cffdeecb299db496f6e8f555c0
skosemP <skose@mparticle.com>
2025-12-04 15:41:03 -0500

    feat: permissions restrictions (#665)
    Co-authored-by: Anastasiia Stavitskaya <stavitskaya.anastasiia@gmail.com>
    Co-authored-by: astavitskaya <astavitskaya@mparticle.com>

---
+++ src/components/UXPatterns/PermissionsRestrictions/UnauthorizedTooltip.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { useState } from 'react'
+import { UnauthorizedTooltip } from './UnauthorizedTooltip'
+import { Button, Radio, Typography } from 'src/components'
+import { Margin } from 'src/styles/style'
+
+const meta: Meta<typeof UnauthorizedTooltip> = {
+  title: 'UX Patterns/PermissionsRestrictions',
+  component: UnauthorizedTooltip,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    isAuthorized: {
+      control: 'boolean',
+      description: 'Whether the user is authorized to perform the action',
+      defaultValue: false,
+    },
+    unauthorizedMessage: {
+      control: 'text',
+      description: 'Custom message to display when unauthorized',
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof UnauthorizedTooltip>
+
+export const DisabledButton: Story = {
+  args: {
+    isAuthorized: false,
+    children: <Button disabled>Delete</Button>,
+  },
+}
+
+export const ModalExample: Story = {
+  render: () => {
+    const [value, setValue] = useState('option2')
+
+    const unauthorizedMessage = (
+      <>
+        <Typography.Text tooltip>
+          You don&apos;t have permissions for this action.{' '}
+          <Typography.Link href="/" tooltip>
+            Learn More
+          </Typography.Link>
+        </Typography.Text>
+      </>
+    )
+
+    return (
+      <Radio.Group value={value} onChange={e => setValue(e.target.value as string)}>
+        <div style={{ marginBottom: Margin }}>
+          <UnauthorizedTooltip isAuthorized={false} unauthorizedMessage={unauthorizedMessage} placement="right">
+            <Radio value="option1" disabled>
+              Option 1
+            </Radio>
+          </UnauthorizedTooltip>
+        </div>
+        <Radio value="option2">Option 2</Radio>
+      </Radio.Group>
+    )
+  },
+}
