f7be9b371d458f7dab57304e2383f40894f5a237
mparticle-automation <developers@mparticle.com>
2025-12-05 15:44:24 +0000

    Deploy preview for PR 672 🛫

---
+++ pr-preview/pr-672/assets/Input.stories-BJ56npeI.js
@@ -0,0 +1,26 @@
+var T=Object.defineProperty,y=Object.defineProperties;var A=Object.getOwnPropertyDescriptors;var m=Object.getOwnPropertySymbols;var w=Object.prototype.hasOwnProperty,E=Object.prototype.propertyIsEnumerable;var h=(e,r,a)=>r in e?T(e,r,{enumerable:!0,configurable:!0,writable:!0,value:a}):e[r]=a,t=(e,r)=>{for(var a in r||(r={}))w.call(r,a)&&h(e,a,r[a]);if(m)for(var a of m(r))E.call(r,a)&&h(e,a,r[a]);return e},o=(e,r)=>y(e,A(r));import{j as c}from"./jsx-runtime-C9TrHvcC.js";import{r as _}from"./index-3OP4wdng.js";import{t as u,a as b,I as P}from"./NavigationItemsService-C3iTtC8K.js";import"./ErrorStateIcon-BEuADFVm.js";import"./SuccessStateIcon--D9CT8eu.js";import"./WarningStateIcon-BCrS_vH5.js";import"./_baseClone-DwBCp-Ss.js";const B={title:"Components/Data Entry/Input",component:u,args:{placeholder:"Input Placeholder",disabled:!1,maxLength:void 0,status:void 0,size:"middle",type:"text",addonBefore:void 0,allowClear:!1,value:"",onChange:e=>{console.log("Input changed: "+e.target.value)},onPressEnter:e=>{console.log("Enter key pressed: "+String(e))}},argTypes:{maxLength:{control:"number"},status:{control:"select",options:["warning","error","default"]},size:{control:"select",options:["small","medium","large"]}}},n={args:{value:""},render:e=>{var d;const[r,a]=_.useState((d=e.value)!=null?d:"");return c.jsx(u,o(t({},e),{value:r,onChange:p=>{var i;a(p.target.value),(i=e.onChange)==null||i.call(e,p)}}))}},s={render:()=>c.jsx(u.TextArea,{size:"middle",autoSize:{minRows:2},placeholder:"Description (optional)",children:"Test"})},l={render:()=>c.jsx(b,{children:c.jsx(u,{placeholder:"Search",allowClear:!0,autoFocus:!0,addonBefore:c.jsx(P,{name:"search",size:"sm",color:"default"})})})};var g,x,S;n.parameters=o(t({},n.parameters),{docs:o(t({},(g=n.parameters)==null?void 0:g.docs),{source:t({originalSource:`{
+  args: {
+    value: ''
+  },
+  render: args => {
+    const [value, setValue] = useState(args.value ?? '');
+    return <Input {...args} value={value} onChange={e => {
+      setValue(e.target.value);
+      args.onChange?.(e);
+    }} />;
+  }
+}`},(S=(x=n.parameters)==null?void 0:x.docs)==null?void 0:S.source)})});var v,f,I;s.parameters=o(t({},s.parameters),{docs:o(t({},(v=s.parameters)==null?void 0:v.docs),{source:t({originalSource:`{
+  render: () => {
+    return <Input.TextArea size="middle" autoSize={{
+      minRows: 2
+    }} placeholder="Description (optional)">
+        Test
+      </Input.TextArea>;
+  }
+}`},(I=(f=s.parameters)==null?void 0:f.docs)==null?void 0:I.source)})});var j,z,C;l.parameters=o(t({},l.parameters),{docs:o(t({},(j=l.parameters)==null?void 0:j.docs),{source:t({originalSource:`{
+  render: () => {
+    return <Space>
+        <Input placeholder="Search" allowClear autoFocus addonBefore={<Icon name="search" size="sm" color="default" />} />
+      </Space>;
+  }
+}`},(C=(z=l.parameters)==null?void 0:z.docs)==null?void 0:C.source)})});const D=["Primary","TextArea","WithSearchAddon"],q=Object.freeze(Object.defineProperty({__proto__:null,Primary:n,TextArea:s,WithSearchAddon:l,__namedExportsOrder:D,default:B},Symbol.toStringTag,{value:"Module"}));export{q as I,B as m};
---
+++ pr-preview/pr-672/assets/UnauthorizedTooltip.stories-DaUqrpOy.js
@@ -0,0 +1,30 @@
+var v=Object.defineProperty,T=Object.defineProperties;var j=Object.getOwnPropertyDescriptors;var l=Object.getOwnPropertySymbols;var M=Object.prototype.hasOwnProperty,b=Object.prototype.propertyIsEnumerable;var u=(t,e,o)=>e in t?v(t,e,{enumerable:!0,configurable:!0,writable:!0,value:o}):t[e]=o,a=(t,e)=>{for(var o in e||(e={}))M.call(e,o)&&u(t,o,e[o]);if(l)for(var o of l(e))b.call(e,o)&&u(t,o,e[o]);return t},s=(t,e)=>T(t,j(e));import{j as r}from"./jsx-runtime-C9TrHvcC.js";import{r as R}from"./index-3OP4wdng.js";import{a6 as y,e as B,R as d,c as p}from"./NavigationItemsService-C3iTtC8K.js";import"./ErrorStateIcon-BEuADFVm.js";import"./SuccessStateIcon--D9CT8eu.js";import"./WarningStateIcon-BCrS_vH5.js";import"./_baseClone-DwBCp-Ss.js";import{s as O}from"./style-B_XLJ90u.js";const S={title:"UX Patterns/PermissionsRestrictions",component:y,parameters:{layout:"centered"},argTypes:{isAuthorized:{control:"boolean",description:"Whether the user is authorized to perform the action",defaultValue:!1},unauthorizedMessage:{control:"text",description:"Custom message to display when unauthorized"}}},i={args:{isAuthorized:!1,children:r.jsx(B,{disabled:!0,children:"Delete"})}},n={render:()=>{const[t,e]=R.useState("option2"),o=r.jsx(r.Fragment,{children:r.jsxs(p.Text,{tooltip:!0,children:["You don't have permissions for this action."," ",r.jsx(p.Link,{href:"/",tooltip:!0,children:"Learn More"})]})});return r.jsxs(d.Group,{value:t,onChange:z=>e(z.target.value),children:[r.jsx("div",{style:{marginBottom:O},children:r.jsx(y,{isAuthorized:!1,unauthorizedMessage:o,placement:"right",children:r.jsx(d,{value:"option1",disabled:!0,children:"Option 1"})})}),r.jsx(d,{value:"option2",children:"Option 2"})]})}};var c,h,m;i.parameters=s(a({},i.parameters),{docs:s(a({},(c=i.parameters)==null?void 0:c.docs),{source:a({originalSource:`{
+  args: {
+    isAuthorized: false,
+    children: <Button disabled>Delete</Button>
+  }
+}`},(m=(h=i.parameters)==null?void 0:h.docs)==null?void 0:m.source)})});var g,x,f;n.parameters=s(a({},n.parameters),{docs:s(a({},(g=n.parameters)==null?void 0:g.docs),{source:a({originalSource:`{
+  render: () => {
+    const [value, setValue] = useState('option2');
+    const unauthorizedMessage = <>
+        <Typography.Text tooltip>
+          You don&apos;t have permissions for this action.{' '}
+          <Typography.Link href="/" tooltip>
+            Learn More
+          </Typography.Link>
+        </Typography.Text>
+      </>;
+    return <Radio.Group value={value} onChange={e => setValue(e.target.value as string)}>
+        <div style={{
+        marginBottom: Margin
+      }}>
+          <UnauthorizedTooltip isAuthorized={false} unauthorizedMessage={unauthorizedMessage} placement="right">
+            <Radio value="option1" disabled>
+              Option 1
+            </Radio>
+          </UnauthorizedTooltip>
+        </div>
+        <Radio value="option2">Option 2</Radio>
+      </Radio.Group>;
+  }
+}`},(f=(x=n.parameters)==null?void 0:x.docs)==null?void 0:f.source)})});const U=["DisabledButton","ModalExample"],P=Object.freeze(Object.defineProperty({__proto__:null,DisabledButton:i,ModalExample:n,__namedExportsOrder:U,default:S},Symbol.toStringTag,{value:"Module"}));export{i as D,n as M,P as U};
