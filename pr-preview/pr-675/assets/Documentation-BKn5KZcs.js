1247d1b9650cc7b3b4626e4cfc067e854ea784f5
mparticle-automation <developers@mparticle.com>
2025-12-12 04:40:42 +0000

    Deploy preview for PR 675 🛫

---
+++ pr-preview/pr-675/assets/Documentation-BKn5KZcs.js
@@ -0,0 +1,32 @@
+var u=Object.defineProperty,l=Object.defineProperties;var x=Object.getOwnPropertyDescriptors;var o=Object.getOwnPropertySymbols;var c=Object.prototype.hasOwnProperty,m=Object.prototype.propertyIsEnumerable;var p=(e,n,i)=>n in e?u(e,n,{enumerable:!0,configurable:!0,writable:!0,value:i}):e[n]=i,s=(e,n)=>{for(var i in n||(n={}))c.call(n,i)&&p(e,i,n[i]);if(o)for(var i of o(n))m.call(n,i)&&p(e,i,n[i]);return e},d=(e,n)=>l(e,x(n));import{j as t}from"./jsx-runtime-C9TrHvcC.js";import{useMDXComponents as a}from"./index-CkQzTg6q.js";import{ae as j,ag as r}from"./index-CcYRDsw4.js";import{I as f,T as I,N as w,P as g,D as T,S as b,a as v,b as y,R as C,O as D,H as R}from"./Input.stories-DC15eepu.js";import"./index-3OP4wdng.js";import"./iframe-BO5b3A8U.js";import"../sb-preview/runtime.js";import"./index-DA79H6I3.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DwBCp-Ss.js";import"./index-dQMQm6Vu.js";import"./index-DrFu-skq.js";import"./NavigationItemsService-DMdrorSJ.js";import"./ErrorStateIcon-BS17_6tf.js";import"./style-BAkx2Mzk.js";import"./WarningFilled-D6ijQqgK.js";import"./AntdIcon-DRQs8XP6.js";import"./presets-CQzyf-6F.js";import"./Context-R5ee3nrF.js";import"./SuccessStateIcon-HuPZrMr2.js";import"./WarningStateIcon-Duz4utQS.js";import"./LightTheme-BtoaoHMG.js";import"./row-DlEJUZa0.js";import"./useToken-BIYN7ogo.js";function h(e){const n=s(s({a:"a",h1:"h1",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul"},a()),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(j,{of:f}),`
+`,`
+`,t.jsx(n.h1,{id:"input",children:"Input"}),`
+`,t.jsx(n.h4,{id:"overview",children:"Overview"}),`
+`,t.jsxs(n.p,{children:["The ",t.jsx(n.strong,{children:"Input"})," component allows users to enter content using a mouse or keyboard. It serves as the most basic form field wrapper and is commonly used where user input is required."]}),`
+`,t.jsx(n.h4,{id:"when-to-use",children:t.jsx(n.strong,{children:"When To Use"})}),`
+`,t.jsxs(n.ul,{children:[`
+`,t.jsx(n.li,{children:"A user input in a form field is needed."}),`
+`,t.jsx(n.li,{children:"A search input is required."}),`
+`]}),`
+`,t.jsx(n.h4,{id:"text-input",children:"Text Input"}),`
+`,t.jsx(r,{of:I}),`
+`,t.jsx(n.h4,{id:"number-input",children:"Number Input"}),`
+`,t.jsx(r,{of:w}),`
+`,t.jsx(n.h4,{id:"percentage-input",children:"Percentage Input"}),`
+`,t.jsx(r,{of:g}),`
+`,t.jsx(n.h4,{id:"duration-input",children:"Duration Input"}),`
+`,t.jsx(r,{of:T}),`
+`,t.jsx(n.h4,{id:"search-input",children:"Search Input"}),`
+`,t.jsx(r,{of:b}),`
+`,t.jsx(n.h4,{id:"text-area-input",children:"Text Area Input"}),`
+`,t.jsx(r,{of:v}),`
+`,t.jsx(n.h4,{id:"password-input",children:"Password Input"}),`
+`,t.jsx(r,{of:y}),`
+`,t.jsx(n.h4,{id:"required-text-input",children:"Required Text Input"}),`
+`,t.jsx(r,{of:C}),`
+`,t.jsx(n.h4,{id:"optional-text-input",children:"Optional Text Input"}),`
+`,t.jsx(r,{of:D}),`
+`,t.jsx(n.h4,{id:"text-input-with-help-tooltip",children:"Text Input with Help Tooltip"}),`
+`,t.jsx(r,{of:R}),`
+`,t.jsx(n.h4,{id:"related-links",children:"Related Links"}),`
+`,t.jsxs(n.table,{children:[t.jsx(n.thead,{children:t.jsxs(n.tr,{children:[t.jsx(n.th,{children:"Type"}),t.jsx(n.th,{children:"Resource"})]})}),t.jsxs(n.tbody,{children:[t.jsxs(n.tr,{children:[t.jsx(n.td,{children:"Eames"}),t.jsx(n.td,{children:t.jsx(n.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=388-11439&p=f&t=zURbLpG60aM6aJiH-0",rel:"nofollow",children:"Input Component"})})]}),t.jsxs(n.tr,{children:[t.jsx(n.td,{children:"AntD"}),t.jsx(n.td,{children:t.jsx(n.a,{href:"https://ant.design/components/input",rel:"nofollow",children:"Input Component"})})]})]})]})]})}function tn(e={}){const{wrapper:n}=s(s({},a()),e.components);return n?t.jsx(n,d(s({},e),{children:t.jsx(h,s({},e))})):h(e)}export{tn as default};
