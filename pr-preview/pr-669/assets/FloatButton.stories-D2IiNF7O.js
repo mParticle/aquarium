c712f3c25c5c201cbe11f4a7e3c63d682d0087d9
mparticle-automation <developers@mparticle.com>
2025-12-12 19:24:47 +0000

    Deploy preview for PR 669 🛫

---
+++ pr-preview/pr-669/assets/FloatButton.stories-D2IiNF7O.js
@@ -0,0 +1,11 @@
+var q=Object.defineProperty,S=Object.defineProperties;var F=Object.getOwnPropertyDescriptors;var m=Object.getOwnPropertySymbols;var b=Object.prototype.hasOwnProperty,x=Object.prototype.propertyIsEnumerable;var c=(o,r,t)=>r in o?q(o,r,{enumerable:!0,configurable:!0,writable:!0,value:t}):o[r]=t,e=(o,r)=>{for(var t in r||(r={}))b.call(r,t)&&c(o,t,r[t]);if(m)for(var t of m(r))x.call(r,t)&&c(o,t,r[t]);return o},a=(o,r)=>S(o,F(r));import{af as C}from"./NavigationItemsService-Bq8SM3Eb.js";import"./jsx-runtime-C9TrHvcC.js";import"./index-3OP4wdng.js";import"./ErrorStateIcon-lPL2zJXs.js";import"./style-D_W9RJEq.js";import"./WarningFilled-D6ijQqgK.js";import"./extends-CF3RwP-h.js";import"./AntdIcon-DRQs8XP6.js";import"./presets-CQzyf-6F.js";import"./Context-R5ee3nrF.js";import"./SuccessStateIcon-DchWMFvD.js";import"./WarningStateIcon-BOk4cYVs.js";import"./LightTheme-WtG2Efeh.js";import"./row-DlEJUZa0.js";import"./useToken-BIYN7ogo.js";import"./index-DA79H6I3.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DwBCp-Ss.js";const K={title:"Components/Not Prod Ready/General/FloatButton",component:C,args:{description:"Float Button",tooltip:"Tooltip Text",type:"primary",shape:"circle",href:"",target:"",badge:void 0,onClick:o=>{alert("Float Button Clicked!")}},argTypes:{shape:{control:"select",options:["circle","square"]},type:{control:"select",options:["default","primary"]}}},s={},p={args:{badge:{count:5}}},i={args:{shape:"square"}};var n,d,l;s.parameters=a(e({},s.parameters),{docs:a(e({},(n=s.parameters)==null?void 0:n.docs),{source:e({originalSource:"{}"},(l=(d=s.parameters)==null?void 0:d.docs)==null?void 0:l.source)})});var u,g,y;p.parameters=a(e({},p.parameters),{docs:a(e({},(u=p.parameters)==null?void 0:u.docs),{source:e({originalSource:`{
+  args: {
+    badge: {
+      count: 5
+    }
+  }
+}`},(y=(g=p.parameters)==null?void 0:g.docs)==null?void 0:y.source)})});var h,B,f;i.parameters=a(e({},i.parameters),{docs:a(e({},(h=i.parameters)==null?void 0:h.docs),{source:e({originalSource:`{
+  args: {
+    shape: 'square'
+  }
+}`},(f=(B=i.parameters)==null?void 0:B.docs)==null?void 0:f.source)})});const L=["Primary","WithBadge","Square"];export{s as Primary,i as Square,p as WithBadge,L as __namedExportsOrder,K as default};
