16a0fb8b95ea26a52f25ebbb6209e6a3a9976702
mparticle-automation <developers@mparticle.com>
2025-12-16 04:09:43 +0000

    Deploy preview for PR 694 🛫

---
+++ pr-preview/pr-694/assets/Documentation-DldSv7MI.js
@@ -0,0 +1,23 @@
+var c=Object.defineProperty,m=Object.defineProperties;var p=Object.getOwnPropertyDescriptors;var o=Object.getOwnPropertySymbols;var x=Object.prototype.hasOwnProperty,j=Object.prototype.propertyIsEnumerable;var h=(n,t,i)=>t in n?c(n,t,{enumerable:!0,configurable:!0,writable:!0,value:i}):n[t]=i,s=(n,t)=>{for(var i in t||(t={}))x.call(t,i)&&h(n,i,t[i]);if(o)for(var i of o(t))j.call(t,i)&&h(n,i,t[i]);return n},l=(n,t)=>m(n,p(t));import{j as e}from"./jsx-runtime-C9TrHvcC.js";import{useMDXComponents as d}from"./index-CkQzTg6q.js";import{ae as f,ag as r}from"./index-KYkey0Y0.js";import{S as w,a as u,P as g,D as b,W as y}from"./Switch.stories-B2JUx8R0.js";import"./index-3OP4wdng.js";import"./iframe-DEEDz9ft.js";import"../sb-preview/runtime.js";import"./index-DA79H6I3.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DwBCp-Ss.js";import"./index-dQMQm6Vu.js";import"./index-DrFu-skq.js";import"./NavigationItemsService-A3jUzeVL.js";import"./ErrorStateIcon-DHDoS-CX.js";import"./style-C9UL_bK6.js";import"./WarningFilled-D6ijQqgK.js";import"./AntdIcon-DRQs8XP6.js";import"./presets-CQzyf-6F.js";import"./Context-R5ee3nrF.js";import"./SuccessStateIcon-_GQD4OK8.js";import"./WarningStateIcon-BSWz87XS.js";import"./LightTheme-BtoaoHMG.js";import"./row-DlEJUZa0.js";import"./useToken-BIYN7ogo.js";function a(n){const t=s(s({a:"a",h1:"h1",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul"},d()),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(f,{of:w}),`
+`,`
+`,e.jsx(t.h1,{id:"switch",children:"Switch"}),`
+`,e.jsx(t.h4,{id:"overview",children:"Overview"}),`
+`,e.jsx(t.p,{children:"Used to toggle between two states."}),`
+`,e.jsx(t.h4,{id:"when-to-use",children:e.jsx(t.strong,{children:"When To Use"})}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"To represent the switching between two states, such as an on-off state."}),`
+`,e.jsxs(t.li,{children:["Use ",e.jsx(t.strong,{children:"Switch"})," when the state change happens immediately upon toggling."]}),`
+`,e.jsxs(t.li,{children:["Use ",e.jsx(t.strong,{children:e.jsx(t.a,{href:"https://mparticle.github.io/aquarium/?path=/story/components-data-entry-checkbox--primary",rel:"nofollow",children:"Checkbox"})})," for state marking, which typically requires a submit operation to apply changes."]}),`
+`]}),`
+`,e.jsx(t.h3,{id:"states",children:"States"}),`
+`,e.jsx(t.h4,{id:"small",children:"Small"}),`
+`,e.jsx(r,{of:u}),`
+`,e.jsx(t.h4,{id:"default",children:"Default"}),`
+`,e.jsx(r,{of:g}),`
+`,e.jsx(t.h3,{id:"variants",children:"Variants"}),`
+`,e.jsx(t.h4,{id:"disabled",children:"Disabled"}),`
+`,e.jsx(r,{of:b}),`
+`,e.jsx(t.h4,{id:"with-label",children:"With Label"}),`
+`,e.jsx(r,{of:y}),`
+`,e.jsx(t.h4,{id:"related-links",children:"Related Links"}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Type"}),e.jsx(t.th,{children:"Resource"})]})}),e.jsxs(t.tbody,{children:[e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"Eames"}),e.jsx(t.td,{children:e.jsx(t.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=396-12305&p=f&t=hGHFK3BQiEoVHZ6E-0",rel:"nofollow",children:"Switch Component"})})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"AntD"}),e.jsx(t.td,{children:e.jsx(t.a,{href:"https://ant.design/components/switch",rel:"nofollow",children:"Switch Component"})})]})]})]})]})}function O(n={}){const{wrapper:t}=s(s({},d()),n.components);return t?e.jsx(t,l(s({},n),{children:e.jsx(a,s({},n))})):a(n)}export{O as default};
