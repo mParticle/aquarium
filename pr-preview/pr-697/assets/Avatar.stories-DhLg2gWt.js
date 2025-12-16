94f1081a071ef1e9e76eeba974f2e25db14b94f0
mparticle-automation <developers@mparticle.com>
2025-12-16 20:02:43 +0000

    Deploy preview for PR 697 🛫

--- pr-preview/pr-697/assets/Avatar.stories-DhLg2gWt.js
+++
@@ -1,9 +0,0 @@
-var h=Object.defineProperty,f=Object.defineProperties;var I=Object.getOwnPropertyDescriptors;var i=Object.getOwnPropertySymbols;var _=Object.prototype.hasOwnProperty,j=Object.prototype.propertyIsEnumerable;var c=(o,r,e)=>r in o?h(o,r,{enumerable:!0,configurable:!0,writable:!0,value:e}):o[r]=e,t=(o,r)=>{for(var e in r||(r={}))_.call(r,e)&&c(o,e,r[e]);if(i)for(var e of i(r))j.call(r,e)&&c(o,e,r[e]);return o},s=(o,r)=>f(o,I(r));import{j as x}from"./jsx-runtime-C9TrHvcC.js";import{A,I as P}from"./NavigationItemsService-DrTay7AA.js";import"./ErrorStateIcon-DHDoS-CX.js";import"./SuccessStateIcon-_GQD4OK8.js";import"./WarningStateIcon-BSWz87XS.js";import"./_baseClone-DwBCp-Ss.js";const S={title:"Components/Data Display/Avatar",component:A},a={args:{children:"mP"}},n={args:{icon:x.jsx(P,{name:"userProfiles",size:"lg",color:"white"})}};var m,p,l;a.parameters=s(t({},a.parameters),{docs:s(t({},(m=a.parameters)==null?void 0:m.docs),{source:t({originalSource:`{
-  args: {
-    children: 'mP'
-  }
-}`},(l=(p=a.parameters)==null?void 0:p.docs)==null?void 0:l.source)})});var d,u,g;n.parameters=s(t({},n.parameters),{docs:s(t({},(d=n.parameters)==null?void 0:d.docs),{source:t({originalSource:`{
-  args: {
-    icon: <Icon name="userProfiles" size="lg" color="white" />
-  }
-}`},(g=(u=n.parameters)==null?void 0:u.docs)==null?void 0:g.source)})});const v=["WithInitials","WithIcon"],E=Object.freeze(Object.defineProperty({__proto__:null,WithIcon:n,WithInitials:a,__namedExportsOrder:v,default:S},Symbol.toStringTag,{value:"Module"}));export{E as A,S as m};
