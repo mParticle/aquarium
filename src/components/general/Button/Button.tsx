16dc0886e395c953f9862ea677dd9d2e3b97cb31
astavitskaya <astavitskaya@mparticle.com>
2025-12-19 16:59:38 -0800

    fix: fix dropdown position with button

--- src/components/general/Button/Button.tsx
+++ src/components/general/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { Button as AntButton } from 'antd'
 import { type ButtonProps as AntButtonProps } from 'antd'
 import { Icon } from 'src/components'
@@ -7,7 +7,9 @@ import { ConfigProvider } from 'src/components/other/ConfigProvider/ConfigProvid
 import { type ReactNode } from 'react'
 import './button.css'
 
-export interface IButtonProps extends Omit<AntButtonProps, 'variant'> {
+type ButtonColor = AntButtonProps['color'] | 'inherit'
+
+export interface IButtonProps extends Omit<AntButtonProps, 'variant' | 'color'> {
   /**
    * @deprecated This variant is a temporary fix for new icons.
    * Use this variant only with new icons to align the icon and text centered.
@@ -15,30 +17,45 @@ export interface IButtonProps extends Omit<AntButtonProps, 'variant'> {
    */
   variant?: 'with-new-icon'
   icon?: ReactNode
+  color?: ButtonColor
 }
-export const Button = (props: IButtonProps) => {
-  const classMap = {
-    'with-new-icon': 'u-display-flex u-align-items-center u-justify-center',
-  }
 
-  const buttonIcon =
-    React.isValidElement<IIconProps>(props.icon) && props.icon.type === Icon ? (
-      <Icon {...props.icon.props} color={props.icon.props.color ?? 'inherit'} />
-    ) : (
-      props.icon
+type ButtonComponent = React.ForwardRefExoticComponent<IButtonProps & React.RefAttributes<HTMLButtonElement>> & {
+  Group: typeof AntButton.Group
+}
+
+export const Button: ButtonComponent = Object.assign(
+  forwardRef<HTMLButtonElement, IButtonProps>(function Button(props, ref) {
+    const classMap = {
+      'with-new-icon': 'u-display-flex u-align-items-center u-justify-center',
+    }
+
+    const buttonIcon =
+      React.isValidElement<IIconProps>(props.icon) && props.icon.type === Icon ? (
+        <Icon {...props.icon.props} color={props.icon.props.color ?? 'inherit'} />
+      ) : (
+        props.icon
+      )
+
+    const extraClass = props.variant === 'with-new-icon' ? classMap['with-new-icon'] : ''
+
+    const { variant, color, ...restProps } = props
+
+    // Pass color to AntButton only if it's a valid Ant color (not 'inherit')
+    const antColor = color === 'inherit' ? undefined : color
+
+    return (
+      <ConfigProvider>
+        <AntButton
+          ref={ref}
+          {...restProps}
+          color={antColor}
+          icon={buttonIcon}
+          className={`${props.className ?? ''} ${extraClass}`}>
+          {props.children}
+        </AntButton>
+      </ConfigProvider>
     )
-
-  const extraClass = props.variant === 'with-new-icon' ? classMap['with-new-icon'] : ''
-
-  const { variant, ...restProps } = props
-
-  return (
-    <ConfigProvider>
-      <AntButton {...restProps} icon={buttonIcon} className={`${props.className ?? ''} ${extraClass}`}>
-        {props.children}
-      </AntButton>
-    </ConfigProvider>
-  )
-}
-
-Button.Group = AntButton.Group
+  }),
+  { Group: AntButton.Group },
+)
