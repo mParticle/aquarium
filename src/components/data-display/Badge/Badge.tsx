02a69e523e95e87806b77ffa6689787372502d47
astavitskaya <astavitskaya@mparticle.com>
2026-01-13 18:03:58 -0800

    feat: add filled badge

--- src/components/data-display/Badge/Badge.tsx
+++ src/components/data-display/Badge/Badge.tsx
@@ -1,10 +1,38 @@
 import { Badge as AntBadge } from 'antd'
 import { type BadgeProps as AntBadgeProps } from 'antd'
 import { ConfigProvider } from 'src/components'
+import './Badge.css'
 
-export interface IBadgeProps extends AntBadgeProps {}
+export type BadgeVariant = 'default' | 'filled'
+export type BadgeSize = 'sm' | 'md' | 'lg'
+export type BadgeShape = 'pill' | 'square'
 
-export const Badge = (props: IBadgeProps) => {
+export interface IBadgeProps extends Omit<AntBadgeProps, 'size'> {
+  variant?: BadgeVariant
+  size?: BadgeSize
+  shape?: BadgeShape
+}
+
+export const Badge = ({ variant = 'default', size = 'md', shape = 'pill', ...props }: IBadgeProps) => {
+  if (variant === 'filled' && props.status && props.text) {
+    const classNames = [
+      'mp-badge-filled',
+      `mp-badge-filled--${props.status}`,
+      `mp-badge-filled--${size}`,
+      `mp-badge-filled--${shape}`,
+    ]
+      .filter(Boolean)
+      .join(' ')
+
+    return (
+      <ConfigProvider>
+        <span className={classNames}>
+          <AntBadge {...props} />
+        </span>
+      </ConfigProvider>
+    )
+  }
+
   return (
     <ConfigProvider>
       <AntBadge {...props} />
