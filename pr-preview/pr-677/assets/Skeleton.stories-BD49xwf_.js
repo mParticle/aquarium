80a89c39ce27b8b7fae69fa5354b95f00b79ad0c
mparticle-automation <developers@mparticle.com>
2026-01-15 22:43:35 +0000

    Deploy preview for PR 677 🛫

---
+++ pr-preview/pr-677/assets/Skeleton.stories-BD49xwf_.js
@@ -0,0 +1,5 @@
+var c=Object.defineProperty,d=Object.defineProperties;var g=Object.getOwnPropertyDescriptors;var n=Object.getOwnPropertySymbols;var u=Object.prototype.hasOwnProperty,S=Object.prototype.propertyIsEnumerable;var p=(r,e,t)=>e in r?c(r,e,{enumerable:!0,configurable:!0,writable:!0,value:t}):r[e]=t,a=(r,e)=>{for(var t in e||(e={}))u.call(e,t)&&p(r,t,e[t]);if(n)for(var t of n(e))S.call(e,t)&&p(r,t,e[t]);return r},s=(r,e)=>d(r,g(e));import{j as f}from"./jsx-runtime-C9TrHvcC.js";import{S as x}from"./UnauthorizedTooltip-8JQzVMDG.js";const h={title:"Components/Feedback/Skeleton",component:r=>f.jsx(x,s(a({},r),{style:{height:"200px",width:"200px"}})),args:{loading:!0,paragraph:!1,title:!1}},o={args:{paragraph:!0}};var m,l,i;o.parameters=s(a({},o.parameters),{docs:s(a({},(m=o.parameters)==null?void 0:m.docs),{source:a({originalSource:`{
+  args: {
+    paragraph: true
+  }
+}`},(i=(l=o.parameters)==null?void 0:l.docs)==null?void 0:i.source)})});const _=["Primary"],k=Object.freeze(Object.defineProperty({__proto__:null,Primary:o,__namedExportsOrder:_,default:h},Symbol.toStringTag,{value:"Module"}));export{k as S,h as m};
