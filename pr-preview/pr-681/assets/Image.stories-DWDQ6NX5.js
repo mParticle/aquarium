68600adf67af137ccd94ea4e6b168718985c1cf3
mparticle-automation <developers@mparticle.com>
2026-01-15 19:16:49 +0000

    Deploy preview for PR 681 🛫

---
+++ pr-preview/pr-681/assets/Image.stories-DWDQ6NX5.js
@@ -0,0 +1,8 @@
+var g=Object.defineProperty,l=Object.defineProperties;var d=Object.getOwnPropertyDescriptors;var p=Object.getOwnPropertySymbols;var u=Object.prototype.hasOwnProperty,f=Object.prototype.propertyIsEnumerable;var c=(a,e,r)=>e in a?g(a,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):a[e]=r,t=(a,e)=>{for(var r in e||(e={}))u.call(e,r)&&c(a,r,e[r]);if(p)for(var r of p(e))f.call(e,r)&&c(a,r,e[r]);return a},o=(a,e)=>l(a,d(e));import{k as h}from"./NavigationItemsService-Dtp5CN9e.js";const v={title:"Components/Data Display/Image",component:h,args:{}},s={args:{src:"https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",preview:!1,width:100,height:100}};var m,i,n;s.parameters=o(t({},s.parameters),{docs:o(t({},(m=s.parameters)==null?void 0:m.docs),{source:t({originalSource:`{
+  args: {
+    src: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+    preview: false,
+    width: 100,
+    height: 100
+  }
+}`},(n=(i=s.parameters)==null?void 0:i.docs)==null?void 0:n.source)})});const y=["Primary"],_=Object.freeze(Object.defineProperty({__proto__:null,Primary:s,__namedExportsOrder:y,default:v},Symbol.toStringTag,{value:"Module"}));export{_ as I,v as m};
