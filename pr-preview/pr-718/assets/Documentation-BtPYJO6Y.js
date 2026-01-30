9b753f27beab7a29d2857b316fc697af20aef89d
mparticle-automation <developers@mparticle.com>
2026-01-30 04:11:51 +0000

    Deploy preview for PR 718 🛫

---
+++ pr-preview/pr-718/assets/Documentation-BtPYJO6Y.js
@@ -0,0 +1,13 @@
+var p=Object.defineProperty,l=Object.defineProperties;var d=Object.getOwnPropertyDescriptors;var n=Object.getOwnPropertySymbols;var h=Object.prototype.hasOwnProperty,x=Object.prototype.propertyIsEnumerable;var s=(i,t,o)=>t in i?p(i,t,{enumerable:!0,configurable:!0,writable:!0,value:o}):i[t]=o,e=(i,t)=>{for(var o in t||(t={}))h.call(t,o)&&s(i,o,t[o]);if(n)for(var o of n(t))x.call(t,o)&&s(i,o,t[o]);return i},m=(i,t)=>l(i,d(t));import{j as r}from"./jsx-runtime-C9TrHvcC.js";import{useMDXComponents as a}from"./index-CkQzTg6q.js";import{ae as u,ag as j}from"./index-BxjBCB46.js";import f,{ModelMetrics as g}from"./StatisticsCard.stories-DA3c3jju.js";import"./index-3OP4wdng.js";import"./iframe-B2j0WFzV.js";import"../sb-preview/runtime.js";import"./index-DA79H6I3.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DwBCp-Ss.js";import"./index-dQMQm6Vu.js";import"./index-DrFu-skq.js";import"./UnauthorizedTooltip-D-gjWMyS.js";import"./ErrorStateIcon-cm84PoQ8.js";import"./style-BV-MbpCE.js";import"./WarningFilled-D6ijQqgK.js";import"./AntdIcon-DRQs8XP6.js";import"./presets-CQzyf-6F.js";import"./Context-R5ee3nrF.js";import"./SuccessStateIcon-ilF-jaRA.js";import"./WarningStateIcon-Bxl054SV.js";import"./LightTheme-DEEWgUVF.js";import"./row-DlEJUZa0.js";import"./useToken-BIYN7ogo.js";function c(i){const t=e(e({h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul"},a()),i.components);return r.jsxs(r.Fragment,{children:[r.jsx(u,{of:f}),`
+`,r.jsx(t.h1,{id:"statistics-card",children:"Statistics Card"}),`
+`,r.jsx(t.p,{children:"A card component for displaying metric values with optional tooltips, commonly used for model metrics, performance indicators, or key statistics."}),`
+`,r.jsx(t.h2,{id:"when-to-use",children:"When to Use"}),`
+`,r.jsxs(t.ul,{children:[`
+`,r.jsx(t.li,{children:"Displaying model performance metrics (accuracy, precision, recall, AUC)"}),`
+`,r.jsx(t.li,{children:"Showing key performance indicators (KPIs)"}),`
+`,r.jsx(t.li,{children:"Presenting statistical data with contextual help"}),`
+`,r.jsx(t.li,{children:"Creating metric dashboards or summary cards"}),`
+`]}),`
+`,r.jsx(t.h2,{id:"model-metrics-example",children:"Model Metrics Example"}),`
+`,r.jsx(t.p,{children:"A common pattern showing multiple metrics in a grid layout:"}),`
+`,r.jsx(j,{of:g})]})}function N(i={}){const{wrapper:t}=e(e({},a()),i.components);return t?r.jsx(t,m(e({},i),{children:r.jsx(c,e({},i))})):c(i)}export{N as default};
