c95347ea845836c9294be382b61b7959c2f016bb
mparticle-automation <developers@mparticle.com>
2026-01-30 20:51:12 +0000

    Deploy preview for PR 726 🛫

---
+++ pr-preview/pr-726/assets/Notification.stories-BnwgGdMl.js
@@ -0,0 +1,21 @@
+var O=Object.defineProperty,R=Object.defineProperties;var M=Object.getOwnPropertyDescriptors;var d=Object.getOwnPropertySymbols;var T=Object.prototype.hasOwnProperty,W=Object.prototype.propertyIsEnumerable;var u=(a,o,s)=>o in a?O(a,o,{enumerable:!0,configurable:!0,writable:!0,value:s}):a[o]=s,r=(a,o)=>{for(var s in o||(o={}))T.call(o,s)&&u(a,s,o[s]);if(d)for(var s of d(o))W.call(o,s)&&u(a,s,o[s]);return a},e=(a,o)=>R(a,M(o));import{j as z}from"./jsx-runtime-C9TrHvcC.js";import{$ as B,c as D}from"./UnauthorizedTooltip-BLMZmsMD.js";const F={title:"Components/Not Prod Ready/Feedback/Notification",component:B,args:{type:"info",children:z.jsx(D,{type:"primary",children:"Show Notification"}),message:"Message",description:"Description goes here",duration:0,btn:void 0,className:void 0,closeIcon:!0,icon:void 0,key:void 0,placement:"topRight",style:void 0,role:"alert",onClick:function(){alert("Notification Clicked")},onClose:function(){alert("Notification Closed")}},argTypes:{role:{control:"select",options:["alert","status"]},type:{control:"select",options:["info","warning","success","error"]}}},t={},n={args:{duration:1}},c={args:{type:"info"}},i={args:{type:"warning"}},p={args:{type:"success"}},m={args:{type:"error"}};var l,g,y;t.parameters=e(r({},t.parameters),{docs:e(r({},(l=t.parameters)==null?void 0:l.docs),{source:r({originalSource:"{}"},(y=(g=t.parameters)==null?void 0:g.docs)==null?void 0:y.source)})});var f,S,N;n.parameters=e(r({},n.parameters),{docs:e(r({},(f=n.parameters)==null?void 0:f.docs),{source:r({originalSource:`{
+  args: {
+    duration: 1
+  }
+}`},(N=(S=n.parameters)==null?void 0:S.docs)==null?void 0:N.source)})});var h,v,_;c.parameters=e(r({},c.parameters),{docs:e(r({},(h=c.parameters)==null?void 0:h.docs),{source:r({originalSource:`{
+  args: {
+    type: 'info'
+  }
+}`},(_=(v=c.parameters)==null?void 0:v.docs)==null?void 0:_.source)})});var b,j,x;i.parameters=e(r({},i.parameters),{docs:e(r({},(b=i.parameters)==null?void 0:b.docs),{source:r({originalSource:`{
+  args: {
+    type: 'warning'
+  }
+}`},(x=(j=i.parameters)==null?void 0:j.docs)==null?void 0:x.source)})});var C,k,w;p.parameters=e(r({},p.parameters),{docs:e(r({},(C=p.parameters)==null?void 0:C.docs),{source:r({originalSource:`{
+  args: {
+    type: 'success'
+  }
+}`},(w=(k=p.parameters)==null?void 0:k.docs)==null?void 0:w.source)})});var E,P,I;m.parameters=e(r({},m.parameters),{docs:e(r({},(E=m.parameters)==null?void 0:E.docs),{source:r({originalSource:`{
+  args: {
+    type: 'error'
+  }
+}`},(I=(P=m.parameters)==null?void 0:P.docs)==null?void 0:I.source)})});const $=["Primary","Short","Info","Warning","Success","Error"],H=Object.freeze(Object.defineProperty({__proto__:null,Error:m,Info:c,Primary:t,Short:n,Success:p,Warning:i,__namedExportsOrder:$,default:F},Symbol.toStringTag,{value:"Module"}));export{H as N,F as m};
---
+++ pr-preview/pr-726/assets/Popover.stories-DVdoKrtN.js
@@ -0,0 +1,25 @@
+var j=Object.defineProperty,v=Object.defineProperties;var y=Object.getOwnPropertyDescriptors;var a=Object.getOwnPropertySymbols;var T=Object.prototype.hasOwnProperty,f=Object.prototype.propertyIsEnumerable;var m=(e,r,i)=>r in e?j(e,r,{enumerable:!0,configurable:!0,writable:!0,value:i}):e[r]=i,n=(e,r)=>{for(var i in r||(r={}))T.call(r,i)&&m(e,i,r[i]);if(a)for(var i of a(r))f.call(r,i)&&m(e,i,r[i]);return e},o=(e,r)=>v(e,y(r));import{j as t}from"./jsx-runtime-C9TrHvcC.js";import{P as l,c as L,l as P,F as E,b as p}from"./UnauthorizedTooltip-BLMZmsMD.js";import"./index-3OP4wdng.js";import"./ErrorStateIcon-CcQoYP7x.js";import"./style-BbwCNso5.js";import"./WarningFilled-D6ijQqgK.js";import"./extends-CF3RwP-h.js";import"./AntdIcon-DRQs8XP6.js";import"./presets-CQzyf-6F.js";import"./Context-R5ee3nrF.js";import"./SuccessStateIcon-Dn1_3x90.js";import"./WarningStateIcon-OlzhE0bB.js";import"./LightTheme-BtoaoHMG.js";import"./row-DlEJUZa0.js";import"./useToken-BIYN7ogo.js";import"./index-DA79H6I3.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DwBCp-Ss.js";const Q={title:"Components/Data Display/Popover",component:l,args:{}},F=e=>{const r=t.jsx("div",{children:t.jsx("p",{children:"Content"})});return t.jsx(t.Fragment,{children:t.jsx(l,{content:r,title:"Title",trigger:e.trigger,children:t.jsx(L,{children:e.children})})})},s={args:{trigger:"click",children:"Button with popover"},argTypes:{trigger:{control:"select",options:["click","hover","focus"]}},render:F},W=e=>{var i;const r=t.jsxs(E,{gap:"small",vertical:!0,children:[t.jsx(p.Link,{href:"#link1",children:"Link Example 1"}),t.jsx(p.Link,{href:"#link2",children:"Link Example 2"}),t.jsx(p.Link,{href:"#link3",children:"Link Example 3"})]});return t.jsx(t.Fragment,{children:t.jsx(l,{content:r,title:"Details",trigger:e.trigger,children:t.jsx(P,{style:{margin:"0",cursor:"pointer"},children:(i=e.children)!=null?i:"3"})})})},c={args:{trigger:"click",children:"3"},argTypes:{trigger:{control:"select",options:["click","hover","focus"]}},render:W};var g,d,h;s.parameters=o(n({},s.parameters),{docs:o(n({},(g=s.parameters)==null?void 0:g.docs),{source:n({originalSource:`{
+  args: {
+    trigger: 'click',
+    children: 'Button with popover'
+  },
+  argTypes: {
+    trigger: {
+      control: 'select',
+      options: ['click', 'hover', 'focus']
+    }
+  },
+  render: PrimaryTemplate
+}`},(h=(d=s.parameters)==null?void 0:d.docs)==null?void 0:h.source)})});var x,k,u;c.parameters=o(n({},c.parameters),{docs:o(n({},(x=c.parameters)==null?void 0:x.docs),{source:n({originalSource:`{
+  args: {
+    trigger: 'click',
+    children: '3'
+  },
+  argTypes: {
+    trigger: {
+      control: 'select',
+      options: ['click', 'hover', 'focus']
+    }
+  },
+  render: PopoverWithLinksTemplate
+}`},(u=(k=c.parameters)==null?void 0:k.docs)==null?void 0:u.source)})});const U=["Primary","ExampleWithLinks"];export{c as ExampleWithLinks,s as Primary,U as __namedExportsOrder,Q as default};
