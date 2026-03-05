c419f1a09137d21219009e09793fc7cad788a257
mparticle-automation <developers@mparticle.com>
2026-03-05 15:49:15 +0000

    Deploy preview for PR 739 🛫

---
+++ pr-preview/pr-739/assets/Documentation-4kEjjCLQ.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-8zkxMVrx.js";import{useMDXComponents as r}from"./index-tviBiwA-.js";import{M as s,C as m}from"./blocks-CvBQZzr_.js";import{A as a,m as c}from"./Anchor.stories-DpmGauxS.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-BbtK-YgI.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
+`,`
+`,n.jsx(o.h1,{id:"anchor",children:"Anchor"}),`
+`,n.jsx(o.p,{children:"This is the documentation for the Anchor component"}),`
+`,`
+`,n.jsx(m,{meta:c})]})}function u(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
