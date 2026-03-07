07a0a331e61d8f8cffc08fd1f9cfdf25d9cd527c
mparticle-automation <developers@mparticle.com>
2026-03-07 21:11:01 +0000

    Deploy preview for PR 739 🛫

---
+++ pr-preview/pr-739/assets/Documentation-D-HG8h6Z.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-iQbfxH-7.js";import{useMDXComponents as s}from"./index-BCYkvQ4V.js";import{M as r,C as m}from"./blocks-Bjoogfnr.js";import{S as i,m as a}from"./Steps.stories-IjHN0Hwv.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-Bt-q1UT5.js";import"./index-BNfmgNB7.js";function o(n){const e={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:i}),`
+`,`
+`,t.jsx(e.h1,{id:"steps",children:"Steps"}),`
+`,t.jsx(e.p,{children:"This is the documentation for the Steps component"}),`
+`,`
+`,t.jsx(m,{meta:a})]})}function d(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{d as default};
