2a820b6c10d3bb8311d80a1f22ed6dbaaaa5eb1f
mparticle-automation <developers@mparticle.com>
2026-03-09 23:13:28 +0000

    Deploy preview for PR 743 🛫

---
+++ pr-preview/pr-743/assets/Documentation-BaGD1bHG.js
@@ -0,0 +1,24 @@
+var p=Object.defineProperty,m=Object.defineProperties;var x=Object.getOwnPropertyDescriptors;var s=Object.getOwnPropertySymbols;var j=Object.prototype.hasOwnProperty,f=Object.prototype.propertyIsEnumerable;var i=(n,t,a)=>t in n?p(n,t,{enumerable:!0,configurable:!0,writable:!0,value:a}):n[t]=a,r=(n,t)=>{for(var a in t||(t={}))j.call(t,a)&&i(n,a,t[a]);if(s)for(var a of s(t))f.call(t,a)&&i(n,a,t[a]);return n},d=(n,t)=>m(n,x(t));import{j as e}from"./jsx-runtime-C9TrHvcC.js";import{useMDXComponents as c}from"./index-CkQzTg6q.js";import{ae as u,ag as o,ah as v}from"./index-B9UGDbAe.js";import{A as g,S as y,a as w,T as b,b as A,c as S,W as C,P as h}from"./Avatar.stories-DKVVgScr.js";import"./index-3OP4wdng.js";import"./iframe-e_26NAP1.js";import"../sb-preview/runtime.js";import"./index-DA79H6I3.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DwBCp-Ss.js";import"./index-dQMQm6Vu.js";import"./index-DrFu-skq.js";import"./UnauthorizedTooltip-BdWwbQHs.js";import"./ErrorStateIcon-qKzc1GK1.js";import"./style-DU2SNM1h.js";import"./WarningFilled-D6ijQqgK.js";import"./AntdIcon-DRQs8XP6.js";import"./presets-CQzyf-6F.js";import"./Context-R5ee3nrF.js";import"./SuccessStateIcon-Yh_2FoSB.js";import"./WarningStateIcon-C0qQ2T-3.js";import"./LightTheme-5-ZTbvef.js";import"./row-DlEJUZa0.js";import"./useToken-BIYN7ogo.js";function l(n){const t=r(r({a:"a",blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",h4:"h4",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr"},c()),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(u,{of:g}),`
+`,e.jsx(t.h1,{id:"avatar",children:"Avatar"}),`
+`,e.jsx(t.h4,{id:"shape",children:"Shape"}),`
+`,e.jsxs(t.p,{children:["The default avatar is circular. Switch the ",e.jsx(t.code,{children:"shape"})," prop to ",e.jsx(t.code,{children:"square"})," when you need hard edges or when aligning with adjacent rectangular content."]}),`
+`,e.jsx(o,{of:y}),`
+`,e.jsx(t.h4,{id:"sizes",children:"Sizes"}),`
+`,e.jsx(o,{of:w}),`
+`,e.jsx(t.h4,{id:"photo-avatar",children:"Photo avatar"}),`
+`,e.jsx(o,{of:b}),`
+`,e.jsx(t.h4,{id:"icon-avatar",children:"Icon avatar"}),`
+`,e.jsx(o,{of:A}),`
+`,e.jsx(t.h4,{id:"initial-avatar",children:"Initial avatar"}),`
+`,e.jsx(o,{of:S}),`
+`,e.jsx(t.h4,{id:"badge-avatars",children:"Badge avatars"}),`
+`,e.jsx(o,{of:C}),`
+`,e.jsx(t.h2,{id:"playground",children:"Playground"}),`
+`,e.jsx(t.p,{children:"Try it — change any prop below and see the result live."}),`
+`,e.jsx(o,{of:h,sourceState:"shown"}),`
+`,e.jsx(v,{of:h}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["Full API reference: ",e.jsx(t.a,{href:"https://ant.design/components/avatar",rel:"nofollow",children:"Avatar — Ant Design"})]}),`
+`]}),`
+`,e.jsx(t.h4,{id:"related-links",children:"Related Links"}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Type"}),e.jsx(t.th,{children:"Resource"})]})}),e.jsxs(t.tbody,{children:[e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"Eames"}),e.jsx(t.td,{children:e.jsx(t.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=397-12044&node-type=canvas&t=B6HJWqqDsUOypZQj-0",rel:"nofollow",children:"Avatar Component"})})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:"AntD"}),e.jsx(t.td,{children:e.jsx(t.a,{href:"https://ant.design/components/avatar",rel:"nofollow",children:"Avatar Component"})})]})]})]})]})}function $(n={}){const{wrapper:t}=r(r({},c()),n.components);return t?e.jsx(t,d(r({},n),{children:e.jsx(l,r({},n))})):l(n)}export{$ as default};
