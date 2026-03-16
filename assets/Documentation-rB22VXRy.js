d145430eedbfa837ff33ff425e22b1113dfbb2d7
nastyastavitskaya <stavitskaya.anastasiia@gmail.com>
2026-03-16 21:48:13 +0000

    Deploying to gh-pages from @ mParticle/aquarium@4216436694016ad96110a0d493d201d8938aaed7 🚀

---
+++ assets/Documentation-rB22VXRy.js
@@ -0,0 +1,27 @@
+var p=Object.defineProperty,x=Object.defineProperties;var j=Object.getOwnPropertyDescriptors;var s=Object.getOwnPropertySymbols;var m=Object.prototype.hasOwnProperty,u=Object.prototype.propertyIsEnumerable;var i=(n,e,a)=>e in n?p(n,e,{enumerable:!0,configurable:!0,writable:!0,value:a}):n[e]=a,o=(n,e)=>{for(var a in e||(e={}))m.call(e,a)&&i(n,a,e[a]);if(s)for(var a of s(e))u.call(e,a)&&i(n,a,e[a]);return n},d=(n,e)=>x(n,j(e));import{j as t}from"./jsx-runtime-C9TrHvcC.js";import{useMDXComponents as c}from"./index-CkQzTg6q.js";import{ae as f,ag as r,ah as g}from"./index-Bap8u2zM.js";import{A as v,S as b,a as y,T as w,b as q,c as S,W as A,P as h}from"./Avatar.stories-DFvXOelK.js";import"./index-3OP4wdng.js";import"./iframe-CoemJ52t.js";import"../sb-preview/runtime.js";import"./index-DA79H6I3.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./isNativeReflectConstruct-CWIsGhkf.js";import"./_baseClone-DwBCp-Ss.js";import"./index-dQMQm6Vu.js";import"./index-DrFu-skq.js";import"./UnauthorizedTooltip-DgIwxlor.js";import"./ErrorStateIcon-BHmLeD-u.js";import"./style-DU2SNM1h.js";import"./WarningFilled-D6ijQqgK.js";import"./AntdIcon-DRQs8XP6.js";import"./presets-CQzyf-6F.js";import"./Context-R5ee3nrF.js";import"./SuccessStateIcon-BxuateXz.js";import"./WarningStateIcon-3dZZcMfk.js";import"./LightTheme-5-ZTbvef.js";import"./row-DlEJUZa0.js";import"./useToken-BIYN7ogo.js";function l(n){const e=o(o({a:"a",blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",h4:"h4",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr"},c()),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(f,{of:v}),`
+`,t.jsx(e.h1,{id:"avatar",children:"Avatar"}),`
+`,t.jsx(e.h4,{id:"shape",children:"Shape"}),`
+`,t.jsxs(e.p,{children:["The default avatar is circular. Switch the ",t.jsx(e.code,{children:"shape"})," prop to ",t.jsx(e.code,{children:"square"})," when you need hard edges or when aligning with adjacent rectangular content."]}),`
+`,t.jsx(r,{of:b}),`
+`,t.jsx(e.h4,{id:"sizes",children:"Sizes"}),`
+`,t.jsx(r,{of:y}),`
+`,t.jsx(e.h4,{id:"photo-avatar",children:"Photo avatar"}),`
+`,t.jsx(r,{of:w}),`
+`,t.jsx(e.h4,{id:"icon-avatar",children:"Icon avatar"}),`
+`,t.jsx(r,{of:q}),`
+`,t.jsx(e.h4,{id:"initial-avatar",children:"Initial avatar"}),`
+`,t.jsx(r,{of:S}),`
+`,t.jsx(e.h4,{id:"badge-avatars",children:"Badge avatars"}),`
+`,t.jsx(r,{of:A}),`
+`,t.jsx(e.h4,{id:"square-avatar-badges",children:"Square avatar badges"}),`
+`,t.jsxs(e.p,{children:["Wrap a square ",t.jsx(e.code,{children:"Avatar"})," with the ",t.jsx(e.code,{children:"Badge"})," component to highlight unread items or online status. The ",t.jsx(e.code,{children:"WithSquareBadge"})," story shows both an icon-based avatar and a photo avatar using badges."]}),`
+`,t.jsx(r,{of:void 0}),`
+`,t.jsx(e.h2,{id:"playground",children:"Playground"}),`
+`,t.jsx(e.p,{children:"Try it — change any prop below and see the result live."}),`
+`,t.jsx(r,{of:h,sourceState:"shown"}),`
+`,t.jsx(g,{of:h}),`
+`,t.jsxs(e.blockquote,{children:[`
+`,t.jsxs(e.p,{children:["Full API reference: ",t.jsx(e.a,{href:"https://ant.design/components/avatar",rel:"nofollow",children:"Avatar — Ant Design"})]}),`
+`]}),`
+`,t.jsx(e.h4,{id:"related-links",children:"Related Links"}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Resource"})]})}),t.jsxs(e.tbody,{children:[t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"Eames"}),t.jsx(e.td,{children:t.jsx(e.a,{href:"https://www.figma.com/design/veXnmignQnJz8StIq10VJ5/Eames-2.0---Foundations-%26-Components?node-id=397-12044&node-type=canvas&t=B6HJWqqDsUOypZQj-0",rel:"nofollow",children:"Avatar Component"})})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:"AntD"}),t.jsx(e.td,{children:t.jsx(e.a,{href:"https://ant.design/components/avatar",rel:"nofollow",children:"Avatar Component"})})]})]})]})]})}function $(n={}){const{wrapper:e}=o(o({},c()),n.components);return e?t.jsx(e,d(o({},n),{children:t.jsx(l,o({},n))})):l(n)}export{$ as default};
