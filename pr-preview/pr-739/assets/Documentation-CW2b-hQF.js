220b09f58cff7d17b6095d7e539a7d842cde4316
mparticle-automation <developers@mparticle.com>
2026-03-18 16:45:03 +0000

    Remove preview for PR 739 🛬

--- pr-preview/pr-739/assets/Documentation-CW2b-hQF.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-XoDIL5UX.js";import{useMDXComponents as s}from"./index-B6Rl91Qb.js";import{M as r,C as m}from"./blocks-NFZrBscH.js";import{S as i,m as a}from"./Steps.stories-DEA9NQo2.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-BYbbjgKe.js";import"./index-dv3hnGPT.js";function o(n){const e={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:i}),`
-`,`
-`,t.jsx(e.h1,{id:"steps",children:"Steps"}),`
-`,t.jsx(e.p,{children:"This is the documentation for the Steps component"}),`
-`,`
-`,t.jsx(m,{meta:a})]})}function d(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{d as default};
--- pr-preview/pr-739/assets/Documentation-_uM1cXcb.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-XoDIL5UX.js";import{useMDXComponents as r}from"./index-B6Rl91Qb.js";import{M as m,C as s}from"./blocks-NFZrBscH.js";import{B as a,m as c}from"./Breadcrumb.stories-B4Tu2j2r.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-BYbbjgKe.js";function o(t){const e={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:a}),`
-`,`
-`,n.jsx(e.h1,{id:"breadcrumb",children:"Breadcrumb"}),`
-`,n.jsx(e.p,{children:"This is the documentation for the Breadcrumb component"}),`
-`,`
-`,n.jsx(s,{meta:c})]})}function f(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{f as default};
