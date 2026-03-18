78b99e922ca77a2ba704de6a43276f24dd815014
mparticle-automation <developers@mparticle.com>
2026-03-18 04:11:47 +0000

    Deploy preview for PR 741 🛫

---
+++ pr-preview/pr-741/assets/Image.stories-CXCSu8gR.js
@@ -0,0 +1,27 @@
+var d=Object.defineProperty,w=Object.defineProperties;var v=Object.getOwnPropertyDescriptors;var p=Object.getOwnPropertySymbols;var f=Object.prototype.hasOwnProperty,y=Object.prototype.propertyIsEnumerable;var c=(s,e,r)=>e in s?d(s,e,{enumerable:!0,configurable:!0,writable:!0,value:r}):s[e]=r,t=(s,e)=>{for(var r in e||(e={}))f.call(e,r)&&c(s,r,e[r]);if(p)for(var r of p(e))y.call(e,r)&&c(s,r,e[r]);return s},o=(s,e)=>w(s,v(e));import{l as b}from"./UnauthorizedTooltip-BBXXfuJx.js";const j={title:"Components/Data Display/Image",component:b,args:{}},a={args:{src:"https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",preview:!1,width:100,height:100},argTypes:{preview:{control:"select",options:[!1,!0]},width:{control:"number"},height:{control:"number"}}},n={args:{src:"https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",preview:!1,width:100,height:100}};var i,g,l;a.parameters=o(t({},a.parameters),{docs:o(t({},(i=a.parameters)==null?void 0:i.docs),{source:t({originalSource:`{
+  args: {
+    src: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+    preview: false,
+    width: 100,
+    height: 100
+  },
+  argTypes: {
+    preview: {
+      control: 'select',
+      options: [false, true]
+    },
+    width: {
+      control: 'number'
+    },
+    height: {
+      control: 'number'
+    }
+  }
+}`},(l=(g=a.parameters)==null?void 0:g.docs)==null?void 0:l.source)})});var m,h,u;n.parameters=o(t({},n.parameters),{docs:o(t({},(m=n.parameters)==null?void 0:m.docs),{source:t({originalSource:`{
+  args: {
+    src: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+    preview: false,
+    width: 100,
+    height: 100
+  }
+}`},(u=(h=n.parameters)==null?void 0:h.docs)==null?void 0:u.source)})});const P=["Playground","Primary"],_=Object.freeze(Object.defineProperty({__proto__:null,Playground:a,Primary:n,__namedExportsOrder:P,default:j},Symbol.toStringTag,{value:"Module"}));export{_ as I,n as P,a};
