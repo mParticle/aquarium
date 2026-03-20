2f3eaaa30af2711604188b1474bb47f8eb66032a
mparticle-automation <developers@mparticle.com>
2026-03-20 14:16:42 +0000

    Deploy preview for PR 753 🛫

---
+++ pr-preview/pr-753/assets/Introduction-CfHKZw0i.js
@@ -0,0 +1,10 @@
+import{j as e}from"./iframe-BiJZ0YfI.js";import{useMDXComponents as i}from"./index-Dzu7GMnD.js";import"./preload-helper-PPVm8Dsz.js";function s(t){const n={h1:"h1",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{id:"introduction---rokt-ui-library",children:"Introduction - Rokt UI Library"}),`
+`,e.jsxs(n.p,{children:["Designed for developers, designers, and product managers, this library makes it easy to create intuitive, interactive interfaces for ",e.jsx(n.strong,{children:"Rokt applications"}),"."]}),`
+`,e.jsx(n.h2,{id:"key-features",children:"Key Features"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Foundations:"})," Design elements like colors, typography, spacing, and icons."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Components:"})," Ant Design-based, scalable, and ready to use."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Pattern Library:"})," The system we rely on to build the ",e.jsx(n.strong,{children:"Rokt application"}),"."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Interactive Examples:"})," Practical demos to help you get started quickly."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Simple, flexible, and reliable—this library helps you focus on building great experiences without worrying about the basics."})]})}function a(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{a as default};
---
+++ pr-preview/pr-753/assets/formatter-EIJCOSYU-BqbmAWCY.js
@@ -0,0 +1,1 @@
+import{d as i,r as m}from"./blocks-CV2YW82e.js";import{ck as o}from"./iframe-BiJZ0YfI.js";import"./preload-helper-PPVm8Dsz.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
