212a717c86bcff646820afd4bc855812e890fff2
mparticle-automation <developers@mparticle.com>
2026-03-23 17:29:04 +0000

    Deploy preview for PR 748 🛫

---
+++ pr-preview/pr-748/assets/Documentation-C627JcFG.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-Ct6d1r9x.js";import{useMDXComponents as r}from"./index-C5V-j8KF.js";import{M as s,C as m}from"./blocks-BVovmShm.js";import{F as a,m as i}from"./Form.stories-DwKfL9ev.js";import"./preload-helper-PPVm8Dsz.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
+`,`
+`,n.jsx(o.h1,{id:"form",children:"Form"}),`
+`,n.jsx(o.p,{children:"This is the documentation for the Forms component"}),`
+`,`
+`,n.jsx(m,{meta:i})]})}function j(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{j as default};
---
+++ pr-preview/pr-748/assets/Documentation-qXlfUQ5z.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-Ct6d1r9x.js";import{useMDXComponents as r}from"./index-C5V-j8KF.js";import{M as s,C as m}from"./blocks-BVovmShm.js";import{D as i,m as a}from"./Dropdown.stories-CGXbBP-S.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-C8x3x33I.js";import"./index-B3lzfIGu.js";function e(o){const t={h1:"h1",p:"p",...r(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:i}),`
+`,`
+`,n.jsx(t.h1,{id:"dropdown",children:"Dropdown"}),`
+`,n.jsx(t.p,{children:"This is the documentation for the Dropdown component"}),`
+`,`
+`,n.jsx(m,{meta:a})]})}function u(o={}){const{wrapper:t}={...r(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{u as default};
