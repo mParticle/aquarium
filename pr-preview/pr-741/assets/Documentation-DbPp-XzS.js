a4b45b437db7b9c05cf1a165a143eb545b7ad997
mparticle-automation <developers@mparticle.com>
2026-03-24 18:58:56 +0000

    Remove preview for PR 741 🛬

--- pr-preview/pr-741/assets/Documentation-DbPp-XzS.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-DGBKSHTq.js";import{useMDXComponents as r}from"./index-y-YuGVPp.js";import{M as s,C as m}from"./blocks-Xx52BWE4.js";import{F as a,m as i}from"./Form.stories-b9Om5AM3.js";import"./preload-helper-PPVm8Dsz.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
-`,`
-`,n.jsx(o.h1,{id:"form",children:"Form"}),`
-`,n.jsx(o.p,{children:"This is the documentation for the Forms component"}),`
-`,`
-`,n.jsx(m,{meta:i})]})}function j(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{j as default};
