49f8e712c3e70588f040992cfd0a663a8c55bb44
mparticle-automation <developers@mparticle.com>
2026-03-24 21:50:57 +0000

    Deploy preview for PR 764 🛫

---
+++ pr-preview/pr-764/assets/Documentation-B_iJQXCy.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-CW9QCQxL.js";import{useMDXComponents as s}from"./index-StOYhsMW.js";import{M as r,C as a}from"./blocks-CzRFuj6M.js";import{C as m,m as c}from"./Cascader.stories-DpzHPaQI.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-CrUJ6hvr.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:m}),`
+`,`
+`,n.jsx(e.h1,{id:"cascader",children:"Cascader"}),`
+`,n.jsx(e.p,{children:"This is the documentation for the Cascaders component"}),`
+`,`
+`,n.jsx(a,{meta:c})]})}function j(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{j as default};
--- pr-preview/pr-764/assets/Documentation-gMaQTaSP.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-CAdVB1w2.js";import{useMDXComponents as r}from"./index-DGErjAOe.js";import{M as m,C as s}from"./blocks-DRYM_F0h.js";import{B as a,m as c}from"./Breadcrumb.stories-ieMiPqR7.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-B3lScFyX.js";function o(t){const e={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:a}),`
-`,`
-`,n.jsx(e.h1,{id:"breadcrumb",children:"Breadcrumb"}),`
-`,n.jsx(e.p,{children:"This is the documentation for the Breadcrumb component"}),`
-`,`
-`,n.jsx(s,{meta:c})]})}function f(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{f as default};
