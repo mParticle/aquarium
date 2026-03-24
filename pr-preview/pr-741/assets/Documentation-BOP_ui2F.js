b817d010dbc736e079ea37185e8d216fadc6dd3c
mparticle-automation <developers@mparticle.com>
2026-03-24 18:20:30 +0000

    Deploy preview for PR 741 🛫

---
+++ pr-preview/pr-741/assets/Documentation-BOP_ui2F.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-DGBKSHTq.js";import{useMDXComponents as s}from"./index-y-YuGVPp.js";import{M as r,C as m}from"./blocks-Xx52BWE4.js";import{M as i,m as a}from"./Menu.stories-Wd04qzqG.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-JwJ35llf.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:i}),`
+`,`
+`,n.jsx(e.h1,{id:"menu",children:"Menu"}),`
+`,n.jsx(e.p,{children:"This is the documentation for the Menu component"}),`
+`,`
+`,n.jsx(m,{meta:a})]})}function f(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{f as default};
--- pr-preview/pr-741/assets/Documentation-BYZCOuHR.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-dEDhc9w4.js";import{useMDXComponents as s}from"./index-ByIiOC30.js";import{M as r,C as m}from"./blocks-B2zikTPd.js";import{M as i,m as a}from"./Menu.stories-B2VExvNg.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-C8DLD-d-.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:i}),`
-`,`
-`,n.jsx(e.h1,{id:"menu",children:"Menu"}),`
-`,n.jsx(e.p,{children:"This is the documentation for the Menu component"}),`
-`,`
-`,n.jsx(m,{meta:a})]})}function f(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{f as default};
