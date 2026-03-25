4c4bceacc8c5444951836afea715b7bd366168cd
mparticle-automation <developers@mparticle.com>
2026-03-25 02:01:38 +0000

    Deploy preview for PR 765 🛫

---
+++ pr-preview/pr-765/assets/Documentation-DklyhWK0.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-Cx1MpTX0.js";import{useMDXComponents as s}from"./index-CniMgaHU.js";import{M as r,C as m}from"./blocks-CiyYjHa1.js";import{S as i,m as a}from"./Steps.stories-B-5stYcg.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-DzjXqHUF.js";import"./index-CMvu_RCv.js";function o(n){const e={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:i}),`
+`,`
+`,t.jsx(e.h1,{id:"steps",children:"Steps"}),`
+`,t.jsx(e.p,{children:"This is the documentation for the Steps component"}),`
+`,`
+`,t.jsx(m,{meta:a})]})}function d(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{d as default};
