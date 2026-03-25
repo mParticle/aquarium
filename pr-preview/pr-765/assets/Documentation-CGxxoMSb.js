4e6fc2151df9fe17ce7b22a092a30dc9d37fee89
mparticle-automation <developers@mparticle.com>
2026-03-25 02:16:48 +0000

    Remove preview for PR 765 🛬

--- pr-preview/pr-765/assets/Documentation-CGxxoMSb.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-CzPnX7uv.js";import{useMDXComponents as r}from"./index-CwnbOkkm.js";import{M as s,C as m}from"./blocks-zNfncV_R.js";import{A as a,m as c}from"./Anchor.stories-BHnGCSH4.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-BmPXxzRo.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
-`,`
-`,n.jsx(o.h1,{id:"anchor",children:"Anchor"}),`
-`,n.jsx(o.p,{children:"This is the documentation for the Anchor component"}),`
-`,`
-`,n.jsx(m,{meta:c})]})}function u(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
