d5fe4e86d2e05bac89f4acd6188444b6b3b4d3f5
mparticle-automation <developers@mparticle.com>
2026-03-26 14:54:08 +0000

    Deploy preview for PR 767 🛫

---
+++ pr-preview/pr-767/assets/Skeleton.stories-DNdyceeE.js
@@ -0,0 +1,5 @@
+import{j as t,bs as a}from"./iframe-B19H_Hxr.js";const o={title:"Components/Feedback/Skeleton",component:r=>t.jsx(a,{...r,style:{height:"200px",width:"200px"}}),args:{loading:!0,paragraph:!1,title:!1}},e={args:{paragraph:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    paragraph: true
+  }
+}`,...e.parameters?.docs?.source}}};const s=["Primary"],p=Object.freeze(Object.defineProperty({__proto__:null,Primary:e,__namedExportsOrder:s,default:o},Symbol.toStringTag,{value:"Module"}));export{p as S,o as m};
