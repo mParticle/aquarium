03b64dc67b77161693abfaa54c25e07dfb1ee276
mparticle-automation <developers@mparticle.com>
2026-03-27 18:14:43 +0000

    Deploy preview for PR 770 🛫

---
+++ pr-preview/pr-770/assets/WarningStateIcon.stories-DVSGtSCn.js
@@ -0,0 +1,5 @@
+import{W as n}from"./iframe-NywP9lib.js";import"./preload-helper-PPVm8Dsz.js";const r={title:"Components/General/Icons/WarningStateIcon",component:n,parameters:{docs:{description:{component:"Warning State Icon component."}}}},e={args:{size:"lg"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'lg'
+  }
+}`,...e.parameters?.docs?.source}}};const a=["Default"];export{e as Default,a as __namedExportsOrder,r as default};
