93ba143b5f8f92246f3c88e7fb484ef3d8877b41
nastyastavitskaya <stavitskaya.anastasiia@gmail.com>
2026-03-30 14:43:38 +0000

    Deploying to gh-pages from @ mParticle/aquarium@5bc063b97616a2924e669558162d206152e1d9d7 🚀

---
+++ assets/Documentation-BNf-Vec1.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-Bijr46BI.js";import{useMDXComponents as r}from"./index-DzKoHKML.js";import{M as s,C as m}from"./blocks-KitLlJoG.js";import{D as i,m as a}from"./Dropdown.stories-ntnAADOj.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-C9htqX1R.js";import"./index-BGZWb55m.js";function e(o){const t={h1:"h1",p:"p",...r(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:i}),`
+`,`
+`,n.jsx(t.h1,{id:"dropdown",children:"Dropdown"}),`
+`,n.jsx(t.p,{children:"This is the documentation for the Dropdown component"}),`
+`,`
+`,n.jsx(m,{meta:a})]})}function u(o={}){const{wrapper:t}={...r(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{u as default};
