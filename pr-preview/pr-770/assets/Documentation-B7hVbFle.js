8f1cced035f57d59af765037a8639b51aa48366c
mparticle-automation <developers@mparticle.com>
2026-03-30 14:06:47 +0000

    Remove preview for PR 770 🛬

--- pr-preview/pr-770/assets/Documentation-B7hVbFle.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-NywP9lib.js";import{useMDXComponents as s}from"./index-Dun1Y0zJ.js";import{M as r,C as m}from"./blocks-DBFdKV35.js";import{S as i,m as a}from"./Steps.stories-DMWMmq87.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-BEbcpOGG.js";import"./index-Fu_kpJD_.js";function o(n){const e={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:i}),`
-`,`
-`,t.jsx(e.h1,{id:"steps",children:"Steps"}),`
-`,t.jsx(e.p,{children:"This is the documentation for the Steps component"}),`
-`,`
-`,t.jsx(m,{meta:a})]})}function d(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{d as default};
