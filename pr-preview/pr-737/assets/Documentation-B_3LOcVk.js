60cbc8b15507fc23f74240d2c39ea6d9fed1037e
mparticle-automation <developers@mparticle.com>
2026-03-30 14:23:29 +0000

    Deploy preview for PR 737 🛫

---
+++ pr-preview/pr-737/assets/Documentation-B_3LOcVk.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-D7MEFxlY.js";import{useMDXComponents as i}from"./index-Cn3sYnmc.js";import{M as a,C as s}from"./blocks-gjXDpDiE.js";import{P as r,m}from"./Pagination.stories-BHsZia7U.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-BOx8bSO9.js";function e(t){const o={h1:"h1",p:"p",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:r}),`
+`,`
+`,n.jsx(o.h1,{id:"pagination",children:"Pagination"}),`
+`,n.jsx(o.p,{children:"This is the documentation for the Pagination component"}),`
+`,`
+`,n.jsx(s,{meta:m})]})}function u(t={}){const{wrapper:o}={...i(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
---
+++ pr-preview/pr-737/assets/Documentation-Dbhs9OmH.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-D7MEFxlY.js";import{useMDXComponents as s}from"./index-Cn3sYnmc.js";import{M as r,C as a}from"./blocks-gjXDpDiE.js";import{C as m,m as c}from"./Cascader.stories-yKTN2Y2V.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-BOx8bSO9.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:m}),`
+`,`
+`,n.jsx(e.h1,{id:"cascader",children:"Cascader"}),`
+`,n.jsx(e.p,{children:"This is the documentation for the Cascaders component"}),`
+`,`
+`,n.jsx(a,{meta:c})]})}function j(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{j as default};
