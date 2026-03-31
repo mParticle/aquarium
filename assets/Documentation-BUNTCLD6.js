1e3b9687dd56162533406eaf2826ff0115563903
nastyastavitskaya <stavitskaya.anastasiia@gmail.com>
2026-03-31 16:26:37 +0000

    Deploying to gh-pages from @ mParticle/aquarium@9c63af2b364620cacce059e71dbcf83a7b63fdb6 🚀

--- assets/Documentation-BUNTCLD6.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-Bijr46BI.js";import{useMDXComponents as i}from"./index-DzKoHKML.js";import{M as a,C as s}from"./blocks-KitLlJoG.js";import{P as r,m}from"./Pagination.stories-e1f7Y351.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-C9htqX1R.js";function e(t){const o={h1:"h1",p:"p",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:r}),`
-`,`
-`,n.jsx(o.h1,{id:"pagination",children:"Pagination"}),`
-`,n.jsx(o.p,{children:"This is the documentation for the Pagination component"}),`
-`,`
-`,n.jsx(s,{meta:m})]})}function u(t={}){const{wrapper:o}={...i(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
