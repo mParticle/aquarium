aadecb39c7f819d5a6c50f6dcb54e23c6fbc07af
mparticle-automation <developers@mparticle.com>
2026-03-31 18:28:20 +0000

    Deploy preview for PR 775 🛫

---
+++ pr-preview/pr-775/assets/Documentation-3ydeuy9d.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-DLzD0hVH.js";import{useMDXComponents as i}from"./index-Dbg29Dvf.js";import{M as a,C as s}from"./blocks-DhGjPv97.js";import{P as r,m}from"./Pagination.stories-CkmtsRfG.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-C86iIyow.js";function e(t){const o={h1:"h1",p:"p",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:r}),`
+`,`
+`,n.jsx(o.h1,{id:"pagination",children:"Pagination"}),`
+`,n.jsx(o.p,{children:"This is the documentation for the Pagination component"}),`
+`,`
+`,n.jsx(s,{meta:m})]})}function u(t={}){const{wrapper:o}={...i(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
---
+++ pr-preview/pr-775/assets/Documentation-BJNuTNt2.js
@@ -0,0 +1,4 @@
+import{j as t}from"./iframe-DLzD0hVH.js";import{useMDXComponents as s}from"./index-Dbg29Dvf.js";import{M as r,C as a}from"./blocks-DhGjPv97.js";import i,{UXPatternExample as c}from"./Steps.stories-D5k7C9ZS.js";import"./preload-helper-PPVm8Dsz.js";function o(e){const n={h1:"h1",p:"p",...s(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:i}),`
+`,t.jsx(n.h1,{id:"steps-progress-indicator",children:"Steps Progress Indicator"}),`
+`,t.jsx(n.p,{children:"Steps help communicate the current state of multistep flows. The following UX pattern centers the vertical label placement version inside its container so it feels like a dashboard callout."}),`
+`,t.jsx(a,{of:c})]})}function h(e={}){const{wrapper:n}={...s(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{h as default};
