0811cb836fda5041bd17d5b2ba801f9b80bd41b5
mparticle-automation <developers@mparticle.com>
2026-04-02 19:11:03 +0000

    Remove preview for PR 782 🛬

--- pr-preview/pr-782/assets/Documentation-CoG6JtId.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-CRvrMLiP.js";import{useMDXComponents as s}from"./index-C4j-D8Ui.js";import{M as r,C as m}from"./blocks-BoMWSYWd.js";import{S as i,m as a}from"./Steps.stories-CZ5dUnX9.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-B3jzxlx8.js";import"./index-DbaAW5CT.js";function o(n){const e={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:i}),`
-`,`
-`,t.jsx(e.h1,{id:"steps",children:"Steps"}),`
-`,t.jsx(e.p,{children:"This is the documentation for the Steps component"}),`
-`,`
-`,t.jsx(m,{meta:a})]})}function d(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{d as default};
--- pr-preview/pr-782/assets/Documentation-PgBTlu_M.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-CRvrMLiP.js";import{useMDXComponents as i}from"./index-C4j-D8Ui.js";import{M as a,C as s}from"./blocks-BoMWSYWd.js";import{P as r,m}from"./Pagination.stories-lC0O32BF.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-B3jzxlx8.js";function e(t){const o={h1:"h1",p:"p",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:r}),`
-`,`
-`,n.jsx(o.h1,{id:"pagination",children:"Pagination"}),`
-`,n.jsx(o.p,{children:"This is the documentation for the Pagination component"}),`
-`,`
-`,n.jsx(s,{meta:m})]})}function u(t={}){const{wrapper:o}={...i(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
