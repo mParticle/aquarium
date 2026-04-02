eadf934ccb63b3470fd52855eb6b4a7fc6d399f4
mparticle-automation <developers@mparticle.com>
2026-04-02 18:47:19 +0000

    Remove preview for PR 780 🛬

--- pr-preview/pr-780/assets/Documentation-B92ev4bd.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-D4pWCPvr.js";import{useMDXComponents as r}from"./index-BEVNAEWf.js";import{M as s,C as m}from"./blocks-C50gnnpm.js";import{D as i,m as a}from"./Dropdown.stories-9oyyO-LG.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-BcLZ2FlD.js";import"./index-BCVCNt4p.js";function e(o){const t={h1:"h1",p:"p",...r(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:i}),`
-`,`
-`,n.jsx(t.h1,{id:"dropdown",children:"Dropdown"}),`
-`,n.jsx(t.p,{children:"This is the documentation for the Dropdown component"}),`
-`,`
-`,n.jsx(m,{meta:a})]})}function u(o={}){const{wrapper:t}={...r(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{u as default};
--- pr-preview/pr-780/assets/Documentation-ui6Pk3Gf.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-D4pWCPvr.js";import{useMDXComponents as s}from"./index-BEVNAEWf.js";import{M as r,C as m}from"./blocks-C50gnnpm.js";import{M as i,m as a}from"./Menu.stories-dwgW47-w.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-BcLZ2FlD.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:i}),`
-`,`
-`,n.jsx(e.h1,{id:"menu",children:"Menu"}),`
-`,n.jsx(e.p,{children:"This is the documentation for the Menu component"}),`
-`,`
-`,n.jsx(m,{meta:a})]})}function f(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{f as default};
