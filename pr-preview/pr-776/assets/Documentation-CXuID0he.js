f4472c6e364409523e486b734b1173af51b909ae
mparticle-automation <developers@mparticle.com>
2026-04-08 19:28:48 +0000

    Remove preview for PR 776 🛬

--- pr-preview/pr-776/assets/Documentation-CXuID0he.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe--4_JQr3W.js";import{useMDXComponents as i}from"./index-D-d89Jxu.js";import{M as a,C as s}from"./blocks-FWOHs2wc.js";import{P as r,m}from"./Pagination.stories-EZ-KjMi5.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-Cn6fqXhJ.js";function e(t){const o={h1:"h1",p:"p",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:r}),`
-`,`
-`,n.jsx(o.h1,{id:"pagination",children:"Pagination"}),`
-`,n.jsx(o.p,{children:"This is the documentation for the Pagination component"}),`
-`,`
-`,n.jsx(s,{meta:m})]})}function u(t={}){const{wrapper:o}={...i(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
