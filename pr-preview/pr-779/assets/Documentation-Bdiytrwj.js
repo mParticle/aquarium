02c77eb6d374eac19a88f767c75b3722f1f324fc
mparticle-automation <developers@mparticle.com>
2026-04-08 19:04:13 +0000

    Remove preview for PR 779 🛬

--- pr-preview/pr-779/assets/Documentation-Bdiytrwj.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-B9KBqt4e.js";import{useMDXComponents as r}from"./index-CV7OYdst.js";import{M as s,C as m}from"./blocks-BcVVhA2o.js";import{F as a,m as i}from"./Form.stories-CKONsFqf.js";import"./preload-helper-PPVm8Dsz.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
-`,`
-`,n.jsx(o.h1,{id:"form",children:"Form"}),`
-`,n.jsx(o.p,{children:"This is the documentation for the Forms component"}),`
-`,`
-`,n.jsx(m,{meta:i})]})}function j(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{j as default};
