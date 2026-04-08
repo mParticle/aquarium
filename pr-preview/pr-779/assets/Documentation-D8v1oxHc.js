fa31c646437a356038221dc08c3834a0e6bd307a
mparticle-automation <developers@mparticle.com>
2026-04-08 17:52:00 +0000

    Deploy preview for PR 779 🛫

---
+++ pr-preview/pr-779/assets/Documentation-D8v1oxHc.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-CiLrwviW.js";import{useMDXComponents as r}from"./index-DVfJGkch.js";import{M as s,C as m}from"./blocks-CU2YkviB.js";import{F as a,m as i}from"./Form.stories-BbVcNL7V.js";import"./preload-helper-PPVm8Dsz.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
+`,`
+`,n.jsx(o.h1,{id:"form",children:"Form"}),`
+`,n.jsx(o.p,{children:"This is the documentation for the Forms component"}),`
+`,`
+`,n.jsx(m,{meta:i})]})}function j(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{j as default};
