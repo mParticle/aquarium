6e8f66e4a40d324ede3dbf8c5866be423311e5b5
mparticle-automation <developers@mparticle.com>
2026-04-08 19:06:39 +0000

    Deploy preview for PR 781 🛫

--- pr-preview/pr-781/assets/Documentation-CTu-LHz4.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-C_Q0yocC.js";import{useMDXComponents as r}from"./index-B28HSB9V.js";import{M as s,C as m}from"./blocks-Dlx9lX0y.js";import{F as a,m as i}from"./Form.stories-DCExEIvh.js";import"./preload-helper-PPVm8Dsz.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
-`,`
-`,n.jsx(o.h1,{id:"form",children:"Form"}),`
-`,n.jsx(o.p,{children:"This is the documentation for the Forms component"}),`
-`,`
-`,n.jsx(m,{meta:i})]})}function j(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{j as default};
---
+++ pr-preview/pr-781/assets/Documentation-IuJeAm1I.js
@@ -0,0 +1,6 @@
+import{j as n}from"./iframe-BngPXtiH.js";import{useMDXComponents as r}from"./index-CRLJGpoh.js";import{M as s,C as m}from"./blocks-iqbbz-_P.js";import{A as a,m as c}from"./Anchor.stories-DOx-PK0d.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-CeLR8vjF.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
+`,`
+`,n.jsx(o.h1,{id:"anchor",children:"Anchor"}),`
+`,n.jsx(o.p,{children:"This is the documentation for the Anchor component"}),`
+`,`
+`,n.jsx(m,{meta:c})]})}function u(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
