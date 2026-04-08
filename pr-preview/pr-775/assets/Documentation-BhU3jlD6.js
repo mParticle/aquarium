6f49a351e8d33e3e2090d13e4f2feb231344432c
mparticle-automation <developers@mparticle.com>
2026-04-08 18:10:10 +0000

    Remove preview for PR 775 🛬

--- pr-preview/pr-775/assets/Documentation-BhU3jlD6.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-BfpRVCra.js";import{useMDXComponents as r}from"./index-DIBI5meD.js";import{M as s,C as m}from"./blocks-QJxvVFzK.js";import{A as a,m as c}from"./Anchor.stories-B_A6g9DF.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-C08c3wxl.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
-`,`
-`,n.jsx(o.h1,{id:"anchor",children:"Anchor"}),`
-`,n.jsx(o.p,{children:"This is the documentation for the Anchor component"}),`
-`,`
-`,n.jsx(m,{meta:c})]})}function u(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
--- pr-preview/pr-775/assets/Documentation-CrdeA3P6.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-BfpRVCra.js";import{useMDXComponents as s}from"./index-DIBI5meD.js";import{M as r,C as m}from"./blocks-QJxvVFzK.js";import{M as i,m as a}from"./Menu.stories-CRQERU7J.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-C08c3wxl.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:i}),`
-`,`
-`,n.jsx(e.h1,{id:"menu",children:"Menu"}),`
-`,n.jsx(e.p,{children:"This is the documentation for the Menu component"}),`
-`,`
-`,n.jsx(m,{meta:a})]})}function f(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{f as default};
