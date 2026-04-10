d537e3eb3dff73f1d727c53f105e9bd6b234280a
mparticle-automation <developers@mparticle.com>
2026-04-10 22:44:03 +0000

    Remove preview for PR 789 🛬

--- pr-preview/pr-789/assets/Documentation-9WXS7L-m.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-D-tZdf8e.js";import{useMDXComponents as s}from"./index-w4sJF_su.js";import{M as r,C as m}from"./blocks-qbQ649UP.js";import{M as i,m as a}from"./Menu.stories-S5SOMTsl.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-7jXB1y0z.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:i}),`
-`,`
-`,n.jsx(e.h1,{id:"menu",children:"Menu"}),`
-`,n.jsx(e.p,{children:"This is the documentation for the Menu component"}),`
-`,`
-`,n.jsx(m,{meta:a})]})}function f(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{f as default};
