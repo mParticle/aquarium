64f7b9de2352932f9852a8cfc795db352d049f22
mparticle-automation <developers@mparticle.com>
2026-04-10 22:38:01 +0000

    Deploy preview for PR 789 🛫

---
+++ pr-preview/pr-789/assets/Documentation-D6TnHPOx.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-D-tZdf8e.js";import{useMDXComponents as s}from"./index-w4sJF_su.js";import{M as r,C as m}from"./blocks-qbQ649UP.js";import{S as i,m as a}from"./Steps.stories-CvD6l4wC.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-7jXB1y0z.js";import"./index-3emILsii.js";function o(n){const e={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:i}),`
+`,`
+`,t.jsx(e.h1,{id:"steps",children:"Steps"}),`
+`,t.jsx(e.p,{children:"This is the documentation for the Steps component"}),`
+`,`
+`,t.jsx(m,{meta:a})]})}function d(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{d as default};
