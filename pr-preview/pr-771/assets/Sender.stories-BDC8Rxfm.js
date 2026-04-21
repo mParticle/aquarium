a8217405c902e1a1ba2d411161b9f0fa9cd0124d
mparticle-automation <developers@mparticle.com>
2026-04-21 00:23:15 +0000

    Deploy preview for PR 771 🛫

---
+++ pr-preview/pr-771/assets/Sender.stories-BDC8Rxfm.js
@@ -0,0 +1,5 @@
+import{j as e,q as s}from"./iframe-CWmmeg14.js";import{S as a}from"./index-DcV1YhRY.js";import"./preload-helper-PPVm8Dsz.js";import"./pickAttrs-CPIpiPo_.js";import"./use-proxy-imperative-handle-Bg3msIAp.js";import"./genStyleUtils-ChRZej2F.js";import"./index-DUVYEYam.js";import"./useLocale-DQp5srxs.js";import"./RecordingIcon-BPWmqvKV.js";const x={title:"Components/AI/Sender",component:a,decorators:[o=>e.jsx(s,{children:e.jsx(o,{})})]},r={args:{placeholder:"Type a message..."}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Type a message...'
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Basic"];export{r as Basic,u as __namedExportsOrder,x as default};
