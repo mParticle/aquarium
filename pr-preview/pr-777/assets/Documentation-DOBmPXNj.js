491851bd8f2a89a74b0ce1a429bbe837fdb624dc
mparticle-automation <developers@mparticle.com>
2026-04-21 00:48:24 +0000

    Deploy preview for PR 777 🛫

--- pr-preview/pr-777/assets/Documentation-DOBmPXNj.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-Bq6_dcka.js";import{useMDXComponents as r}from"./index-BG6QoBnM.js";import{M as m,C as s}from"./blocks-AaPFc3Ng.js";import{B as a,m as c}from"./Breadcrumb.stories-D4Taqa7A.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-AxW8CmjC.js";function o(t){const e={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:a}),`
-`,`
-`,n.jsx(e.h1,{id:"breadcrumb",children:"Breadcrumb"}),`
-`,n.jsx(e.p,{children:"This is the documentation for the Breadcrumb component"}),`
-`,`
-`,n.jsx(s,{meta:c})]})}function f(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{f as default};
---
+++ pr-preview/pr-777/assets/Sender.stories-eYOftgDR.js
@@ -0,0 +1,5 @@
+import{j as e,q as s}from"./iframe-Cca8AuLj.js";import{S as a}from"./index-DtfxCtTL.js";import"./preload-helper-PPVm8Dsz.js";import"./pickAttrs-ucuC9ZBb.js";import"./use-proxy-imperative-handle-C8ghiugu.js";import"./genStyleUtils-DoJfQs4H.js";import"./index-BQIXZOYS.js";import"./useLocale-c0uf5b8Q.js";import"./RecordingIcon-yXxTBsTQ.js";const x={title:"Components/AI/Sender",component:a,decorators:[o=>e.jsx(s,{children:e.jsx(o,{})})]},r={args:{placeholder:"Type a message..."}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Type a message...'
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Basic"];export{r as Basic,u as __namedExportsOrder,x as default};
