adb0b0e87e960f32852d84a0f4265073abf2afba
mparticle-automation <developers@mparticle.com>
2026-04-23 00:08:39 +0000

    Remove preview for PR 800 🛬

--- pr-preview/pr-800/assets/Documentation-IDy8BY8R.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-DHNzjgti.js";import{useMDXComponents as s}from"./index-D25Ukwej.js";import{M as r,C as m}from"./blocks-BmGiSDIg.js";import{M as i,m as a}from"./Menu.stories-B132dzne.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-6pyPetu4.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:i}),`
-`,`
-`,n.jsx(e.h1,{id:"menu",children:"Menu"}),`
-`,n.jsx(e.p,{children:"This is the documentation for the Menu component"}),`
-`,`
-`,n.jsx(m,{meta:a})]})}function f(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{f as default};
--- pr-preview/pr-800/assets/Sender.stories-B_6ykiKg.js
+++
@@ -1,5 +0,0 @@
-import{j as e,p as s}from"./iframe-DHNzjgti.js";import{S as a}from"./index-DCTbmdId.js";import"./preload-helper-PPVm8Dsz.js";import"./pickAttrs-6g13qgN3.js";import"./use-proxy-imperative-handle-CZmBh3qs.js";import"./genStyleUtils-CFPj1Yc3.js";import"./index-C2_VwOCh.js";import"./useLocale-BEySuCBw.js";import"./RecordingIcon-BcCkOl9j.js";const x={title:"Components/AI/Sender",component:a,decorators:[o=>e.jsx(s,{children:e.jsx(o,{})})]},r={args:{placeholder:"Type a message..."}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    placeholder: 'Type a message...'
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Basic"];export{r as Basic,u as __namedExportsOrder,x as default};
