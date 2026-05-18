a2884c094edec80326612f22098e802a04ecc8fd
mparticle-automation <developers@mparticle.com>
2026-05-18 21:06:07 +0000

    Remove preview for PR 801 🛬

--- pr-preview/pr-801/assets/Sender.stories-DRSzkJ4M.js
+++
@@ -1,5 +0,0 @@
-import{j as e,p as s}from"./iframe-heDX7BHP.js";import{S as a}from"./index-B-aQgMhE.js";import"./preload-helper-PPVm8Dsz.js";import"./pickAttrs-wFPx9zuw.js";import"./use-proxy-imperative-handle-CtRK1MmZ.js";import"./genStyleUtils-DTUehnUe.js";import"./index-CgCl33MM.js";import"./useLocale-C-FOfrqa.js";import"./RecordingIcon-Blf5jKZU.js";const x={title:"Components/AI/Sender",component:a,decorators:[o=>e.jsx(s,{children:e.jsx(o,{})})]},r={args:{placeholder:"Type a message..."}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    placeholder: 'Type a message...'
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Basic"];export{r as Basic,u as __namedExportsOrder,x as default};
