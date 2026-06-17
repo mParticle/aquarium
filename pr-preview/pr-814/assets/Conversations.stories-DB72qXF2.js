0508c3fe8d411fd8b178c0d04fc14ac946eeae85
mparticle-automation <developers@mparticle.com>
2026-06-17 20:38:40 +0000

    Deploy preview for PR 814 🛫

---
+++ pr-preview/pr-814/assets/Conversations.stories-DB72qXF2.js
@@ -0,0 +1,14 @@
+import{j as o,p as r}from"./iframe-CeL89nAb.js";import{C as a}from"./index-XPAPqFtV.js";import"./preload-helper-PPVm8Dsz.js";import"./pickAttrs-BQUZbHI5.js";import"./genStyleUtils-BWcNulH1.js";import"./index-CVL1kLYE.js";import"./use-proxy-imperative-handle-D-LGbeRZ.js";import"./useLocale-D0M7hg-R.js";const u={title:"Components/AI/Conversations",component:a,decorators:[s=>o.jsx(r,{children:o.jsx(s,{})})]},e={args:{items:[{key:"1",label:"What is Aquarium?"},{key:"2",label:"How do I add icons?"},{key:"3",label:"Design token usage"}]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      key: '1',
+      label: 'What is Aquarium?'
+    }, {
+      key: '2',
+      label: 'How do I add icons?'
+    }, {
+      key: '3',
+      label: 'Design token usage'
+    }]
+  }
+}`,...e.parameters?.docs?.source}}};const g=["Basic"];export{e as Basic,g as __namedExportsOrder,u as default};
