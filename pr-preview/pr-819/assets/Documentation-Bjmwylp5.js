c33d1e99fd30429ef06d20d373595543bb98e853
mparticle-automation <developers@mparticle.com>
2026-08-25 14:07:06 +0000

    Deploy preview for PR 819 🛫

---
+++ pr-preview/pr-819/assets/Documentation-Bjmwylp5.js
@@ -0,0 +1,7 @@
+import{j as n}from"./iframe-B4GNa_6i.js";import{useMDXComponents as e}from"./index-BLQDGB9O.js";import{M as i,S as t}from"./blocks-DszejVac.js";import{S as a,a as c,M as m,P as d,R as p}from"./Spacing.stories-RzfIqs7V.js";import"./preload-helper-PPVm8Dsz.js";import"./CopyableToken-DbJYaKbK.js";import"./Copy01-CPv5X4j_.js";function r(s){const o={code:"code",h1:"h1",p:"p",...e(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:a}),`
+`,n.jsx(o.h1,{id:"spacing",children:"Spacing"}),`
+`,n.jsxs(o.p,{children:["Spacing, margin, padding, and radius tokens. Import from ",n.jsx(o.code,{children:"src/styles/style"}),"."]}),`
+`,n.jsx(t,{of:c}),`
+`,n.jsx(t,{of:m}),`
+`,n.jsx(t,{of:d}),`
+`,n.jsx(t,{of:p})]})}function M(s={}){const{wrapper:o}={...e(),...s.components};return o?n.jsx(o,{...s,children:n.jsx(r,{...s})}):r(s)}export{M as default};
---
+++ pr-preview/pr-819/assets/Documentation-DNttUA67.js
@@ -0,0 +1,6 @@
+import{j as t}from"./iframe-B4GNa_6i.js";import{useMDXComponents as s}from"./index-BLQDGB9O.js";import{M as r,C as m}from"./blocks-DszejVac.js";import{S as i,m as a}from"./Steps.stories-CRsx4fy7.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-uuCoCT9m.js";import"./index-_bnllAA5.js";function o(n){const e={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:i}),`
+`,`
+`,t.jsx(e.h1,{id:"steps",children:"Steps"}),`
+`,t.jsx(e.p,{children:"This is the documentation for the Steps component"}),`
+`,`
+`,t.jsx(m,{meta:a})]})}function d(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{d as default};
