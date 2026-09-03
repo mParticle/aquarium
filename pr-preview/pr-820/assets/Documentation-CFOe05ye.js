aa186b81db07b14525d9b394c0a7e3f78a90dd6a
mparticle-automation <developers@mparticle.com>
2026-09-03 14:51:03 +0000

    Remove preview for PR 820 🛬

--- pr-preview/pr-820/assets/Documentation-CFOe05ye.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./iframe-CqU-JZIl.js";import{useMDXComponents as i}from"./index-vEp6w6jC.js";import{M as a,C as s}from"./blocks-ve8sXWC6.js";import{P as r,m}from"./Pagination.stories-BRdCfT9W.js";import"./preload-helper-PPVm8Dsz.js";import"./ExampleStory-Bnzq3_cG.js";function e(t){const o={h1:"h1",p:"p",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:r}),`
-`,`
-`,n.jsx(o.h1,{id:"pagination",children:"Pagination"}),`
-`,n.jsx(o.p,{children:"This is the documentation for the Pagination component"}),`
-`,`
-`,n.jsx(s,{meta:m})]})}function u(t={}){const{wrapper:o}={...i(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{u as default};
--- pr-preview/pr-820/assets/Sender.stories-DBUf7LYG.js
+++
@@ -1,5 +0,0 @@
-import{j as e,p as s}from"./iframe-CqU-JZIl.js";import{S as a}from"./index-DFSU4pcc.js";import"./preload-helper-PPVm8Dsz.js";import"./pickAttrs-Bh7Q0qXm.js";import"./use-proxy-imperative-handle-NNkcBgiU.js";import"./genStyleUtils-DnXnDLNb.js";import"./index-BfSgO9rI.js";import"./useLocale-BYzCkaaF.js";import"./RecordingIcon-CiRWinI2.js";const x={title:"Components/AI/Sender",component:a,decorators:[o=>e.jsx(s,{children:e.jsx(o,{})})]},r={args:{placeholder:"Type a message..."}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    placeholder: 'Type a message...'
-  }
-}`,...r.parameters?.docs?.source}}};const u=["Basic"];export{r as Basic,u as __namedExportsOrder,x as default};
